442537ef7fac992fb7fe3a80f87fe99cfa668ee9
Hoàng Anh Tú <tuha263@gmail.com>
2023-01-22 00:10:42 +0700

    Implement Base UI

---
+++ Scripts/Scenes/Loading/UITemplateLoadingSceneInstaller.cs
@@ -0,0 +1,12 @@
+namespace UITemplate.Scripts.Scenes.Loading
+{
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
+
+    public class UITemplateLoadingSceneInstaller : BaseSceneInstaller
+    {
+        public override void InstallBindings()
+        {
+            base.InstallBindings();
+        }
+    }
+}
---
+++ Scripts/Scenes/Main/UITemplateMainSceneInstaller.cs
@@ -0,0 +1,9 @@
+namespace UITemplate.Scripts.Scenes.Main
+{
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
+
+    public class UITemplateMainSceneInstaller : BaseSceneInstaller
+    {
+        
+    }
+}

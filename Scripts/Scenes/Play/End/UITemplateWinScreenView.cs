d482c349fdd39fad7a31de59c10b5288a73023ff
ducyb294 <ducyb2942@gmail.com>
2023-02-03 11:06:27 +0700

    Update win logic

---
+++ Scripts/Scenes/Play/End/UITemplateWinScreenView.cs
@@ -0,0 +1,59 @@
+namespace UITemplate.Scripts.Scenes.Play.End
+{
+    using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.View;
+    using GameFoundation.Scripts.Utilities.LogService;
+    using UITemplate.Scripts.Scenes.Popups;
+    using UnityEngine.UI;
+    using Zenject;
+
+    public class UITemplateWinScreenModel
+    {
+        public int StarRate;
+        public UITemplateWinScreenModel(int starRate)
+        {
+            this.StarRate = starRate;
+        }
+    }
+
+    public class UITemplateWinScreenView : BaseView
+    {
+        public Button                 HomeButton;
+        public Button                 HomeEndgameButton;
+        public Button                 ReplayEndgameButton;
+        public Button                 NextEndgameButton;
+        public UITemplateCurrencyText CoinText;
+        public UITemplateStarRate     StarRate;
+    }
+
+    public class UITemplateWinScreenPresenter : BaseScreenPresenter<UITemplateWinScreenView, UITemplateWinScreenModel>
+    {
+        public UITemplateWinScreenPresenter(SignalBus signalBus, ILogService logService) : base(signalBus, logService) { }
+
+        protected override async void OnViewReady()
+        {
+            base.OnViewReady();
+            await this.OpenViewAsync();
+            this.View.HomeButton.onClick.AddListener(this.OnClickHome);
+            this.View.HomeEndgameButton.onClick.AddListener(this.OnClickHome);
+            this.View.ReplayEndgameButton.onClick.AddListener(this.OnClickReplay);
+            this.View.NextEndgameButton.onClick.AddListener(this.OnClickNext);
+        }
+
+        public override void BindData(UITemplateWinScreenModel model)
+        {
+            this.View.CoinText.Subscribe(this.SignalBus);
+            this.View.StarRate.SetStarRate(this.Model.StarRate);
+        }
+
+        private void OnClickHome()   { }
+        private void OnClickReplay() { }
+        private void OnClickNext()   { }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            this.View.CoinText.Unsubscribe(this.SignalBus);
+        }
+    }
+}

69084517add0c3df7b2cdba9969ac6333420d6ec
ducyb294 <ducyb2942@gmail.com>
2023-02-03 12:38:18 +0700

    Update star rate

--- Scripts/Scenes/Utils/UITemplateStarRate.cs
+++ Scripts/Scenes/Utils/UITemplateStarRate.cs
@@ -1,8 +1,9 @@
 ﻿namespace UITemplate.Scripts.Scenes.Popups
 {
+    using System.Collections.Generic;
     using UnityEngine;
     
-    public class UITemplateStarRate
+    public class UITemplateStarRate : MonoBehaviour
     {
         public GameObject Star1On;
         public GameObject Star2On;
@@ -10,32 +11,24 @@
         public GameObject Star1Off;
         public GameObject Star2Off;
         public GameObject Star3Off;
+        private readonly Dictionary<int, List<bool>> starStates = new()
+        {
+            { 1, new List<bool> { true, false, false } },
+            { 2, new List<bool> { true, true, false } },
+            { 3, new List<bool> { true, true, true } },
+            { default, new List<bool> { false, false, false } }
+        };
 
         public void SetStarRate(int rate)
         {
-            switch (rate)
+            if (!this.starStates.TryGetValue(rate, out var starStates))
             {
-                case 1:
-                    this.SetOnOffStar(this.Star1On, this.Star1Off, true);
-                    this.SetOnOffStar(this.Star2On, this.Star2Off, false);
-                    this.SetOnOffStar(this.Star3On, this.Star3Off, false);
-                    break;
-                case 2:
-                    this.SetOnOffStar(this.Star1On, this.Star1Off, true);
-                    this.SetOnOffStar(this.Star2On, this.Star2Off, true);
-                    this.SetOnOffStar(this.Star3On, this.Star3Off, false);
-                    break;
-                case 3:
-                    this.SetOnOffStar(this.Star1On, this.Star1Off, true);
-                    this.SetOnOffStar(this.Star2On, this.Star2Off, true);
-                    this.SetOnOffStar(this.Star3On, this.Star3Off, true);
-                    break;
-                default:
-                    this.SetOnOffStar(this.Star1On, this.Star1Off, false);
-                    this.SetOnOffStar(this.Star2On, this.Star2Off, false);
-                    this.SetOnOffStar(this.Star3On, this.Star3Off, false);
-                    break;
+                starStates = this.starStates[default];
             }
+
+            this.SetOnOffStar(this.Star1On, this.Star1Off, starStates[0]);
+            this.SetOnOffStar(this.Star2On, this.Star2Off, starStates[1]);
+            this.SetOnOffStar(this.Star3On, this.Star3Off, starStates[2]);
         }
 
         private void SetOnOffStar(GameObject onObj, GameObject offObj, bool isActive)

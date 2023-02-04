43dc8595086c122f78a0181d004e44865f02cb66
hailuu <hoanghai.luu.71@gmail.com>
2023-02-04 17:00:01 +0700

    complete get time online from api, init time when initialize

--- Scripts/Models/UITemplateDailyRewardData.cs
+++ Scripts/Models/UITemplateDailyRewardData.cs
@@ -1,7 +1,15 @@
 ﻿namespace UITemplate.Scripts.Models
 {
+    using System;
+    using Cysharp.Threading.Tasks;
+    using Utility;
+
     public class UITemplateDailyRewardData
     {
-        
+        private readonly IOnlineTime onlineTime = new WorldTimeAPI();
+
+        public       DateTime     BeginDate         { get; set; }
+        public       int          LoginDay          { get; set; }
+        public async UniTask<int> GetUserLoginDay() => (await this.onlineTime.GetCurrentTimeAsync()).Day - this.BeginDate.Day + 1;
     }
 }
--- Scripts/Models/UITemplateUserData.cs
+++ Scripts/Models/UITemplateUserData.cs
@@ -1,26 +1,22 @@
 namespace UITemplate.Scripts.Models
 {
-    using GameFoundation.Scripts.Interfaces;
     using UITemplate.Scripts.Blueprints;
 
-    public class UITemplateUserData : ILocalData
+    public class UITemplateUserData
     {
-        public readonly UITemplateLevelData     LevelData;
-        public readonly UITemplateShopData      ShopData;
-        public readonly UITemplateInventoryData InventoryData;
-        public readonly UITemplateSettingData   SettingData;
+        public readonly UITemplateLevelData       LevelData;
+        public readonly UITemplateShopData        ShopData;
+        public readonly UITemplateInventoryData   InventoryData;
+        public readonly UITemplateSettingData     SettingData;
+        public readonly UITemplateDailyRewardData DailyRewardData;
 
         public UITemplateUserData(UITemplateShopBlueprint uiTemplateShopBlueprint, UITemplateLevelBlueprint uiTemplateLevelBlueprint)
         {
-            this.LevelData     = new UITemplateLevelData(uiTemplateLevelBlueprint);
-            this.ShopData      = new UITemplateShopData(uiTemplateShopBlueprint);
-            this.InventoryData = new UITemplateInventoryData();
-            this.SettingData   = new UITemplateSettingData();
-        }
-
-        public void Init()
-        {
-            
+            this.LevelData       = new UITemplateLevelData(uiTemplateLevelBlueprint);
+            this.ShopData        = new UITemplateShopData(uiTemplateShopBlueprint);
+            this.InventoryData   = new UITemplateInventoryData();
+            this.SettingData     = new UITemplateSettingData();
+            this.DailyRewardData = new UITemplateDailyRewardData();
         }
     }
 }

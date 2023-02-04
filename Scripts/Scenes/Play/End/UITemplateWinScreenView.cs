66610534f9599e85f946eb5af3fcf05844aaf93f
ducyb294 <ducyb2942@gmail.com>
2023-02-04 16:01:44 +0700

    Add win game animation and button animation

--- Scripts/Scenes/Play/End/UITemplateWinScreenView.cs
+++ Scripts/Scenes/Play/End/UITemplateWinScreenView.cs
@@ -1,11 +1,15 @@
 namespace UITemplate.Scripts.Scenes.Play.End
 {
+    using System;
+    using DG.Tweening;
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.View;
     using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
     using GameFoundation.Scripts.Utilities.LogService;
     using UITemplate.Scripts.Scenes.Main;
     using UITemplate.Scripts.Scenes.Popups;
+    using UniRx;
+    using UnityEngine;
     using UnityEngine.UI;
     using Zenject;
 
@@ -22,13 +26,23 @@ namespace UITemplate.Scripts.Scenes.Play.End
         public Button                 NextEndgameButton;
         public UITemplateCurrencyView CoinText;
         public UITemplateStarRateView StarRateView;
+        public Image                  LightGlowImage;
     }
 
     [ScreenInfo(nameof(UITemplateWinScreenView))]
     public class UITemplateWinScreenPresenter : BaseScreenPresenter<UITemplateWinScreenView, UITemplateWinScreenModel>
     {
-        private readonly IScreenManager screenManager;
-        public UITemplateWinScreenPresenter(SignalBus signalBus, ILogService logService, IScreenManager screenManager) : base(signalBus, logService) { this.screenManager = screenManager; }
+        private readonly IScreenManager                  screenManager;
+        private readonly UITemplateButtonAnimationHelper uiTemplateButtonAnimationHelper;
+        private          IDisposable                     spinDisposable;
+        private const    float                           glowSpinSpeed = -100f;
+        
+        public UITemplateWinScreenPresenter(SignalBus signalBus, ILogService logService, IScreenManager screenManager,
+            UITemplateButtonAnimationHelper uiTemplateButtonAnimationHelper) : base(signalBus, logService)
+        {
+            this.screenManager                    = screenManager;
+            this.uiTemplateButtonAnimationHelper = uiTemplateButtonAnimationHelper;
+        }
 
         protected override async void OnViewReady()
         {
@@ -43,15 +57,41 @@ namespace UITemplate.Scripts.Scenes.Play.End
         {
             this.View.CoinText.Subscribe(this.SignalBus);
             this.View.StarRateView.SetStarRate(this.Model.StarRate);
+            this.AnimGlow();
         }
 
-        private void OnClickHome()   { this.screenManager.OpenScreen<UITemplateHomeSimpleScreenPresenter>(); }
-        private void OnClickReplay() { }
-        private void OnClickNext()   { }
+        private void AnimGlow()
+        {
+            this.spinDisposable = Observable.EveryUpdate().Subscribe(_ => Spin());
+
+            void Spin()
+            {
+                var transform = this.View.LightGlowImage.transform;
+                var zAngle    = transform.eulerAngles.z + Time.deltaTime * glowSpinSpeed;
+                transform.eulerAngles = new Vector3(0, 0, zAngle);
+            }
+        }
+
+        private async void OnClickHome()
+        {
+            await this.uiTemplateButtonAnimationHelper.AnimationButton(this.View.HomeButton.transform);
+            this.screenManager.OpenScreen<UITemplateHomeSimpleScreenPresenter>();
+        }
+        private async void OnClickReplay()
+        {
+            await this.uiTemplateButtonAnimationHelper.AnimationButton(this.View.ReplayEndgameButton.transform);
+            this.screenManager.OpenScreen<UITemplateHomeSimpleScreenPresenter>();
+        }
+        private async void OnClickNext()
+        {
+            await this.uiTemplateButtonAnimationHelper.AnimationButton(this.View.NextEndgameButton.transform);
+            this.screenManager.OpenScreen<UITemplateHomeSimpleScreenPresenter>();
+        }
 
         public override void Dispose()
         {
             base.Dispose();
+            this.spinDisposable.Dispose();
             this.View.CoinText.Unsubscribe(this.SignalBus);
         }
     }
--- Scripts/Scenes/Utils/UITemplateStarRateView.cs
+++ Scripts/Scenes/Utils/UITemplateStarRateView.cs
@@ -1,19 +1,36 @@
 ﻿namespace UITemplate.Scripts.Scenes.Popups
 {
     using System.Collections.Generic;
+    using Cysharp.Threading.Tasks;
     using UnityEngine;
+    using DG.Tweening;
     
     public class UITemplateStarRateView : MonoBehaviour
     {
         [SerializeField] private List<GameObject> StarOnList;
         [SerializeField] private List<GameObject> StarOffList;
-
-        public void SetStarRate(int rate)
+        private const            float            timeAnimStar = 0.5f;
+        
+        public async UniTask SetStarRate(int rate)
         {
+            foreach (var starOn in this.StarOnList)
+            {
+                starOn.SetActive(false);
+                starOn.transform.localScale = Vector3.zero;
+            }
+            foreach (var starOff in this.StarOffList)
+            {
+                starOff.SetActive(true);
+            }
+            
             for (int i = 0; i < this.StarOnList.Count; i++)
             {
+                await UniTask.Delay(200);
                 bool isActive = i < rate;
                 this.StarOnList[i].SetActive(isActive);
+                this.StarOnList[i].transform.DORotate(new Vector3(0, 0, -360), timeAnimStar, RotateMode.FastBeyond360).SetEase(Ease.Linear);
+                this.StarOnList[i].transform.DOScale(Vector3.one, timeAnimStar).SetEase(Ease.OutBounce);
+                
                 this.StarOffList[i].SetActive(!isActive);
             }
         }

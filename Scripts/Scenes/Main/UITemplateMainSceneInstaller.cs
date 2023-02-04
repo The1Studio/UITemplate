805a227c6590c955ab635ac0e6a1667019aec3a2
ducyb294 <ducyb2942@gmail.com>
2023-02-04 16:01:44 +0700

    Add win game animation and button animation

--- Scripts/Scenes/Main/UITemplateMainSceneInstaller.cs
+++ Scripts/Scenes/Main/UITemplateMainSceneInstaller.cs
@@ -2,6 +2,7 @@ namespace UITemplate.Scripts.Scenes.Main
 {
     using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
     using GameFoundation.Scripts.UIModule.Utilities;
+    using UITemplate.Scripts.Scenes.Popups;
     using UITemplate.Scripts.Signals;
     using Zenject;
 
@@ -12,6 +13,7 @@ namespace UITemplate.Scripts.Scenes.Main
             base.InstallBindings();
             this.Container.InitScreenManually<UITemplateHomeSimpleScreenPresenter>();
             this.Container.DeclareSignal<UpdateCurrencySignal>();
+            this.Container.Bind<UITemplateButtonAnimationHelper>().AsCached().NonLazy();
         }
     }
 }

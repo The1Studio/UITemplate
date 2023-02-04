34a548ce88ed645b43aa8280a8dcae3a14dad11b
ducyb294 <ducyb2942@gmail.com>
2023-02-04 17:29:24 +0700

    Update extension

--- Scripts/Scenes/Utils/UITemplateStarRateView.cs
+++ Scripts/Scenes/Utils/UITemplateStarRateView.cs
@@ -1,16 +1,19 @@
 ﻿namespace UITemplate.Scripts.Scenes.Popups
 {
+    using System;
     using System.Collections.Generic;
     using Cysharp.Threading.Tasks;
-    using UnityEngine;
     using DG.Tweening;
-    
+    using UnityEngine;
+
     public class UITemplateStarRateView : MonoBehaviour
     {
         [SerializeField] private List<GameObject> StarOnList;
         [SerializeField] private List<GameObject> StarOffList;
-        private const            float            timeAnimStar = 0.5f;
-        
+        private const            float            timeAnimStar = 1.5f;
+        private const            float            timeDelay    = 0.3f;
+
+
         public async UniTask SetStarRate(int rate)
         {
             foreach (var starOn in this.StarOnList)
@@ -18,19 +21,20 @@
                 starOn.SetActive(false);
                 starOn.transform.localScale = Vector3.zero;
             }
+
             foreach (var starOff in this.StarOffList)
             {
                 starOff.SetActive(true);
             }
-            
+
             for (int i = 0; i < this.StarOnList.Count; i++)
             {
-                await UniTask.Delay(200);
+                await UniTask.Delay(TimeSpan.FromSeconds(timeDelay));
                 bool isActive = i < rate;
                 this.StarOnList[i].SetActive(isActive);
-                this.StarOnList[i].transform.DORotate(new Vector3(0, 0, -360), timeAnimStar, RotateMode.FastBeyond360).SetEase(Ease.Linear);
+                this.StarOnList[i].transform.DORotate(new Vector3(0, 0, -360), timeAnimStar, RotateMode.WorldAxisAdd).SetEase(Ease.OutBounce);
                 this.StarOnList[i].transform.DOScale(Vector3.one, timeAnimStar).SetEase(Ease.OutBounce);
-                
+
                 this.StarOffList[i].SetActive(!isActive);
             }
         }

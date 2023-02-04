7bd074309089b8107919c08aaf4bd9b5fdc34923
hailuu <hoanghai.luu.71@gmail.com>
2023-02-04 17:00:01 +0700

    complete get time online from api, init time when initialize

--- Scripts/Scenes/Loading/UITemplateLoadingSceneInstaller.cs
+++ Scripts/Scenes/Loading/UITemplateLoadingSceneInstaller.cs
@@ -1,14 +1,27 @@
 namespace UITemplate.Scripts.Scenes.Loading
 {
+    using BlueprintFlow.Signals;
     using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
     using GameFoundation.Scripts.UIModule.Utilities;
+    using LocalData;
+    using Utility;
+    using Zenject;
 
     public class UITemplateLoadingSceneInstaller : BaseSceneInstaller
     {
+        [Inject] private UserLocalData userLocalData;
+
         public override void InstallBindings()
         {
             base.InstallBindings();
+            this.Container.Bind<IOnlineTime>().To<WorldTimeAPI>().AsSingle().NonLazy();
+            this.Container.Resolve<SignalBus>().Subscribe<LoadBlueprintDataSucceedSignal>(this.OnLoadingBlueprintSuccess);
             this.Container.InitScreenManually<UITemplateLoadingScreenPresenter>();
+        }
+
+        private async void OnLoadingBlueprintSuccess()
+        {
+            
         }
     }
 }

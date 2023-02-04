a192d5eb548c865595eb47a7b21c256c2887497b
ducyb294 <ducyb2942@gmail.com>
2023-02-04 10:41:56 +0700

    Update some prefab

--- Scripts/Scenes/Main/UITemplateHomeSimpleScreenView.cs
+++ Scripts/Scenes/Main/UITemplateHomeSimpleScreenView.cs
@@ -10,10 +10,9 @@
 
     public class UITemplateHomeSimpleScreenView : BaseView
     {
-        public Button                      PlayButton;
-        public Button                      LevelButton;
-        public UITemplateCurrencyView      CoinText;
-        public UITemplateSettingButtonView SettingButtonView;
+        public Button                 PlayButton;
+        public Button                 LevelButton;
+        public UITemplateCurrencyView CoinText;
     }
 
     [ScreenInfo(nameof(UITemplateHomeSimpleScreenView))]
@@ -25,7 +24,7 @@
         private readonly IScreenManager screenManager;
 
         #endregion
-
+        
         public UITemplateHomeSimpleScreenPresenter(SignalBus signalBus, IScreenManager screenManager, DiContainer diContainer) : base(signalBus)
         {
             this.diContainer   = diContainer;

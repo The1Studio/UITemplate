7164dee8631975acd04c2ca72eee6754448349a8
ducyb294 <ducyb2942@gmail.com>
2023-02-08 15:11:28 +0700

    Tap to play logic

--- Scripts/Scenes/Main/UITemplateMainSceneInstaller.cs
+++ Scripts/Scenes/Main/UITemplateMainSceneInstaller.cs
@@ -2,13 +2,20 @@ namespace UITemplate.Scripts.Scenes.Main
 {
     using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
     using GameFoundation.Scripts.UIModule.Utilities;
+    using UITemplate.Scripts.Models;
+    using UITemplate.Scripts.Signals;
+    using Zenject;
 
     public class UITemplateMainSceneInstaller : BaseSceneInstaller
     {
         public override void InstallBindings()
         {
-            base.InstallBindings();
+            base.InstallBindings(); 
+            this.DeclareSignals();
+            this.Container.Bind<UITemplateSettingData>().AsSingle().NonLazy();
             this.Container.InitScreenManually<UITemplateHomeSimpleScreenPresenter>();
         }
+
+        private void DeclareSignals() { this.Container.DeclareSignal<UpdateCurrencySignal>(); }
     }
 }

92f006328450d78d31bb12b410b8150b1a2c0aee
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-02-24 10:38:15 +0700

    Fix UITemplate analytic handler

--- Scripts/Services/UITemplateAnalyticHandler.cs
+++ Scripts/Services/UITemplateAnalyticHandler.cs
@@ -1,9 +1,7 @@
 namespace TheOneStudio.UITemplate.UITemplate.Services
 {
     using System;
-    using System.Collections.Generic;
     using Core.AnalyticServices;
-    using Core.AnalyticServices.Data;
     using TheOneStudio.UITemplate.UITemplate.Models;
     using TheOneStudio.UITemplate.UITemplate.Signals;
     using TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents;
@@ -14,13 +12,13 @@ namespace TheOneStudio.UITemplate.UITemplate.Services
         #region inject
 
         private readonly SignalBus               signalBus;
-        private readonly List<IAnalyticServices> analyticServices;
+        private readonly IAnalyticServices       analyticServices;
         private readonly IAnalyticEventFactory   analyticEventFactory;
         private readonly UITemplateUserLevelData uiTemplateUserLevelData;
 
         #endregion
 
-        public UITemplateAnalyticHandler(SignalBus signalBus, List<IAnalyticServices> analyticServices, IAnalyticEventFactory analyticEventFactory, UITemplateUserLevelData uiTemplateUserLevelData)
+        public UITemplateAnalyticHandler(SignalBus signalBus, IAnalyticServices analyticServices, IAnalyticEventFactory analyticEventFactory, UITemplateUserLevelData uiTemplateUserLevelData)
         {
             this.signalBus               = signalBus;
             this.analyticServices        = analyticServices;
@@ -30,7 +28,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Services
 
         public void Initialize()
         {
-            this.analyticServices.ForEach(analytic => analytic.Start());
+            this.analyticServices.Start();
 
             this.signalBus.Subscribe<LevelStartedSignal>(this.LevelStartedHandler);
             this.signalBus.Subscribe<LevelEndedSignal>(this.LevelEndedHandler);
@@ -38,20 +36,22 @@ namespace TheOneStudio.UITemplate.UITemplate.Services
             this.signalBus.Subscribe<InterstitialAdShowedSignal>(this.InterstitialAdShowedHandler);
             this.signalBus.Subscribe<RewardedAdShowedSignal>(this.RewardedAdShowedHandler);
         }
-        private void RewardedAdShowedHandler(RewardedAdShowedSignal obj) { this.AnalyticTrack(this.analyticEventFactory.RewardedVideoShow(this.uiTemplateUserLevelData.CurrentLevel, obj.place)); }
+        private void RewardedAdShowedHandler(RewardedAdShowedSignal obj)
+        {
+            this.analyticServices.Track(this.analyticEventFactory.RewardedVideoShow(this.uiTemplateUserLevelData.CurrentLevel, obj.place));
+        }
         private void InterstitialAdShowedHandler(InterstitialAdShowedSignal obj)
         {
-            this.AnalyticTrack(this.analyticEventFactory.InterstitialShow(this.uiTemplateUserLevelData.CurrentLevel, obj.place));
+            this.analyticServices.Track(this.analyticEventFactory.InterstitialShow(this.uiTemplateUserLevelData.CurrentLevel, obj.place));
         }
 
-        private void AnalyticTrack(IEvent analyticEvent) { this.analyticServices.ForEach(analyticService => analyticService.Track(analyticEvent)); }
 
-        private void LevelSkippedHandler(LevelSkippedSignal obj) { this.AnalyticTrack(this.analyticEventFactory.LevelSkipped(obj.Level, obj.Time)); }
+        private void LevelSkippedHandler(LevelSkippedSignal obj) { this.analyticServices.Track(this.analyticEventFactory.LevelSkipped(obj.Level, obj.Time)); }
         private void LevelEndedHandler(LevelEndedSignal obj)
         {
-            this.AnalyticTrack(obj.IsWin ? this.analyticEventFactory.LevelWin(obj.Level, obj.Time) : this.analyticEventFactory.LevelLose(obj.Level, obj.Time));
+            this.analyticServices.Track(obj.IsWin ? this.analyticEventFactory.LevelWin(obj.Level, obj.Time) : this.analyticEventFactory.LevelLose(obj.Level, obj.Time));
         }
-        private void LevelStartedHandler(LevelStartedSignal obj) { this.AnalyticTrack(this.analyticEventFactory.LevelStart(obj.Level)); }
+        private void LevelStartedHandler(LevelStartedSignal obj) { this.analyticServices.Track(this.analyticEventFactory.LevelStart(obj.Level)); }
 
         public void Dispose()
         {

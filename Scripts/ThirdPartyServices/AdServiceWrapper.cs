5bfae932aba4733f552b41d375f95422abfc17f6
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-02-24 20:55:40 +0700

    Fix AOA bug

--- Scripts/ThirdPartyServices/AdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/AdServiceWrapper.cs
@@ -6,7 +6,7 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices
     using TheOneStudio.UITemplate.UITemplate.Signals;
     using Zenject;
 
-    public class AdServiceWrapper : IAdServices, IAOAAdService
+    public class AdServiceWrapper
     {
         #region inject
 
@@ -25,20 +25,14 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices
             this.signalBus    = signalBus;
         }
 
-        public void                                        GrantDataPrivacyConsent()                                                    { this.adServices.GrantDataPrivacyConsent(); }
-        public void                                        RevokeDataPrivacyConsent()                                                   { this.adServices.RevokeDataPrivacyConsent(); }
-        public void                                        GrantDataPrivacyConsent(AdNetwork adNetwork)                                 { this.adServices.GrantDataPrivacyConsent(adNetwork); }
-        public void                                        RevokeDataPrivacyConsent(AdNetwork adNetwork)                                { this.adServices.RevokeDataPrivacyConsent(adNetwork); }
-        public ConsentStatus                               GetDataPrivacyConsent(AdNetwork adNetwork)                                   { return this.adServices.GetDataPrivacyConsent(adNetwork); }
-        public void                                        ShowBannerAd(BannerAdsPosition bannerAdsPosition = BannerAdsPosition.Bottom) { this.adServices.ShowBannerAd(bannerAdsPosition); }
-        public void                                        HideBannedAd()                                                               { this.adServices.HideBannedAd(); }
-        public void                                        DestroyBannerAd()                                                            { this.adServices.DestroyBannerAd(); }
-        public event Action<InterstitialAdNetwork, string> InterstitialAdCompleted;
-        public bool                                        IsInterstitialAdReady() { return this.adServices.IsInterstitialAdReady(); }
+        public void ShowBannerAd()
+        {
+            this.adServices.ShowBannerAd();
+        }
 
         public void ShowInterstitialAd(string place)
         {
-            if (!this.IsInterstitialAdReady())
+            if (!this.adServices.IsInterstitialAdReady())
             {
                 this.logService.Warning("InterstitialAd was not loaded");
 
@@ -49,13 +43,9 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices
             this.adServices.ShowInterstitialAd(place);
         }
 
-        public event Action<InterstitialAdNetwork, string> RewardedAdCompleted;
-        public event Action<InterstitialAdNetwork, string> RewardedAdSkipped;
-        public bool                                        IsRewardedAdReady() { return this.adServices.IsRewardedAdReady(); }
-
-        public void ShowRewardedAd(string place)
+        public void ShowRewardedAd(string place, Action onComplete)
         {
-            if (!this.IsRewardedAdReady())
+            if (!this.adServices.IsRewardedAdReady())
             {
                 this.logService.Warning("Rewarded was not loaded");
 
@@ -63,24 +53,8 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices
             }
 
             this.signalBus.Fire(new RewardedAdShowedSignal(place));
-            this.adServices.ShowRewardedAd(place);
+            this.aoaAdService.OpenInterstitialAdHandler();
+            this.adServices.ShowRewardedAd(place, onComplete);
         }
-
-        public void                                        ShowRewardedAd(string place, Action onCompleted) { this.adServices.ShowRewardedAd(place, onCompleted); }
-        public event Action<InterstitialAdNetwork, string> RewardedInterstitialAdCompleted;
-        public event Action<InterstitialAdNetwork, string> RewardedInterstitialAdSkipped;
-        public bool                                        IsRewardedInterstitialAdReady()                              { return this.adServices.IsInterstitialAdReady(); }
-        public void                                        ShowRewardedInterstitialAd(string place)                     { this.adServices.ShowRewardedInterstitialAd(place); }
-        public void                                        ShowRewardedInterstitialAd(string place, Action onCompleted) { this.adServices.ShowRewardedInterstitialAd(place, onCompleted); }
-        public event Action                                AdsRemoved;
-        public void                                        RemoveAds(bool revokeConsent = false) { this.adServices.RemoveAds(revokeConsent); }
-        public bool                                        IsAdsInitialized()                    { return this.adServices.IsAdsInitialized(); }
-        public bool                                        IsRemoveAds()                         { return this.adServices.IsRemoveAds(); }
-
-        public void LoadAOAAd()
-        {
-            this.aoaAdService.LoadAOAAd();
-        }
-        public void ShowAdIfAvailable() { this.aoaAdService.ShowAdIfAvailable(); }
     }
 }

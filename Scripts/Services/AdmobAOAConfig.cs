af71cde51d2f70118abaf1451f6d9c0af14d64bd
hoanghai1308 <hoangthanhhai1308@gmail.com>
2023-02-28 16:51:26 +0700

    Add admob AOA config

---
+++ Scripts/Services/AdmobAOAConfig.cs
@@ -0,0 +1,14 @@
+namespace TheOneStudio.UITemplate.UITemplate.Scripts.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using global::Models;
+    using UnityEngine;
+
+    [Serializable]
+    [CreateAssetMenu(fileName = "AdmobAOAConfig", menuName = "Configs/AdmobAOAConfig", order = 0)]
+    public class AdmobAOAConfig : ScriptableObject, IGameConfig
+    {
+        public List<string> listAoaAppId = new();
+    }
+}

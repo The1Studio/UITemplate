983410d08e77494d22ec7f74abe5d784614da683
anhndq <quanganh01082002@gmail.com>
2023-03-04 11:22:17 +0700

    not using audio manager to mute sound

--- Scripts/Models/Controllers/UITemplateSettingDataController.cs
+++ Scripts/Models/Controllers/UITemplateSettingDataController.cs
@@ -7,26 +7,22 @@
         #region Inject
 
         private readonly  UITemplateUserSettingData uiTemplateUserSettingData;
-        private readonly IAudioManager             audioManager;
 
         #endregion
 
-        public UITemplateSettingDataController(UITemplateUserSettingData uiTemplateUserSettingData, IAudioManager audioManager)
+        public UITemplateSettingDataController(UITemplateUserSettingData uiTemplateUserSettingData)
         {
             this.uiTemplateUserSettingData = uiTemplateUserSettingData;
-            this.audioManager              = audioManager;
         }
 
         public void SetSoundOnOff()
         {
             this.uiTemplateUserSettingData.SoundValue.Value = this.IsSoundOn ? 0 : 1; 
-            this.audioManager.CheckToMuteSound(!this.IsSoundOn);
         }
 
         public void SetMusicOnOff()
         {
             this.uiTemplateUserSettingData.MusicValue.Value = this.IsMusicOn ? 0 : 1; 
-            this.audioManager.CheckToMuteMusic(!this.IsMusicOn);
         }
 
         public void SetVibrationOnOff() { this.uiTemplateUserSettingData.IsVibrationEnable = !this.uiTemplateUserSettingData.IsVibrationEnable; }

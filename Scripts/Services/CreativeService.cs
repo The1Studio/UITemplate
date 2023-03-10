6f23bb10382f487fc3bb2d92007d443a6938a87e
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-03-10 14:00:30 +0700

    UPdate creative service

--- Scripts/Services/CreativeService.cs
+++ Scripts/Services/CreativeService.cs
@@ -1,45 +1,24 @@
 ﻿namespace TheOneStudio.UITemplate.UITemplate.Scripts.Services
 {
-    using System;
     using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
     using UnityEngine;
     using Zenject;
 
-    public class CreativeService : IInitializable, ITickable
+    public class CreativeService : ITickable
     {
         private readonly IScreenManager screenManager;
 
-        private int      mouseDownCounter;
-        private DateTime lastMouseDownTime;
+        private int mouseDownCounter;
 
         public CreativeService(IScreenManager screenManager) { this.screenManager = screenManager; }
 
-        public void Initialize()
-        {
-            this.lastMouseDownTime = DateTime.Now;
-        }
-        
         public void Tick()
         {
             //TODO change this to more significant behaviour
             //Get three continuous mouse down in time
-            if (Input.GetMouseButtonDown(0))
+            if (Input.touchCount == 3)
             {
-                if ((DateTime.Now - this.lastMouseDownTime).TotalSeconds > 0.5f)
-                {
-                    this.mouseDownCounter = 1;
-                }
-                else
-                {
-                    this.mouseDownCounter++;
-                    if (this.mouseDownCounter == 3)
-                    {
-                        this.screenManager.RootUICanvas.gameObject.SetActive(!this.screenManager.RootUICanvas.gameObject.activeSelf);
-                        this.mouseDownCounter = 0;
-                    }
-                }
-
-                this.lastMouseDownTime = DateTime.Now;
+                this.screenManager.RootUICanvas.gameObject.SetActive(!this.screenManager.RootUICanvas.gameObject.activeSelf);
             }
         }
     }

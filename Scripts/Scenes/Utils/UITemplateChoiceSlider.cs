aab4bac5c435db3462007addac30a7890428a4a3
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-03-24 17:25:29 +0700

    add OnValueChanged event (#75)

--- Scripts/Scenes/Utils/UITemplateChoiceSlider.cs
+++ Scripts/Scenes/Utils/UITemplateChoiceSlider.cs
@@ -1,7 +1,9 @@
 namespace TheOneStudio.HyperCasual.Runtime.Common
 {
+    using System;
     using System.Collections.Generic;
     using DG.Tweening;
+    using DG.Tweening.Core;
     using UnityEngine;
     using UnityEngine.UI;
 
@@ -17,22 +19,53 @@ namespace TheOneStudio.HyperCasual.Runtime.Common
         [SerializeField]
         private float speed = 1f;
 
+        private int     lastChoiceIndex = -1;
+        private Tweener tween;
+
+        public Action<int> OnChoiceChanged { get; set; }
+
         private void Start()
         {
             // tween the slider from 0 to 1 and back to 0 looping forever
-            DOTween.To(() => this.slider.value, value => this.slider.value = value, 1f, this.speed)
-                   .SetEase(Ease.Linear)
-                   .SetLoops(-1, LoopType.Yoyo);
+            this.tween = DOTween.To(() => this.slider.value, value => this.slider.value = value, 1f, this.speed)
+                                .SetEase(Ease.Linear)
+                                .SetLoops(-1, LoopType.Yoyo);
         }
 
-#if UNITY_EDITOR
         private void LateUpdate()
         {
+#if UNITY_EDITOR
             var rect = this.slider.GetComponent<RectTransform>();
             rect.offsetMin = new Vector2(this.layoutGroup.padding.left, rect.offsetMin.y);
             rect.offsetMax = new Vector2(-this.layoutGroup.padding.right, rect.offsetMax.y);
-        }
 #endif
+            // Call OnChoiceChanged event when the slider value changes
+            if (this.OnChoiceChanged is not null)
+            {
+                var currentChoiceIndex = this.GetCurrentChoiceIndex();
+
+                if (currentChoiceIndex != this.lastChoiceIndex)
+                {
+                    this.OnChoiceChanged(currentChoiceIndex);
+                    this.lastChoiceIndex = currentChoiceIndex;
+                }
+            }
+        }
+
+        public void StartSlider()
+        {
+            this.tween.Play();
+        }
+
+        public void StopSlider()
+        {
+            this.tween.Pause();
+        }
+
+        public void RestartSlider()
+        {
+            this.tween.Restart();
+        }
 
         private int GetCurrentChoiceIndex()
         {

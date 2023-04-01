07870f5aee41b0893288151b5d9bf8cebc13365c
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-04-01 09:10:48 +0700

    UPdate leaderboard feature

--- Scripts/Scenes/Leaderboard/UITemplateLeaderboardPopupView.cs
+++ Scripts/Scenes/Leaderboard/UITemplateLeaderboardPopupView.cs
@@ -2,7 +2,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Scenes.Leaderboard
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
+    using System.Threading;
     using Cysharp.Threading.Tasks;
     using DG.Tweening;
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
@@ -36,7 +36,9 @@ namespace TheOneStudio.UITemplate.UITemplate.Scenes.Leaderboard
 
         #endregion
 
-        private GameObject yourClone;
+        private GameObject              yourClone;
+        private CancellationTokenSource animationCancelTokenSource;
+        private List<Tween>             animationTweenList = new();
 
         public UITemplateLeaderBoardPopupPresenter(SignalBus signalBus, DiContainer diContainer, UITemplateLevelDataController uiTemplateLevelDataController) : base(signalBus)
         {
@@ -61,10 +63,10 @@ namespace TheOneStudio.UITemplate.UITemplate.Scenes.Leaderboard
             var TestList = new List<UITemplateLeaderboardItemModel>();
 
             var currentLevel = this.uiTemplateLevelDataController.GetCurrentLevelData.Level;
-            var oldRank  = this.GetRankWithLevel(currentLevel - 1);
-            var newRank  = this.GetRankWithLevel(currentLevel);
-            var newIndex = indexPadding;
-            var oldIndex = (oldRank - newRank - indexPadding);
+            var oldRank      = this.GetRankWithLevel(currentLevel - 1);
+            var newRank      = this.GetRankWithLevel(currentLevel);
+            var newIndex     = indexPadding;
+            var oldIndex     = (oldRank - newRank - indexPadding);
 
             for (var i = newRank - indexPadding; i < oldRank + indexPadding; i++)
             {
@@ -76,28 +78,39 @@ namespace TheOneStudio.UITemplate.UITemplate.Scenes.Leaderboard
             TestList[oldIndex].CountryFlag = this.View.CountryFlags.GetLocalDeviceFlagByDeviceLang();
             TestList[oldIndex].Name        = "You";
 
+            //Setup view
             await this.View.Adapter.InitItemAdapter(TestList, this.diContainer);
             this.View.Adapter.ScrollTo(oldIndex - indexPadding);
+            
+            //Create your clone
             this.yourClone                                   = Object.Instantiate(this.View.Adapter.GetItemViewsHolderIfVisible(oldIndex).root.gameObject, this.View.YourRankerParentTransform);
             this.yourClone.GetComponent<CanvasGroup>().alpha = 1;
             var cloneView = this.yourClone.GetComponent<UITemplateLeaderboardItemView>();
-
-            this.yourClone.transform.DOScale(Vector3.one * 1.1f, scaleTime).SetEase(Ease.InOutBack);
-            await UniTask.Delay(TimeSpan.FromSeconds(scaleTime));
-            DOTween.To(() => oldRank, setValue => cloneView.SetRank(setValue), newRank, scrollDuration);
+            
+            this.animationCancelTokenSource = new CancellationTokenSource();
+            //Do animation
+            //Do scale up
+            this.animationTweenList.Add(this.yourClone.transform.DOScale(Vector3.one * 1.1f, scaleTime).SetEase(Ease.InOutBack));
+            await UniTask.Delay(TimeSpan.FromSeconds(scaleTime), cancellationToken: this.animationCancelTokenSource.Token);
+            //Do move to new rank
+            this.animationTweenList.Add(DOTween.To(() => oldRank, setValue => cloneView.SetRank(setValue), newRank, scrollDuration));
             this.View.Adapter.SmoothScrollTo(newIndex - indexPadding, scrollDuration);
-            await UniTask.Delay(TimeSpan.FromSeconds(scrollDuration));
-            if (this.yourClone != null)
-            {
-                this.yourClone.transform.DOScale(Vector3.one, scaleTime).SetEase(Ease.InOutBack);
-            }
+            await UniTask.Delay(TimeSpan.FromSeconds(scrollDuration), cancellationToken: this.animationCancelTokenSource.Token);
+            //Do scale down
+            this.animationTweenList.Add(this.yourClone.transform.DOScale(Vector3.one, scaleTime).SetEase(Ease.InOutBack));
         }
 
         public override void Dispose()
         {
             base.Dispose();
+            this.animationCancelTokenSource.Cancel();
+            this.animationCancelTokenSource.Dispose();
+            this.View.Adapter.StopScrollingIfAny();
+            foreach (var tween in this.animationTweenList)
+            {
+                tween.Kill();
+            }
             Object.Destroy(this.yourClone);
-            this.yourClone = null;
         }
     }
 }

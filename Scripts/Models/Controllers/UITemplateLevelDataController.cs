a5a6170d76cbf547358223d0e45ed46086f904e1
qa0108 <100855723+qa0108@users.noreply.github.com>
2023-04-06 14:27:00 +0700

    calculate level reward progression (#145)
    * calculate level reward progression
    
    * add parameterless GetRewardProgress method
    
    * remove parameterless GetRewardProgress method

--- Scripts/Models/Controllers/UITemplateLevelDataController.cs
+++ Scripts/Models/Controllers/UITemplateLevelDataController.cs
@@ -4,6 +4,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
     using System.Collections.Generic;
     using System.Linq;
     using GameFoundation.Scripts.Utilities.Extension;
+    using Sirenix.Serialization;
     using TheOneStudio.UITemplate.UITemplate.Blueprints;
     using TheOneStudio.UITemplate.UITemplate.Signals;
     using Zenject;
@@ -12,18 +13,21 @@ namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
     {
         #region inject
 
-        private readonly UITemplateLevelBlueprint uiTemplateLevelBlueprint;
-        private readonly UITemplateUserLevelData  uiTemplateUserLevelData;
-        private readonly SignalBus                signalBus;
+        private readonly UITemplateLevelBlueprint          uiTemplateLevelBlueprint;
+        private readonly UITemplateUserLevelData           uiTemplateUserLevelData;
+        private readonly UITemplateInventoryDataController uiTemplateInventoryDataController;
+        private readonly SignalBus                         signalBus;
 
         #endregion
 
-        public UITemplateLevelDataController(UITemplateLevelBlueprint uiTemplateLevelBlueprint, UITemplateUserLevelData uiTemplateUserLevelData,
-            SignalBus signalBus)
+        private int lastUnlockRewardLevel;
+        
+        public UITemplateLevelDataController(UITemplateLevelBlueprint uiTemplateLevelBlueprint, UITemplateUserLevelData uiTemplateUserLevelData, UITemplateInventoryDataController uiTemplateInventoryDataController, SignalBus signalBus)
         {
-            this.uiTemplateLevelBlueprint = uiTemplateLevelBlueprint;
-            this.uiTemplateUserLevelData  = uiTemplateUserLevelData;
-            this.signalBus                = signalBus;
+            this.uiTemplateLevelBlueprint          = uiTemplateLevelBlueprint;
+            this.uiTemplateUserLevelData           = uiTemplateUserLevelData;
+            this.uiTemplateInventoryDataController = uiTemplateInventoryDataController;
+            this.signalBus                         = signalBus;
         }
 
         public List<LevelData> GetAllLevels() { return this.uiTemplateLevelBlueprint.Values.Select(levelRecord => this.GetLevelData(levelRecord.Level)).ToList(); }
@@ -45,13 +49,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
         public void PassCurrentLevel()
         {
             this.uiTemplateUserLevelData.SetLevelStatusByLevel(this.uiTemplateUserLevelData.CurrentLevel, LevelData.Status.Passed);
-            this.signalBus.Fire(new LevelEndedSignal
-            {
-                Level            = this.uiTemplateUserLevelData.CurrentLevel,
-                IsWin            = true,
-                Time             = 0,
-                CurrentIdToValue = null
-            });
+            this.signalBus.Fire(new LevelEndedSignal { Level = this.uiTemplateUserLevelData.CurrentLevel, IsWin = true, Time = 0, CurrentIdToValue = null });
             this.GoToNextLevel();
         }
 
@@ -59,11 +57,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
         {
             this.uiTemplateUserLevelData.SetLevelStatusByLevel(this.uiTemplateUserLevelData.CurrentLevel, LevelData.Status.Skipped);
 
-            this.signalBus.Fire(new LevelSkippedSignal
-            {
-                Level = this.uiTemplateUserLevelData.CurrentLevel,
-                Time  = 0
-            });
+            this.signalBus.Fire(new LevelSkippedSignal { Level = this.uiTemplateUserLevelData.CurrentLevel, Time = 0 });
 
             this.GoToNextLevel();
         }
@@ -82,13 +76,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
 
         public void IncreaseLoseCount(int level)
         {
-            this.signalBus.Fire(new LevelEndedSignal
-            {
-                Level            = level,
-                IsWin            = false,
-                Time             = 0,
-                CurrentIdToValue = null
-            });
+            this.signalBus.Fire(new LevelEndedSignal { Level = level, IsWin = false, Time = 0, CurrentIdToValue = null });
             this.GetLevelData(level).LoseCount++;
         }
 
@@ -104,6 +92,56 @@ namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
             var maxIndex = Math.Max(skippedLevelIndex, this.MaxLevel);
 
             return level == maxIndex + 1;
+        }
+        
+        public float GetRewardProgress(int level)
+        {
+            var levelUnlockReward = this.GetLevelUnlockReward(level);
+            if (levelUnlockReward < 0) return 0;
+
+            //update last unlock reward level
+            var temp = this.lastUnlockRewardLevel;
+            if(level == levelUnlockReward) this.UpdateLastUnlockRewardLevel(level);
+            
+            return (float)(level - temp) / (levelUnlockReward - temp);
+        }
+
+        public string GetRewardId(int currentLevel)
+        {
+            var levelUnlockReward = this.GetLevelUnlockReward(currentLevel);
+            
+            if (levelUnlockReward < 0) return null;
+            var rewardList = this.uiTemplateLevelBlueprint.GetDataById(levelUnlockReward).Rewards;
+
+            for (int i = 0; i < rewardList.Count; i++)
+            {
+                var status = this.uiTemplateInventoryDataController.GetItemData(rewardList[i]).CurrentStatus;
+                if (this.uiTemplateInventoryDataController.GetItemData(rewardList[i]).CurrentStatus == UITemplateItemData.Status.Unlocked) return rewardList[i];
+            }
+
+            return null;
+        }
+
+        public Dictionary<string, int> GetAltReward(int currentLevel)
+        {
+            var levelUnlockReward = this.GetLevelUnlockReward(currentLevel);
+
+            return levelUnlockReward < 0 ? null : this.uiTemplateLevelBlueprint.GetDataById(levelUnlockReward).AltReward;
+        }
+
+        private int GetLevelUnlockReward(int level)
+        {
+            for (int i = level; i < this.uiTemplateLevelBlueprint.Count; i++)
+            {
+                if (this.uiTemplateLevelBlueprint.GetDataById(i).Rewards.Count > 0) return i;
+            }
+
+            return -1;
+        }
+
+        private void UpdateLastUnlockRewardLevel(int level)
+        {
+            this.lastUnlockRewardLevel = level;
         }
     }
 }

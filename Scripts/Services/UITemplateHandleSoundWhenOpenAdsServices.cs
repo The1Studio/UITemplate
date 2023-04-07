1d6e83485e189bd8e284dec489c6f22778fd220d
hoanghai1308 <hoangthanhhai1308@gmail.com>
2023-04-07 13:43:35 +0700

    Listten AOA signal oepn/close

--- Scripts/Services/UITemplateHandleSoundWhenOpenAdsServices.cs
+++ Scripts/Services/UITemplateHandleSoundWhenOpenAdsServices.cs
@@ -1,31 +1,42 @@
 namespace TheOneStudio.UITemplate.UITemplate.Services
 {
+    using Core.AdsServices;
+    using Core.AdsServices.Signals;
     using GameFoundation.Scripts.Utilities;
-    using ServiceImplementation.AdsServices.Signal;
     using Zenject;
 
     public class UITemplateHandleSoundWhenOpenAdsServices
     {
         private readonly SignalBus     signalBus;
+        private readonly IAdServices   adServices;
         private readonly IAudioManager audioManager;
 
-        public UITemplateHandleSoundWhenOpenAdsServices(SignalBus signalBus, IAudioManager audioManager)
+        public UITemplateHandleSoundWhenOpenAdsServices(SignalBus signalBus, IAdServices adServices, IAudioManager audioManager)
         {
             this.signalBus    = signalBus;
+            this.adServices   = adServices;
             this.audioManager = audioManager;
-            this.signalBus.Subscribe<AppStateChangeSignal>(this.OnApplicationPause);
+            this.signalBus.Subscribe<RewardedAdDisplayedSignal>(this.OnDisplayRewardedAd);
+            this.signalBus.Subscribe<InterstitialAdDisplayedSignal>(this.OnInterstitialAdDisplayed);
+            this.signalBus.Subscribe<AppOpenFullScreenContentClosedSignal>(this.OnAppFullScreenContentClosed);
+            this.signalBus.Subscribe<AppOpenFullScreenContentOpenedSignal>(this.OnAppFullScreenContentOpened);
+            this.adServices.RewardedAdSkipped             += this.OnRewardedAdSkipped;
+            this.adServices.RewardedInterstitialAdSkipped += this.OnRewardedInterstitialAdSkipped;
+            this.adServices.InterstitialAdCompleted       += this.OnInterstitialAdCompleted;
         }
 
-        private void OnApplicationPause(AppStateChangeSignal obj)
-        {
-            if (obj.IsBackground)
-            {
-                this.audioManager.PauseEverything();
-            }
-            else
-            {
-                this.audioManager.ResumeEverything();
-            }
-        }
+        private void OnAppFullScreenContentOpened(AppOpenFullScreenContentOpenedSignal obj) { this.audioManager.PauseEverything(); }
+
+        private void OnAppFullScreenContentClosed(AppOpenFullScreenContentClosedSignal obj) { this.audioManager.ResumeEverything(); }
+
+        private void OnInterstitialAdCompleted(InterstitialAdNetwork arg1, string arg2) { this.audioManager.ResumeEverything(); }
+
+        private void OnRewardedInterstitialAdSkipped(InterstitialAdNetwork arg1, string arg2) { this.audioManager.ResumeEverything(); }
+
+        private void OnRewardedAdSkipped(RewardedAdNetwork arg1, string arg2) { this.audioManager.ResumeEverything(); }
+
+        private void OnInterstitialAdDisplayed(InterstitialAdDisplayedSignal obj) { this.audioManager.PauseEverything(); }
+
+        private void OnDisplayRewardedAd(RewardedAdDisplayedSignal obj) { this.audioManager.PauseEverything(); }
     }
 }

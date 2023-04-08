1341322d5b9ba010f953a213ef281940f87c2654
qa0108 <100855723+qa0108@users.noreply.github.com>
2023-04-08 14:25:19 +0700

    config admob aoa ad open app threshold (#155)

--- Scripts/Services/AdmobAOAConfig.cs
+++ Scripts/Services/AdmobAOAConfig.cs
@@ -10,9 +10,11 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.Services
     [CreateAssetMenu(fileName = "AdmobAOAConfig", menuName = "Configs/AdmobAOAConfig", order = 0)]
     public class AdmobAOAConfig : ScriptableObject, IGameConfig
     {
-        [SerializeField] private List<string> listAoaAppId    = new();
-        [SerializeField] private List<string> listAoaIOSAppId = new();
-
+        [SerializeField] private List<string> listAoaAppId             = new();
+        [SerializeField] private List<string> listAoaIOSAppId          = new();
+        
+        [SerializeField] private int          adMobAOAOpenAppThreshold = 5;
+        
         public List<AdViewPosition> listMRecAdViewPosition = new();
 
         [SerializeField] private List<string> listMRecAndroidId = new();
@@ -21,8 +23,9 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.Services
         [SerializeField] private List<string> listNativeAndroidId = new();
         [SerializeField] private List<string> listNativeIOSId     = new();
 
-        public List<string> ListAoaAppId => Application.platform is RuntimePlatform.Android or RuntimePlatform.WindowsEditor ? this.listAoaAppId : this.listAoaIOSAppId;
-        public List<string> ListMRecId   => Application.platform is RuntimePlatform.Android or RuntimePlatform.WindowsEditor ? this.listMRecAndroidId : this.listMRecIOSId;
-        public List<string> ListNativeId => Application.platform is RuntimePlatform.Android or RuntimePlatform.WindowsEditor ? this.listNativeAndroidId : this.listNativeIOSId;
+        public List<string> ListAoaAppId             => Application.platform is RuntimePlatform.Android or RuntimePlatform.WindowsEditor ? this.listAoaAppId : this.listAoaIOSAppId;
+        public List<string> ListMRecId               => Application.platform is RuntimePlatform.Android or RuntimePlatform.WindowsEditor ? this.listMRecAndroidId : this.listMRecIOSId;
+        public List<string> ListNativeId             => Application.platform is RuntimePlatform.Android or RuntimePlatform.WindowsEditor ? this.listNativeAndroidId : this.listNativeIOSId;
+        public int          AdMObAOAOpenAppThreshold => this.adMobAOAOpenAppThreshold;
     }
 }

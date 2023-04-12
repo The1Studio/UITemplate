8c0be81d6b6b6c720bf25a3d1a1222362b0f61e0
hoanghai1308 <hoangthanhhai1308@gmail.com>
2023-04-12 18:19:38 +0700

    Update ShopPackBlueprint

--- Scripts/Blueprints/UITemplateShopPackBlueprint.cs
+++ Scripts/Blueprints/UITemplateShopPackBlueprint.cs
@@ -2,6 +2,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Blueprints
 {
     using System.Collections.Generic;
     using BlueprintFlow.BlueprintReader;
+    using ServiceImplementation.IAPServices;
 
     [CsvHeaderKey("Id")]
     [BlueprintReader("UITemplateShopPack", true)]
@@ -11,21 +12,15 @@ namespace TheOneStudio.UITemplate.UITemplate.Blueprints
 
     public class ShopPackRecord
     {
-        public string    Id         { get; set; }
-        public string    PackName   { get; set; }
-        public List<int> PackValues { get; set; }
+        public string       Id          { get; set; }
+        public List<string> Platforms   { get; set; }
+        public string       PackName    { get; set; }
+        public bool         IsRemoveAds { get; set; }
+        public List<int>    PackValues  { get; set; }
 
         public ProductType ProductType  { get; set; }
         public string      ImageAddress { get; set; }
         public string      Name         { get; set; }
         public string      Description  { get; set; }
-    }
-
-    public enum ProductType
-    {
-        Consumable,
-        NonConsumable,
-        Subscription,
-        RemoveAds
     }
 }

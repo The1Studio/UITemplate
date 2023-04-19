02f989a438713d3fb818cce383bc33274f560507
Yuyu <62789396+quang2002@users.noreply.github.com>
2023-04-19 16:01:49 +0700

    add position and size to blueprint (#199)

--- Scripts/Blueprints/UITemplateDailyRewardBlueprint.cs
+++ Scripts/Blueprints/UITemplateDailyRewardBlueprint.cs
@@ -2,6 +2,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Blueprints
 {
     using System.Collections.Generic;
     using BlueprintFlow.BlueprintReader;
+    using UnityEngine;
 
     [CsvHeaderKey("Day")]
     [BlueprintReader("UITemplateDailyReward", true)]
@@ -14,5 +15,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Blueprints
         public int                     Day         { get; set; }
         public Dictionary<string, int> Reward      { get; set; }
         public string                  RewardImage { get; set; }
+        public Vector2?                Position    { get; set; }
+        public Vector2?                Size        { get; set; }
     }
 }

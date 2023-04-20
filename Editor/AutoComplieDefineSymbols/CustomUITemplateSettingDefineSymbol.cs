daaf636d8c0d43fc5eed068986e7cf043ac42c10
hoangthanhhai1308 <hoangthanhhai1308@gmail.com>
2023-04-20 23:38:10 +0700

    Update Auto refresh define symbols

--- Editor/AutoComplieDefineSymbols/CustomUITemplateSettingDefineSymbol.cs
+++ Editor/AutoComplieDefineSymbols/CustomUITemplateSettingDefineSymbol.cs
@@ -10,24 +10,16 @@ namespace UITemplate.Editor.AutoComplieDefineSymbols
     [CustomEditor(typeof(UITemplateSettingDefineSymbol))]
     public class CustomUITemplateSettingDefineSymbol : Editor
     {
-        private int                      Selected = 0;
-        private AnimBool                 customizeValues;
-        private Dictionary<string, bool> fieldDictionary = new();
+        private int      selected = 0;
+        private AnimBool customizeValues;
 
         private void OnEnable()
         {
             var script    = UITemplateSettingDefineSymbol.Instance;
             var fieldInfo = script.Partner.GetType().GetFields();
-            this.fieldDictionary.Clear();
 
             this.customizeValues = new AnimBool(script.IsEnable);
-            this.Selected        = fieldInfo.ToList().FindIndex(x => (bool)x.GetValue(script.Partner));
-
-            this.InitOriginalData(script.AnalyticAndTracking.GetType().GetFields(), script.AnalyticAndTracking);
-            this.InitOriginalData(script.Monetization.GetType().GetFields(), script.Monetization);
-            this.InitOriginalData(script.UITemplateGameAndServices.GetType().GetFields(), script.UITemplateGameAndServices);
-            this.InitOriginalData(script.Partner.GetType().GetFields(), script.Partner);
-            this.InitOriginalData(script.CustomDefineSymbols.GetType().GetFields(), script.CustomDefineSymbols);
+            this.selected        = fieldInfo.ToList().FindIndex(x => (bool)x.GetValue(script.Partner));
 
             this.customizeValues.valueChanged.AddListener(() =>
             {
@@ -38,56 +30,32 @@ namespace UITemplate.Editor.AutoComplieDefineSymbols
 
         public override void OnInspectorGUI()
         {
-            base.OnInspectorGUI();
             var script = UITemplateSettingDefineSymbol.Instance;
 
-            this.customizeValues.target = EditorGUILayout.ToggleLeft("Allow Auto Setting Define Symbols", this.customizeValues.target);
-
-            if (EditorGUILayout.BeginFadeGroup(this.customizeValues.faded))
+            var style = new GUIStyle
             {
-                EditorGUI.indentLevel++;
-                EditorGUILayout.LabelField("Analytic And Tracking", GUILayout.Height(30));
+                normal =
+                {
+                    textColor = Color.green
+                },
+                fontSize  = 18,
+                fontStyle = FontStyle.Bold
+            };
+
+            this.customizeValues.target = EditorGUILayout.ToggleLeft("ENABLE", this.customizeValues.target, style);
+
+           
+
+            if (this.customizeValues.target)
+            {
+                base.OnInspectorGUI();
                 EditorGUILayout.Space(5);
-                this.SetDataForField(script.AnalyticAndTracking.GetType().GetFields(), script.AnalyticAndTracking);
-                EditorGUILayout.Space(5);
-                EditorGUILayout.LabelField("Monetization", GUILayout.Height(30));
-                this.SetDataForField(script.Monetization.GetType().GetFields(), script.Monetization);
-                EditorGUILayout.Space(5);
-                EditorGUILayout.LabelField("UITemplateGameAndServices", GUILayout.Height(30));
-                this.SetDataForField(script.UITemplateGameAndServices.GetType().GetFields(), script.UITemplateGameAndServices);
-                EditorGUILayout.Space(5);
-                EditorGUILayout.LabelField("CustomDefineSymbol", GUILayout.Height(30));
                 this.CustomDropDownPartner(script);
-                EditorGUILayout.Space(5);
-
-                EditorGUI.indentLevel--;
             }
-
-            EditorGUILayout.EndFadeGroup();
 
             if (GUILayout.Button("Manual Refresh Editor", GUILayout.Height(50)))
             {
                 script.Apply();
-            }
-        }
-
-        private void InitOriginalData(FieldInfo[] propertyInfos, object objectContain)
-        {
-            foreach (var p in propertyInfos)
-            {
-                var isEnable   = (bool)p.GetValue(objectContain);
-                var headerName = p.GetCustomAttribute<HeaderAttribute>().header;
-                this.fieldDictionary.Add(headerName, isEnable);
-            }
-        }
-
-        private void SetDataForField(FieldInfo[] propertyInfos, object objectContain)
-        {
-            foreach (var p in propertyInfos)
-            {
-                //var isEnable   = (bool)p.GetValue(objectContain);
-                var headerName = p.GetCustomAttribute<HeaderAttribute>().header;
-                this.fieldDictionary[headerName] = EditorGUILayout.Toggle(headerName, this.fieldDictionary[headerName]);
             }
         }
 
@@ -100,13 +68,27 @@ namespace UITemplate.Editor.AutoComplieDefineSymbols
             for (var index = 0; index < fieldInfo.Length; index++)
             {
                 var p        = fieldInfo[index];
-                var isEnable = this.Selected == index;
+                var isEnable = this.selected == index;
                 p.SetValue(script.Partner, isEnable);
                 var headerName = p.GetCustomAttribute<HeaderAttribute>().header;
                 options.Add($"{headerName}");
             }
 
-            this.Selected = EditorGUILayout.Popup("Partners", this.Selected, options.ToArray());
+            var style = new GUIStyle
+            {
+                normal =
+                {
+                    textColor = Color.cyan
+                },
+                fontSize  = 15,
+                fontStyle = FontStyle.Bold
+            };
+
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("PARTNERS", style);
+
+            this.selected = EditorGUILayout.Popup(this.selected, options.ToArray(), GUILayout.Height(30));
+            EditorGUILayout.EndHorizontal();
         }
     }
 }

5829ebc8b22d0c6b372fc403f720edbcdc8bcccc
gog1071 <goldendarkness1071@gmail.com>
2023-05-09 17:45:30 +0700

    Fix Interactable

--- Scripts/Scenes/Main/Level/UITemplateLevelItemView.cs
+++ Scripts/Scenes/Main/Level/UITemplateLevelItemView.cs
@@ -35,7 +35,7 @@
             this.LevelText.text         = data.Level.ToString();
             this.BackgroundImage.sprite = this.GetStatusBackground(data.LevelStatus);
             if (data.Level == userLevelData.GetCurrentLevelData.Level) this.BackgroundImage.sprite = this.NowSprite;
-            this.LevelButton.interactable = data.Level <= userLevelData.MaxLevel;
+            this.LevelButton.interactable = data.LevelStatus != LevelData.Status.Locked;
         }
 
         private Sprite GetStatusBackground(LevelData.Status levelStatus)

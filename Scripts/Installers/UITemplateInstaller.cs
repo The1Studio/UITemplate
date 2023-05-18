252472ba93ca83bb74590226034a8731b06a33a8
Nguyễn Mạnh <75465287+frostbun@users.noreply.github.com>
2023-05-18 12:18:45 +0700

    Add FBInstantInstaller, auto bind all local data (#263)
    * Add FBInstantInstaller, auto bind all local data
    
    * .
    
    * fix link.xml

--- Scripts/Installers/UITemplateInstaller.cs
+++ Scripts/Installers/UITemplateInstaller.cs
@@ -1,19 +1,19 @@
 namespace TheOneStudio.UITemplate.UITemplate.Installers
 {
-    using System.Collections.Generic;
     using System.Linq;
     using GameFoundation.Scripts.Utilities.LogService;
+    using ServiceImplementation.FBInstant;
     using ServiceImplementation.FireBaseRemoteConfig;
     using ServiceImplementation.IAPServices;
     using TheOneStudio.UITemplate.UITemplate.Extension;
     using TheOneStudio.UITemplate.UITemplate.FTUE;
-    using TheOneStudio.UITemplate.UITemplate.Interfaces;
-    using TheOneStudio.UITemplate.UITemplate.Scenes.Main.Decoration;
-    using TheOneStudio.UITemplate.UITemplate.Scripts.Signals;
     using TheOneStudio.UITemplate.UITemplate.Services;
     using TheOneStudio.UITemplate.UITemplate.Services.Toast;
     using UnityEngine;
     using Zenject;
+#if UITEMPLATE_DECORATION
+    using TheOneStudio.UITemplate.UITemplate.Scenes.Main.Decoration
+#endif
 
     public class UITemplateInstaller : Installer<GameObject, ToastController, UITemplateInstaller>
     {
@@ -33,11 +33,12 @@ namespace TheOneStudio.UITemplate.UITemplate.Installers
             this.Container.Bind<UITemplateAnimationHelper>().AsCached();
 
             UITemplateDeclareSignalInstaller.Install(this.Container);
-            UITemplateLocalDataInstaller.Install(this.Container);
             UnityIapInstaller.Install(this.Container);
             FTUEInstaller.Install(this.Container);
             UITemplateServicesInstaller.Install(this.Container, this.soundGroupPrefab, this.toastCanvas);
-            UITemplateThirdPartyInstaller.Install(this.Container);
+            FBInstantInstaller.Install(this.Container); // depend on UITemplateThirdPartyInstaller for signals
+            UITemplateLocalDataInstaller.Install(this.Container); // bind after FBInstantInstaller for remote user data
+            UITemplateThirdPartyInstaller.Install(this.Container); // bind after UITemplateLocalDataInstaller for local data analytics
             UITemplateAdsInstaller.Install(this.Container); // this depend on third party service signals
 
             this.EnableReporterWithDeVicesID();

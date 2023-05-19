b65ae4d2933b2d4d826f7bd5b9fd94ddf03eec44
Nguyễn Mạnh <75465287+frostbun@users.noreply.github.com>
2023-05-19 16:14:34 +0700

    add UserDataManager (#267)
    * load all user data simultaneously
    
    * add UserDataManager
    user data now will bind with empty data & will be loaded in loading screen

--- Scripts/Installers/UITemplateLocalDataInstaller.cs
+++ Scripts/Installers/UITemplateLocalDataInstaller.cs
@@ -1,50 +1,50 @@
 namespace TheOneStudio.UITemplate.UITemplate.Installers
 {
     using System;
-    using Cysharp.Threading.Tasks;
     using GameFoundation.Scripts.Interfaces;
     using GameFoundation.Scripts.Utilities.Extension;
     using GameFoundation.Scripts.Utilities.LogService;
-    using GameFoundation.Scripts.Utilities.UserData;
     using ModestTree;
+    using Sirenix.Utilities;
     using TheOneStudio.UITemplate.UITemplate.Models.Controllers;
     using TheOneStudio.UITemplate.UITemplate.Models.LocalDatas;
+    using TheOneStudio.UITemplate.UITemplate.UserData;
     using Zenject;
 
     public class UITemplateLocalDataInstaller : Installer<UITemplateLocalDataInstaller>
     {
         public override void InstallBindings()
         {
-            this.BindLocalData().ContinueWith(this.BindAllController).Forget();
+            this.BindLocalData();
+            this.BindAllController();
         }
 
-        private async UniTask BindLocalData()
+        private void BindLocalData()
         {
-            var logger            = this.Container.Resolve<ILogService>();
-            var handleDataService = this.Container.Resolve<IHandleUserDataServices>();
-            var listLocalData     = ReflectionUtils.GetAllDerivedTypes<ILocalData>();
+            var logger = this.Container.Resolve<ILogService>();
 
-            foreach (var localDataType in listLocalData)
+            ReflectionUtils.GetAllDerivedTypes<ILocalData>().ForEach(type =>
             {
-                var localData = await handleDataService.Load(localDataType);
-
-                if (localDataType.DerivesFrom<IUITemplateLocalData>())
+                var data = Activator.CreateInstance(type);
+                if (type.DerivesFrom<IUITemplateLocalData>())
                 {
-                    if (localDataType.GetProperty(nameof(IUITemplateLocalData.ControllerType))?.GetValue(localData) is Type controllerType)
+                    if (type.GetProperty(nameof(IUITemplateLocalData.ControllerType))?.GetValue(data) is Type controllerType)
                     {
-                        this.Container.Bind(localDataType).FromInstance(localData).WhenInjectedInto(controllerType);
+                        this.Container.Bind(type).FromInstance(data).WhenInjectedInto(controllerType);
                     }
                     else
                     {
-                        logger.Error($"Waring, the local data {localDataType.Name} has no controller, consider to create new controller");
-                        this.Container.Bind(localDataType).FromInstance(localData).AsCached();
+                        logger.Error($"Waring, the local data {type.Name} has no controller, consider to create new controller");
+                        this.Container.Bind(type).FromInstance(data).AsCached();
                     }
                 }
                 else
                 {
-                    this.Container.Bind(localDataType).FromInstance(localData).AsCached();
+                    this.Container.Bind(type).FromInstance(data).AsCached();
                 }
-            }
+            });
+
+            this.Container.Bind<UserDataManager>().AsCached();
         }
 
         private void BindAllController()
---
+++ Scripts/UserData/UserDataManager.cs
@@ -0,0 +1,49 @@
+namespace TheOneStudio.UITemplate.UITemplate.UserData
+{
+    using System;
+    using System.Linq;
+    using System.Reflection;
+    using GameFoundation.Scripts.Interfaces;
+    using GameFoundation.Scripts.Utilities.Extension;
+    using GameFoundation.Scripts.Utilities.UserData;
+    using Sirenix.Utilities;
+    using TheOneStudio.UITemplate.UITemplate.Models.LocalDatas;
+    using Zenject;
+
+    public class UserDataManager
+    {
+        private readonly DiContainer             container;
+        private readonly SignalBus               signalBus;
+        private readonly IHandleUserDataServices handleUserDataService;
+
+        public UserDataManager(DiContainer container, SignalBus signalBus, IHandleUserDataServices handleUserDataService)
+        {
+            this.container             = container;
+            this.signalBus             = signalBus;
+            this.handleUserDataService = handleUserDataService;
+        }
+
+        public async void LoadUserData()
+        {
+            var types = ReflectionUtils.GetAllDerivedTypes<ILocalData>().ToArray();
+            var datas = await this.handleUserDataService.Load(types);
+            Enumerable.Zip(types, datas, (type, data) => (type, data)).ForEach(p =>
+            {
+                var type = p.type;
+                var data = p.data;
+                if (type.GetProperty(nameof(IUITemplateLocalData.ControllerType))?.GetValue(data) is Type controllerType)
+                {
+                    data.CopyTo(controllerType.GetFields(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public)
+                                              .First(fieldInfo => fieldInfo.FieldType == type)
+                                              .GetValue(this.container.Resolve(controllerType))
+                    );
+                }
+                else
+                {
+                    data.CopyTo(this.container.Resolve(type) as ILocalData);
+                }
+            });
+            this.signalBus.Fire<UserDataLoadedSignal>();
+        }
+    }
+}

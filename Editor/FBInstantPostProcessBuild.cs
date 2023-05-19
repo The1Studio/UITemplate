27b8f7aeab31e1c76e3eb70bfb79b93864517702
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-05-19 11:30:24 +0700

    Implement fb instant post process

---
+++ Editor/FBInstantPostProcessBuild.cs
@@ -0,0 +1,22 @@
+#if UNITY_WEBGL
+using System.IO;
+using GameFoundation.BuildScripts.Runtime;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public static class FBInstantPostProcessBuild
+{
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target != BuildTarget.WebGL) return;
+        var webLoaderPath = Path.Combine(pathToBuiltProject, $"Build/{GameVersion.ProjectName}.loader.js");
+        var scriptContent = File.ReadAllText(webLoaderPath);
+
+        //  Facebook block the blob when UnityWebGL try to create, so it's needed to specify the relative path 
+        scriptContent = scriptContent.Replace("URL.createObjectURL(new Blob([e],{type:\"application/javascript\"}))", $"\"Build/{GameVersion.ProjectName}.framework.js\"");
+
+        File.WriteAllText(webLoaderPath, scriptContent);
+    }
+}
+#endif

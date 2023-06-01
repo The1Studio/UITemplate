4e391f2e5ec659eba02e22db686f2d6f17fb03a8
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-06-01 21:45:24 +0700

    Enable decompress fallback

--- Editor/FBInstantPostProcessBuild.cs
+++ Editor/FBInstantPostProcessBuild.cs
@@ -6,17 +6,17 @@ using UnityEditor.Callbacks;
 
 public static class FBInstantPostProcessBuild
 {
-    // [PostProcessBuild]
-    // public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
-    // {
-    //     if (target != BuildTarget.WebGL) return;
-    //     var webLoaderPath = Path.Combine(pathToBuiltProject, $"Build/{GameVersion.ProjectName}.loader.js");
-    //     var scriptContent = File.ReadAllText(webLoaderPath);
-    //
-    //     //  Facebook block the blob when UnityWebGL try to create, so it's needed to specify the relative path 
-    //     scriptContent = scriptContent.Replace("URL.createObjectURL(new Blob([e],{type:\"application/javascript\"}))", $"\"Build/{GameVersion.ProjectName}.framework.js\"");
-    //
-    //     File.WriteAllText(webLoaderPath, scriptContent);
-    // }
+    [PostProcessBuild]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        if (target != BuildTarget.WebGL) return;
+        var webLoaderPath = Path.Combine(pathToBuiltProject, $"Build/{GameVersion.ProjectName}.loader.js");
+        var scriptContent = File.ReadAllText(webLoaderPath);
+    
+        //  Facebook block the blob when UnityWebGL try to create, so it's needed to specify the relative path 
+        scriptContent = scriptContent.Replace("URL.createObjectURL(new Blob([e],{type:\"application/javascript\"}))", $"\"Build/{GameVersion.ProjectName}.framework.js\"");
+    
+        File.WriteAllText(webLoaderPath, scriptContent);
+    }
 }
 #endif

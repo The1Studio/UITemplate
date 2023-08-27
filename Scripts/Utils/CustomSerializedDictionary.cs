e9a3eb34dbbb2e23aef04cbcfea0b0c50cb4b79c
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-08-27 16:08:10 +0700

    Update custom dictionary

--- Scripts/Utils/CustomSerializedDictionary.cs
+++ Scripts/Utils/CustomSerializedDictionary.cs
@@ -1,6 +1,9 @@
 namespace TheOneStudio.UITemplate.UITemplate.Utils
 {
     using System;
+    using System.Collections.Generic;
+    using DG.Tweening;
+    using GameFoundation.Scripts.Utilities.Extension;
     using ServiceImplementation.Configs.CustomTypes;
     using TMPro;
     using UnityEngine.UI;
@@ -8,6 +11,29 @@ namespace TheOneStudio.UITemplate.UITemplate.Utils
     [Serializable]
     public class StringToTextDictionary : SerializableDictionary<string, TMP_Text>
     {
+        private Dictionary<string, int> currencyIdToRewardValue = new();
+
+        public void SetReward(Dictionary<string, int> finalValue, Func<int, string> contentFunc, float duration = 0.5f)
+        {
+            foreach (var (key, value) in finalValue)
+            {
+                var starValue = GetCurrentRewardValue(key);
+                DOTween.To(() => starValue, x => this[key].text = contentFunc.Invoke(x), value, duration);
+                this.currencyIdToRewardValue[key] = value;
+            }
+
+            return;
+
+            int GetCurrentRewardValue(string rewardKey)
+            {
+                return this.currencyIdToRewardValue.GetOrAdd(rewardKey, () => 0);
+            }
+        }
+
+        public void Reset()
+        {
+            this.currencyIdToRewardValue.Clear();
+        }
     }
     
     [Serializable]

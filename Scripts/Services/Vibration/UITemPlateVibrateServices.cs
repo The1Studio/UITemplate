4ec3a35941dda1b6b32cebfe0536fe24034843e0
phuongdmp7 <phuongdmp7@gmail.com>
2023-09-22 16:10:26 +0700

    check vibrate pref

--- Scripts/Services/Vibration/UITemPlateVibrateServices.cs
+++ Scripts/Services/Vibration/UITemPlateVibrateServices.cs
@@ -18,39 +18,42 @@ namespace TheOneStudio.UITemplate.UITemplate.Services.Vibration
         public UITemPlateVibrateServices(UITemplateSettingDataController uiTemplateSettingDataController)
         {
             this.uiTemplateSettingDataController = uiTemplateSettingDataController;
-            this.hapticsSupported = DeviceCapabilities.isVersionSupported;
+            this.hapticsSupported                = DeviceCapabilities.isVersionSupported;
         }
 
         private HapticPatterns.PresetType GetHapticPatternsPresetType(VibrationPresetType vibrationPresetType)
         {
             return vibrationPresetType switch
             {
-                VibrationPresetType.Selection => HapticPatterns.PresetType.Selection,
-                VibrationPresetType.Success => HapticPatterns.PresetType.Success,
-                VibrationPresetType.Warning => HapticPatterns.PresetType.Warning,
-                VibrationPresetType.Failure => HapticPatterns.PresetType.Failure,
-                VibrationPresetType.LightImpact => HapticPatterns.PresetType.LightImpact,
+                VibrationPresetType.Selection    => HapticPatterns.PresetType.Selection,
+                VibrationPresetType.Success      => HapticPatterns.PresetType.Success,
+                VibrationPresetType.Warning      => HapticPatterns.PresetType.Warning,
+                VibrationPresetType.Failure      => HapticPatterns.PresetType.Failure,
+                VibrationPresetType.LightImpact  => HapticPatterns.PresetType.LightImpact,
                 VibrationPresetType.MediumImpact => HapticPatterns.PresetType.MediumImpact,
-                VibrationPresetType.HeavyImpact => HapticPatterns.PresetType.HeavyImpact,
-                VibrationPresetType.RigidImpact => HapticPatterns.PresetType.RigidImpact,
-                VibrationPresetType.SoftImpact => HapticPatterns.PresetType.SoftImpact,
-                VibrationPresetType.None => HapticPatterns.PresetType.None,
-                _ => throw new ArgumentOutOfRangeException(nameof(vibrationPresetType), vibrationPresetType, null)
+                VibrationPresetType.HeavyImpact  => HapticPatterns.PresetType.HeavyImpact,
+                VibrationPresetType.RigidImpact  => HapticPatterns.PresetType.RigidImpact,
+                VibrationPresetType.SoftImpact   => HapticPatterns.PresetType.SoftImpact,
+                VibrationPresetType.None         => HapticPatterns.PresetType.None,
+                _                                => throw new ArgumentOutOfRangeException(nameof(vibrationPresetType), vibrationPresetType, null)
             };
         }
 
         public void PlayPresetType(VibrationPresetType vibrationPresetType)
         {
+            if (!this.uiTemplateSettingDataController.IsVibrationOn) return;
             HapticPatterns.PlayPreset(this.GetHapticPatternsPresetType(vibrationPresetType));
         }
-        
+
         public void PlayEmphasis(float amplitude, float frequency)
         {
+            if (!this.uiTemplateSettingDataController.IsVibrationOn) return;
             HapticPatterns.PlayEmphasis(amplitude, frequency);
         }
-        
+
         public void PlayConstant(float amplitude, float frequency, float duration)
         {
+            if (!this.uiTemplateSettingDataController.IsVibrationOn) return;
             HapticPatterns.PlayConstant(amplitude, frequency, duration);
         }
     }

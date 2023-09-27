d73255ae32ab4d96e4e1bb0aab7cc7d99b82de9c
HungNH <work.hung.nh@gmail.com>
2023-09-27 13:35:14 +0700

    add symbol crazylab

---
+++ Scripts/ThirdPartyServices/AnalyticEvents/Crazylab/CrazylabAnalyticEventFactory.cs
@@ -0,0 +1,16 @@
+#if CRAZYLAB
+namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Crazylab
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.AnalyticServices;
+    using TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents;
+    using Zenject;
+    public class CrazylabAnalyticEventFactory : BaseAnalyticEventFactory
+    {
+        public CrazylabAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
+        {
+        }
+    }
+}
+#endif

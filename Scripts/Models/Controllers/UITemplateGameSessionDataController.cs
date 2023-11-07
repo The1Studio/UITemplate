1629d966cf7f47964e788cab7213945e4fff2fa0
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-11-07 10:47:46 +0700

    Fix opentime bug

--- Scripts/Models/Controllers/UITemplateGameSessionDataController.cs
+++ Scripts/Models/Controllers/UITemplateGameSessionDataController.cs
@@ -1,19 +1,30 @@
 namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
 {
     using System;
+    using GameFoundation.Scripts.Utilities.UserData;
     using TheOneStudio.UITemplate.UITemplate.Models.LocalDatas;
     using Zenject;
 
     public class UITemplateGameSessionDataController : IUITemplateControllerData, IInitializable
     {
         private readonly UITemplateGameSessionData gameSessionData;
+        private readonly SignalBus                 signalBus;
 
-        public UITemplateGameSessionDataController(UITemplateGameSessionData gameSessionData) { this.gameSessionData = gameSessionData; }
+        public UITemplateGameSessionDataController(UITemplateGameSessionData gameSessionData, SignalBus signalBus)
+        {
+            this.gameSessionData = gameSessionData;
+            this.signalBus       = signalBus;
+        }
 
         public DateTime FirstInstallDate => this.gameSessionData.FirstInstallDate;
         public int OpenTime => this.gameSessionData.OpenTime;
         
         public void Initialize()
+        {
+            this.signalBus.Subscribe<UserDataLoadedSignal>(this.OnUserDataLoadedHandler);
+        }
+        
+        private void OnUserDataLoadedHandler()
         {
             this.gameSessionData.OpenTime++;
         }

fa4fdbdc2d4314b1a0b326e68b2c4d090fc8d40d
phuongdmp7 <phuongdmp7@gmail.com>
2023-11-22 17:13:53 +0700

    update creative service

--- Scripts/Creative/NewCreativeService.cs
+++ Scripts/Creative/NewCreativeService.cs
@@ -13,7 +13,7 @@
 
         public void Initialize()
         {
-            var clickStream = Observable.EveryUpdate().Where(_ => Input.touchCount > 0);
+            var clickStream = Observable.EveryUpdate().Where(_ => Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began);
 
             clickStream.Buffer(clickStream.Throttle(TimeSpan.FromMilliseconds(250)))
                        .Where(xs => xs.Count >= 3)

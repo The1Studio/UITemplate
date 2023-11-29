547d6abe5c705a304eb034b7dc1c628f1abe650e
phuongdmp7 <phuongdmp7@gmail.com>
2023-11-29 18:04:51 +0700

    add-break-ads

---
+++ Scripts/Services/BreakAds/BreakAdsPopupView.cs
@@ -0,0 +1,34 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Services.BreakAds
+{
+    using Core.AdsServices.Signals;
+    using Cysharp.Threading.Tasks;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.View;
+    using Zenject;
+
+    public class BreakAdsPopupView : BaseView
+    {
+    }
+
+    [PopupInfo(nameof(BreakAdsPopupView))]
+    public class BreakAdsPopupPresenter : BasePopupPresenter<BreakAdsPopupView>
+    {
+        private readonly SignalBus signalBus;
+
+        public BreakAdsPopupPresenter(SignalBus signalBus) : base(signalBus) { this.signalBus = signalBus; }
+
+        public override UniTask BindData()
+        {
+            this.signalBus.Subscribe<InterstitialAdClosedSignal>(this.CloseView);
+            this.signalBus.Subscribe<InterstitialAdDisplayedFailedSignal>(this.CloseView);
+
+            return UniTask.CompletedTask;
+        }
+
+        public override void Dispose()
+        {
+            this.signalBus.Unsubscribe<InterstitialAdClosedSignal>(this.CloseView);
+            this.signalBus.Unsubscribe<InterstitialAdDisplayedFailedSignal>(this.CloseView);
+        }
+    }
+}
---
+++ Scripts/Services/BreakAds/BreakAdsServices.cs
@@ -0,0 +1,36 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Services.BreakAds
+{
+    using Core.AdsServices.Signals;
+    using Cysharp.Threading.Tasks;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
+    using ServiceImplementation.Configs;
+    using Zenject;
+
+    public class BreakAdsServices : IInitializable
+    {
+        #region inject
+
+        private readonly SignalBus          signalBus;
+        private readonly IScreenManager     screenManager;
+        private readonly ThirdPartiesConfig thirdPartiesConfig;
+
+        public BreakAdsServices(SignalBus signalBus, IScreenManager screenManager, ThirdPartiesConfig thirdPartiesConfig)
+        {
+            this.signalBus          = signalBus;
+            this.screenManager      = screenManager;
+            this.thirdPartiesConfig = thirdPartiesConfig;
+        }
+
+        #endregion
+
+        private BreakAdsPopupPresenter breakAdsPopupPresenter;
+
+        public void Initialize()
+        {
+            if (this.thirdPartiesConfig.AdSettings.EnableBreakAds)
+                this.signalBus.Subscribe<InterstitialAdCalledSignal>(this.OpenBreakAds);
+        }
+
+        private void OpenBreakAds() { this.screenManager.OpenScreen<BreakAdsPopupPresenter>().Forget(); }
+    }
+}

327cf56eaaf7d1ef7dbfdc7d2009b141d88f4636
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-12-08 18:26:55 +0700

    Show reward data in inspector

--- Scripts/Models/LocalDatas/UITemplateRewardData.cs
+++ Scripts/Models/LocalDatas/UITemplateRewardData.cs
@@ -3,21 +3,28 @@ namespace TheOneStudio.UITemplate.UITemplate.Models.LocalDatas
     using System;
     using System.Collections.Generic;
     using GameFoundation.Scripts.Interfaces;
+    using Sirenix.Serialization;
     using TheOneStudio.UITemplate.UITemplate.Models.Controllers;
 
     public class UITemplateRewardData : ILocalData, IUITemplateLocalData
     {
+        [OdinSerialize]
         public Dictionary<string, Dictionary<string, UITemplateRewardItemData>> PackIdToIdToRewardData { get; set; } = new();
 
         public void Init()         { }
         public Type ControllerType => typeof(UITemplateRewardDataController);
     }
 
+    [Serializable]
     public class UITemplateRewardItemData
     {
+        [OdinSerialize]
         public int      RewardValue           { get; set; }
+        [OdinSerialize]
         public int      Repeat                { get; set; }
+        [OdinSerialize]
         public string   AddressableFlyingItem { get; set; }
+        [OdinSerialize]
         public DateTime LastTimeReceive       { get; set; }
 
         public UITemplateRewardItemData(int rewardValue, int repeat, string addressableFlyingItem)

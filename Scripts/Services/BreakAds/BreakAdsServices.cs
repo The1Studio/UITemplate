7eb156879cde570085037e4d781e1e6490d6bf70
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-12-08 21:25:22 +0700

    Hot fix break ads

--- Scripts/Services/BreakAds/BreakAdsServices.cs
+++ Scripts/Services/BreakAds/BreakAdsServices.cs
@@ -4,16 +4,21 @@
     using Cysharp.Threading.Tasks;
     using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
     using ServiceImplementation.Configs;
+    using UnityEngine;
     using Zenject;
 
-    public class BreakAdsServices : IInitializable
+    public class BreakAdsServices : IInitializable, ITickable
     {
         #region inject
 
         protected readonly SignalBus          SignalBus;
         protected readonly IScreenManager     ScreenManager;
         protected readonly ThirdPartiesConfig ThirdPartiesConfig;
+        
+        #endregion
 
+        private float BreakAdsOnScreenTime;
+        
         public BreakAdsServices(SignalBus signalBus, IScreenManager screenManager, ThirdPartiesConfig thirdPartiesConfig)
         {
             this.SignalBus          = signalBus;
@@ -21,14 +26,28 @@
             this.ThirdPartiesConfig = thirdPartiesConfig;
         }
 
-        #endregion
-
         public void Initialize()
         {
             if (this.ThirdPartiesConfig.AdSettings.EnableBreakAds)
                 this.SignalBus.Subscribe<InterstitialAdCalledSignal>(this.OpenBreakAds);
         }
 
-        protected virtual void OpenBreakAds() { this.ScreenManager.OpenScreen<BreakAdsPopupPresenter>().Forget(); }
+        protected virtual void OpenBreakAds()
+        {
+            this.BreakAdsOnScreenTime = 0;
+            this.ScreenManager.OpenScreen<BreakAdsPopupPresenter>().Forget();
+        }
+        
+        public async void Tick()
+        {
+            while (this.ScreenManager.CurrentActiveScreen.Value is BreakAdsPopupPresenter)
+            {
+                this.BreakAdsOnScreenTime += Time.unscaledDeltaTime;
+                if (this.BreakAdsOnScreenTime >= 2f)
+                {
+                    await this.ScreenManager.CloseCurrentScreen();
+                }
+            }
+        }
     }
 }

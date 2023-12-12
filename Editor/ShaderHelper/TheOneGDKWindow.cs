1518aba4470dd8923b8fed4f647f48be402daf36
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-12-12 20:53:24 +0700

    Implemmetn game event manager

--- Editor/ShaderHelper/TheOneGDKWindow.cs
+++ Editor/ShaderHelper/TheOneGDKWindow.cs
@@ -2,51 +2,53 @@ namespace UITemplate.Editor.ShaderHelper
 {
     using Core.AnalyticServices;
     using ServiceImplementation.Configs;
+    using ServiceImplementation.Configs.GameEvents;
     using ServiceImplementation.FireBaseRemoteConfig;
     using Sirenix.OdinInspector;
     using Sirenix.OdinInspector.Editor;
     using UnityEditor;
     using UnityEngine;
-    using UnityEngine.Serialization;
 
     public class TheOneWindow : OdinEditorWindow
     {
         [MenuItem("TheOne/Configuration And Tools")]
         private static void OpenWindow() { GetWindow<TheOneWindow>().Show(); }
-        
-        [TabGroup("Analytics Config")]
-        [InlineEditor]
+
+        [TabGroup("Analytics Config")] [InlineEditor]
         public AnalyticConfig AnalyticConfig;
-        [TabGroup("Ads Config")]
-        [InlineEditor]
+
+        [TabGroup("Ads Config")] [InlineEditor]
         public ThirdPartiesConfig ThirdPartiesConfig;
-        
-        [FormerlySerializedAs("RemoteConfig")] [InlineEditor][TabGroup("Remote Config")]
+
+        [InlineEditor] [TabGroup("Remote Config")]
         public RemoteConfigSetting remoteConfigSetting;
+        
+        [InlineEditor] [TabGroup("Game Events")]
+        public GameEventsSetting GameEventsSetting;
 
         private void OnEnable()
         {
             this.AnalyticConfig      = Resources.Load<AnalyticConfig>($"GameConfigs/{nameof(AnalyticConfig)}");
             this.ThirdPartiesConfig  = Resources.Load<ThirdPartiesConfig>($"GameConfigs/{nameof(ThirdPartiesConfig)}");
             this.remoteConfigSetting = Resources.Load<RemoteConfigSetting>(RemoteConfigSetting.ResourcePath);
-            
+
             if (this.remoteConfigSetting == null)
             {
                 AssetDatabase.CreateAsset(CreateInstance<RemoteConfigSetting>(), $"Assets/Resources/{RemoteConfigSetting.ResourcePath}.asset");
                 this.remoteConfigSetting = Resources.Load<RemoteConfigSetting>($"GameConfigs/{nameof(RemoteConfigSetting)}");
             }
-            
+
             this.ThirdPartiesConfig.AdSettings.AdMob.OnDataChange = (admobSetting) =>
             {
                 EditorUtility.SetDirty(admobSetting);
                 AssetDatabase.SaveAssets();
             };
         }
-        
+
         protected override void OnGUI()
         {
             base.OnGUI();
-        
+
             // If you make changes and want to save them back to the asset (optional)
             if (GUILayout.Button("Save Changes"))
             {

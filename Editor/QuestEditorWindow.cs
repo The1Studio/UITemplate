1878844b7b4e8d61242d70775dcb891e1971eaef
Manh Nguyen <manhnb1911@gmail.com>
2023-12-13 11:29:04 +0700

    fix Quest Editor

--- Editor/QuestEditorWindow.cs
+++ Editor/QuestEditorWindow.cs
@@ -12,7 +12,7 @@ namespace TheOneStudio.HyperCasual.Editor
 
     public class QuestEditorWindow : OdinEditorWindow
     {
-        private const string DEFAULT_BLUEPRINT_PATH = "Assets/Resources/BlueprintData/UITemplateQuest.csv";
+        private const string BLUEPRINT_PATH = "Assets/Resources/BlueprintData/UITemplateQuest.csv";
 
         [MenuItem("TheOne/Quest Editor")]
         public static void Open()
@@ -20,32 +20,21 @@ namespace TheOneStudio.HyperCasual.Editor
             GetWindow<QuestEditorWindow>("Quest Editor");
         }
 
+        [SerializeField] private List<QuestRecord.Quest> quests;
+
         private void Awake()
         {
             this.Load();
         }
 
-        [SerializeField]
-        [InlineButton(nameof(Load))]
-        [InlineButton(nameof(Default))]
-        private string blueprintPath = DEFAULT_BLUEPRINT_PATH;
-
-        [SerializeField] private List<QuestRecord.Quest> quests;
-
-        private void Default()
-        {
-            this.blueprintPath = DEFAULT_BLUEPRINT_PATH;
-            this.Load();
-        }
-
         private void Load()
         {
-            if (!File.Exists(this.blueprintPath))
+            if (!File.Exists(BLUEPRINT_PATH))
             {
-                File.WriteAllText(this.blueprintPath, "Id,Record");
+                File.WriteAllText(BLUEPRINT_PATH, "Id,Record");
             }
             var       blueprint = new QuestBlueprint();
-            var       text      = File.ReadAllText(this.blueprintPath);
+            var       text      = File.ReadAllText(BLUEPRINT_PATH);
             using var reader    = CsvDataReader.Create(new StringReader(text));
             while (reader.Read()) blueprint.Add(reader);
             this.quests = blueprint.Values.Select(row => row.Record).ToList();
@@ -57,7 +46,7 @@ namespace TheOneStudio.HyperCasual.Editor
             var blueprint = new QuestBlueprint();
             this.quests.ForEach(quest => blueprint.Add(quest.Id, new QuestRecord(quest.Id, quest)));
             var text = blueprint.SerializeToRawData().Select(row => string.Join(",", row.Select(cell => $"\"{cell.Replace("\"", "\"\"")}\"")));
-            File.WriteAllLines(this.blueprintPath, text);
+            File.WriteAllLines(BLUEPRINT_PATH, text);
         }
     }
 }

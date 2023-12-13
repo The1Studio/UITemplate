bac35a5aae1feff8ecdf3b9896bcffb6fce2f04f
Hoàng Anh Tú <tuha263@gmail.com>
2023-12-13 08:59:47 +0700

    Update racing event

--- Scripts/Configs/GameEvents/GameEventsSetting.cs
+++ Scripts/Configs/GameEvents/GameEventsSetting.cs
@@ -1,12 +1,27 @@
 ﻿namespace TheOneStudio.UITemplate.UITemplate.Configs.GameEvents
 {
+    using ServiceImplementation.Configs.Editor;
+    using Sirenix.OdinInspector;
     using UnityEngine;
 
-    [CreateAssetMenu(fileName = nameof(GameEventsSetting), menuName = "TheOne/ScriptableObjects/SpawnGameEventSettingConfig", order = 1)]
+    [CreateAssetMenu(fileName = nameof(GameEventsSetting),
+        menuName = "TheOne/ScriptableObjects/SpawnGameEventSettingConfig", order = 1)]
     public class GameEventsSetting : ScriptableObject
     {
+        private const string RacingEventSymbol = "THEONE_RACING_EVENT";
+
         public static string ResourcePath = $"GameConfigs/{nameof(GameEventsSetting)}";
 
-        public        GameEventRacingConfig RacingConfig;
+        [OnValueChanged("OnChangeRacingEvent")]
+        public bool enableRacingEvent;
+
+        [SerializeField] [ShowIf("enableRacingEvent")] [BoxGroup("Racing Event")] public GameEventRacingConfig racingConfig;
+
+        public GameEventRacingConfig RacingConfig => this.racingConfig;
+
+        private void OnChangeRacingEvent()
+        {
+            DefineSymbolEditorUtils.SetDefineSymbol(RacingEventSymbol, this.enableRacingEvent);
+        }
     }
 }

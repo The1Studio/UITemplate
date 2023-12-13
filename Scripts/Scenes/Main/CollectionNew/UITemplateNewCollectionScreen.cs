5d1a1ea95fecdd2514792753720f87e001951f3b
PhatNN <107571396+ngocphat03@users.noreply.github.com>
2023-12-13 10:58:14 +0700

    fix use item when finish buy

--- Scripts/Scenes/Main/CollectionNew/UITemplateNewCollectionScreen.cs
+++ Scripts/Scenes/Main/CollectionNew/UITemplateNewCollectionScreen.cs
@@ -362,8 +362,8 @@ namespace TheOneStudio.UITemplate.UITemplate.Scenes.Main.CollectionNew
                 return;
             }
 
-            this.uiTemplateInventoryDataController.SetOwnedItemData(obj.ItemData, false);
-            this.OnSelectItem(obj);
+            this.uiTemplateInventoryDataController.SetOwnedItemData(obj.ItemData, true);
+            this.OnUseItem(obj);
         }
 
         #endregion

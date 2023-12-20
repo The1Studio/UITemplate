4fc6c99d72796619ee81945312d941136aac91e5
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-12-20 11:13:22 +0700

    Implement button

--- Scripts/Scenes/BottomBarNavigator/BottomBarNavigatorTabButtonView.cs
+++ Scripts/Scenes/BottomBarNavigator/BottomBarNavigatorTabButtonView.cs
@@ -1,5 +1,6 @@
 namespace TheOneStudio.UITemplate.UITemplate.Scenes.BottomBarNavigator
 {
+    using DG.Tweening;
     using TMPro;
     using UnityEngine;
     using UnityEngine.UI;
@@ -8,5 +9,12 @@ namespace TheOneStudio.UITemplate.UITemplate.Scenes.BottomBarNavigator
     {
         public Button   Button;
         public TMP_Text TabName;
+        public Image    TabIcon;
+
+        public void SetActive(bool isActive)
+        {
+            this.TabIcon.rectTransform.DOMoveY(-30, 0.3f).SetEase(Ease.InOutBack);
+            this.TabName.transform.DOScale(1.3f, 0.3f).SetEase(Ease.OutBounce);
+        }
     }
 }

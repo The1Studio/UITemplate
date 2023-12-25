ec1491880c8437eee5ebc5545deeb564641924b8
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-12-25 13:54:00 +0700

    First commit

---
+++ Scripts/Scenes/BadgeNotify/UItemplateBadgeNotifyView.cs
@@ -0,0 +1,31 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Scenes.BadgeNotify
+{
+    using UnityEngine;
+    using Zenject;
+
+    public class UItemplateBadgeNotifyView : MonoBehaviour
+    {
+        #region inject
+
+        private UITemplateBadgeNotifySystem uiTemplateBadgeNotifySystem;
+
+        #endregion
+
+        [SerializeField] private GameObject badge;
+        [SerializeField] private string     key;
+
+        [Inject]
+        private void Construct(UITemplateBadgeNotifySystem uiTemplateBadgeNotifySystem)
+        {
+            if (this.uiTemplateBadgeNotifySystem != null) return;
+
+            this.uiTemplateBadgeNotifySystem = uiTemplateBadgeNotifySystem;
+        }
+
+        public void SetActive(bool isActive) { this.badge.SetActive(isActive); }
+
+        public void Register(string parentKey) { this.uiTemplateBadgeNotifySystem.RegisterBadge(this, this.key, null); }
+        
+        public bool GetStatus() => this.badge.activeSelf;
+    }
+}

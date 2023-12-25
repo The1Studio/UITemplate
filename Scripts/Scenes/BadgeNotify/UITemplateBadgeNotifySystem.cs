a372fba44039df2996fe1725d21298298fa798d8
Tuha <88423816+tuhamm@users.noreply.github.com>
2023-12-25 13:54:00 +0700

    First commit

---
+++ Scripts/Scenes/BadgeNotify/UITemplateBadgeNotifySystem.cs
@@ -0,0 +1,22 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Scenes.BadgeNotify
+{
+    using System.Collections.Generic;
+
+    public class UITemplateBadgeNotifySystem
+    {
+        private Dictionary<string, UItemplateBadgeNotifyView> badgeKeyToView      = new();
+        private Dictionary<string, string>                    badgeKeyToParentKey = new();
+
+        public void RegisterBadge(UItemplateBadgeNotifyView badgeNotifyView, string key, string parentBadgeNotify)
+        {
+            this.badgeKeyToView.Add(key, badgeNotifyView);
+            this.badgeKeyToParentKey.Add(key, parentBadgeNotify);
+        }
+
+        public void UpdateBadgeNotifyStatus()
+        {
+        }
+        
+        
+    }
+}

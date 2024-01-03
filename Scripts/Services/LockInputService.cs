2aa8a3f929f8647c88d919190fd1d0384f6f5e5b
HungVM <86660856+hungdeptrai1311@users.noreply.github.com>
2024-01-03 03:28:10 -0800

    lock input service (#413)
    * lock input service
    
    * inject event system
    
    * inject event system

---
+++ Scripts/Services/LockInputService.cs
@@ -0,0 +1,41 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Services
+{
+    using System;
+    using Core.AdsServices.Signals;
+    using UnityEngine.EventSystems;
+    using Zenject;
+
+    public class LockInputService : IInitializable
+    {
+        #region Inject
+
+        private readonly SignalBus   signalBus;
+        private readonly EventSystem eventSystem;
+
+        public LockInputService(SignalBus signalBus, EventSystem eventSystem)
+        {
+            this.signalBus   = signalBus;
+            this.eventSystem = eventSystem;
+        }
+
+        #endregion
+
+
+        public void Initialize()
+        {
+            this.signalBus.Subscribe<InterstitialAdCalledSignal>(() => this.SetLockInput(false));
+            this.signalBus.Subscribe<InterstitialAdDisplayedFailedSignal>(() => this.SetLockInput(true));
+            this.signalBus.Subscribe<InterstitialAdClosedSignal>(() => this.SetLockInput(true));
+            
+            this.signalBus.Subscribe<RewardedAdCalledSignal>(() => this.SetLockInput(false));
+            this.signalBus.Subscribe<RewardedAdShowFailedSignal>(() => this.SetLockInput(true));
+            this.signalBus.Subscribe<RewardedAdClosedSignal>(() => this.SetLockInput(true));
+        }
+
+        private void SetLockInput(bool value)
+        {
+            if (this.eventSystem == null) return;
+            this.eventSystem.enabled = value;
+        }
+    }
+}

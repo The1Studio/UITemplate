350c9edcede5fbc58d71dee981f831b406a7d685
Lê Dũng <113307765+Leeduxng@users.noreply.github.com>
2024-01-15 02:26:17 +0700

    fix art 2d in blueprint

--- Scripts/Blueprints/UITemplateDailyRewardBlueprint.cs
+++ Scripts/Blueprints/UITemplateDailyRewardBlueprint.cs
@@ -19,5 +19,6 @@ namespace TheOneStudio.UITemplate.UITemplate.Blueprints
         public Vector2?                Size        { get; set; }
         public bool                    SpoilReward { get; set; }
         public bool                    ShowValue   { get; set; }
+        
     }
 }

373b4622873e9d3da60648ad780cf165702aa911
khai1412 <84337343+khai1412@users.noreply.github.com>
2024-01-15 19:03:52 +0700

    Khai/develop/update logic badge system (#434)
    * refactor badge system
    
    * update badge system
    
    * refactor
    
    * format code
    
    * refactor
    
    * refactor code
    
    * refactor code
    
    * refactor code
    
    * refactor code
    
    * rename function

---
+++ Scripts/Scenes/BadgeNotify/UITemplateBadgeNotifyView.cs
@@ -0,0 +1,10 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Scenes.BadgeNotify
+{
+    using UnityEngine;
+
+    public class UITemplateBadgeNotifyView : MonoBehaviour
+    {
+        public GameObject badge;
+        public string     badgeId;
+    }
+}

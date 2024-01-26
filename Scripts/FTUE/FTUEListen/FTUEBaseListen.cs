7a765dfd8a734caa2a87bd7355cd6af0d1973655
khai1412 <84337343+khai1412@users.noreply.github.com>
2024-01-26 17:25:55 +0700

    fix open screen logic

--- Scripts/FTUE/FTUEListen/FTUEBaseListen.cs
+++ Scripts/FTUE/FTUEListen/FTUEBaseListen.cs
@@ -1,26 +1,30 @@
 namespace TheOneStudio.UITemplate.UITemplate.FTUE.FTUEListen
 {
+    using Cysharp.Threading.Tasks;
     using TheOneStudio.UITemplate.UITemplate.Blueprints;
     using TheOneStudio.UITemplate.UITemplate.FTUE.Signal;
     using Zenject;
 
     public abstract class FTUEBaseListen : IInitializable
     {
-        protected readonly SignalBus               SignalBus;
-        protected readonly UITemplateFTUEBlueprint FtueBlueprint;
+        protected readonly SignalBus                SignalBus;
+        protected readonly UITemplateFTUEBlueprint  FtueBlueprint;
+        protected readonly UITemplateFTUEController ftueController;
 
-        protected FTUEBaseListen(SignalBus signalBus,UITemplateFTUEBlueprint ftueBlueprint)
+        protected FTUEBaseListen(SignalBus signalBus, UITemplateFTUEBlueprint ftueBlueprint, UITemplateFTUEController ftueController)
         {
-            this.SignalBus     = signalBus;
-            this.FtueBlueprint = ftueBlueprint;
+            this.SignalBus      = signalBus;
+            this.FtueBlueprint  = ftueBlueprint;
+            this.ftueController = ftueController;
         }
 
-        public             void Initialize() { this.InitInternal(); }
+        public void Initialize() { this.InitInternal(); }
+
         protected abstract void InitInternal();
-        
+
         protected void FireFtueTriggerSignal(string ftueId)
         {
-            this.SignalBus.Fire(new FTUETriggerSignal(ftueId));
+            UniTask.WaitUntil(() => !this.ftueController.ThereIsFTUEActive()).ContinueWith(() => this.SignalBus.Fire(new FTUETriggerSignal(ftueId)));
         }
     }
 }

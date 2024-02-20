704f0bd8ed6a6e8e5dc76f6d0202f7382e5524a2
Nguyễn Mạnh <75465287+frostbun@users.noreply.github.com>
2024-02-20 10:58:10 +0700

    add playfab leaderboard (#467)

---
+++ Scripts/Leaderboard/DictionaryExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace TheOneStudio.HyperCasual
+{
+    using System.Collections.Generic;
+
+    public static class DictionaryExtensions
+    {
+        public static TValue GetOrAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key) where TValue : new()
+        {
+            if (!dictionary.ContainsKey(key))
+            {
+                dictionary.Add(key, new TValue());
+            }
+            return dictionary[key];
+        }
+    }
+}

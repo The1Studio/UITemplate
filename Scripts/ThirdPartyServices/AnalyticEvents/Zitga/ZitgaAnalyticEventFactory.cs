86070919d352f86360351bd63f8630f2dc0fb282
Nguyen Dinh Quang Anh <100855723+qa0108@users.noreply.github.com>
2024-03-05 01:22:04 -0800

    add zitga analytic event factory (#480)
    Co-authored-by: Nguyen Dinh Quang Anh <anhndqhe161737@fpt.edu.vn>

---
+++ Scripts/ThirdPartyServices/AnalyticEvents/Zitga/ZitgaAnalyticEventFactory.cs
@@ -0,0 +1,16 @@
+﻿#if ZITGA
+
+namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Zitga
+{
+    using Core.AnalyticServices;
+    using Zenject;
+
+    public class ZitgaAnalyticEventFactory : BaseAnalyticEventFactory
+    {
+        public ZitgaAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
+        {
+        }
+    }
+}
+
+#endif

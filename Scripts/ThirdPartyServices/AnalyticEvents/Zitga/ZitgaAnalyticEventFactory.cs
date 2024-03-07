60e5de132e7370aca0568105186f9bbc34bd2e6d
Nguyen Dinh Quang Anh <100855723+qa0108@users.noreply.github.com>
2024-03-06 19:50:04 -0800

    update zitga event analytic factory (#484)
    Co-authored-by: Nguyen Dinh Quang Anh <anhndqhe161737@fpt.edu.vn>

--- Scripts/ThirdPartyServices/AnalyticEvents/Zitga/ZitgaAnalyticEventFactory.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/Zitga/ZitgaAnalyticEventFactory.cs
@@ -2,7 +2,11 @@
 
 namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Zitga
 {
+    using System;
+    using System.Collections.Generic;
     using Core.AnalyticServices;
+    using Core.AnalyticServices.CommonEvents;
+    using Core.AnalyticServices.Data;
     using Zenject;
 
     public class ZitgaAnalyticEventFactory : BaseAnalyticEventFactory
@@ -10,6 +14,31 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Z
         public ZitgaAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
         {
         }
+
+        public override AnalyticsEventCustomizationConfig AppsFlyerAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>()
+            {
+            },
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(FirstOpenEvent), "af_first_open" },
+                { nameof(AdsRevenueEvent), "af_ad_complete" }
+            }
+        };
+
+        public override AnalyticsEventCustomizationConfig FireBaseAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>()
+            {
+                typeof(GameStarted),
+                typeof(FirstOpenEvent),
+            },
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(AdsRevenueEvent), "ad_revenue_report" }
+            }
+        };
     }
 }
 

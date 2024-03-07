c358eb9df4c094603cd2b74d0bcc68c086d903fa
Nguyen Dinh Quang Anh <anhndqhe161737@fpt.edu.vn>
2024-03-07 10:37:06 +0700

    update zitga event analytic factory

---
+++ Scripts/ThirdPartyServices/AnalyticEvents/Zitga/FirstOpenEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Zitga
+{
+    using Core.AnalyticServices.Data;
+
+    public class FirstOpenEvent : IEvent
+    {
+        
+    }
+}

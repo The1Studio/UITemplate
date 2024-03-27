140179841803e33e660be2caba87daf81e94b59c
DuyMaiB <103514604+DuyMaiBui@users.noreply.github.com>
2024-03-27 13:47:53 +0700

    add config GameFeaturesSetting.cs (#498)
    Co-authored-by: zommpie-duymai <buiduymai2k@gmail.com>

--- Scripts/Configs/GameEvents/GameFeaturesSetting.cs
+++ Scripts/Configs/GameEvents/GameFeaturesSetting.cs
@@ -4,6 +4,8 @@
     using TheOneStudio.UITemplate.UITemplate.Services.Vibration;
     using UnityEngine;
 #if UNITY_EDITOR
+    using UnityEditor;
+    using System.IO;
     using ServiceImplementation.Configs.Editor;
 #endif
 
@@ -11,11 +13,12 @@
         menuName = "TheOne/ScriptableObjects/SpawnGameFeatruesSettingConfig", order = 1)]
     public class GameFeaturesSetting : ScriptableObject
     {
-        private const string RacingEventSymbol  = "THEONE_RACING_EVENT";
-        private const string DailyRewardSymbol  = "THEONE_DAILY_REWARD";
-        private const string BadgeNotifySymbol  = "THEONE_BADGE_NOTIFY";
-        private const string QuestSystemSymbol  = "THEONE_QUEST_SYSTEM";
-        private const string FireBaseAuthSymbol = "THEONE_FIREBASE_AUTH";
+        private const string RacingEventSymbol      = "THEONE_RACING_EVENT";
+        private const string DailyRewardSymbol      = "THEONE_DAILY_REWARD";
+        private const string BadgeNotifySymbol      = "THEONE_BADGE_NOTIFY";
+        private const string QuestSystemSymbol      = "THEONE_QUEST_SYSTEM";
+        private const string FireBaseAuthSymbol     = "THEONE_FIREBASE_AUTH";
+        private const string DailyQueueRewardSymbol = "THEONE_DAILY_QUEUE_REWARD";
 
         public static string ResourcePath = $"GameConfigs/{nameof(GameFeaturesSetting)}";
 
@@ -89,6 +92,13 @@
 
         #endregion
 
+        #region Daily Queue Reward
+
+        [OnValueChanged("OnChangeDailyQueueReward")] [BoxGroup("Daily Queue Reward")]
+        public bool enableDailyQueueReward;
+
+        #endregion
+
 #if UNITY_EDITOR
         private void OnChangeRacingEvent() { DefineSymbolEditorUtils.SetDefineSymbol(RacingEventSymbol, this.enableRacingEvent); }
 
@@ -97,9 +107,24 @@
         private void OnChangeBadgeNotify() { DefineSymbolEditorUtils.SetDefineSymbol(BadgeNotifySymbol, this.enableBadgeNotify); }
 
         private void OnChangeQuestSystem() { DefineSymbolEditorUtils.SetDefineSymbol(QuestSystemSymbol, this.enableQuestSystem); }
-        
+
         private void OnChangeFirebaseAuth() { DefineSymbolEditorUtils.SetDefineSymbol(FireBaseAuthSymbol, this.enableFirebaseAuth); }
 
+        private async void OnChangeDailyQueueReward()
+        {
+            const string filePath = "Assets/Resources/BlueprintData/UITemplateDailyQueueOffer.csv";
+            DefineSymbolEditorUtils.SetDefineSymbol(DailyQueueRewardSymbol, this.enableDailyQueueReward);
+
+            if (!this.enableDailyQueueReward) return;
+            if (File.Exists(filePath)) return;
+
+            await using (var writer = new StreamWriter(filePath, true))
+            {
+                await writer.WriteLineAsync("Day,OfferId,ItemId,ImageId,Value,IsRewardedAds");
+            }
+
+            AssetDatabase.Refresh();
+        }
 #endif
     }
 }

11be3e5f8bad2bc439d4079f1392a928f1ea8265
tuha <tuha263@gmail.com>
2024-04-02 17:27:02 +0700

    Implement remove ads popup

--- Scripts/Scenes/RemoveAds/UITemplateRemoveAdPopupView.cs
+++ Scripts/Scenes/RemoveAds/UITemplateRemoveAdPopupView.cs
@@ -1,22 +1,23 @@
 ﻿namespace TheOneStudio.UITemplate.UITemplate.Scenes.RemoveAdsBottomBar
 {
     using Cysharp.Threading.Tasks;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.View;
     using TheOneStudio.UITemplate.UITemplate.Scenes.Utils;
+    using TMPro;
+    using UnityEngine.UI;
     using Zenject;
 
     public class UITemplateRemoveAdPopupView : BaseView
     {
+        public TMP_Text priceText;
+        public Button   btnRemoveAds;
     }
-    
+
+    [PopupInfo(nameof(UITemplateRemoveAdPopupView))]
     public class UITemplateRemoveAdPopupPresenter : UITemplateBasePopupPresenter<UITemplateRemoveAdPopupView>
     {
-        public UITemplateRemoveAdPopupPresenter(SignalBus signalBus) : base(signalBus)
-        {
-        }
-        public override UniTask BindData()
-        {
-            return UniTask.CompletedTask;
-        }
+        public UITemplateRemoveAdPopupPresenter(SignalBus signalBus) : base(signalBus) { }
+        public override UniTask BindData() { return UniTask.CompletedTask; }
     }
 }

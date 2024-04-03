24ae3966c56d4ba1185b02437c31eb9c94cd0364
TungLV-GOG1071 <goldendarkness1071@gmail.com>
2024-04-03 11:38:50 +0700

    Done ads remove popup

---
+++ Scripts/Configs/GameEvents/IAPConfig.cs
@@ -0,0 +1,13 @@
+namespace TheOneStudio.UITemplate.UITemplate.Configs.GameEvents
+{
+    using System;
+    using System.Collections.Generic;
+
+    [Serializable]
+    public class IAPConfig
+    {
+        public string removeAdsProductId;
+        
+        public Dictionary<string,string> productIds;
+    }
+}
--- Scripts/Scenes/RemoveAds/UITemplateRemoveAdPopupView.cs
+++ Scripts/Scenes/RemoveAds/UITemplateRemoveAdPopupView.cs
@@ -3,7 +3,10 @@
     using Cysharp.Threading.Tasks;
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.View;
+    using TheOneStudio.UITemplate.UITemplate.Configs.GameEvents;
     using TheOneStudio.UITemplate.UITemplate.Scenes.Utils;
+    using TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices;
+    using TheOneStudio.UITemplate.UITemplate.Services;
     using TMPro;
     using UnityEngine.UI;
     using Zenject;
@@ -12,12 +15,47 @@
     {
         public TMP_Text priceText;
         public Button   btnRemoveAds;
+        public Button   btnClose;
     }
 
     [PopupInfo(nameof(UITemplateRemoveAdPopupView))]
     public class UITemplateRemoveAdPopupPresenter : UITemplateBasePopupPresenter<UITemplateRemoveAdPopupView>
     {
-        public UITemplateRemoveAdPopupPresenter(SignalBus signalBus) : base(signalBus) { }
-        public override UniTask BindData() { return UniTask.CompletedTask; }
+        private readonly UITemplateIapServices      uiTemplateIapServices;
+        private readonly GameFeaturesSetting        gameFeaturesSetting;
+        private readonly UITemplateAdServiceWrapper adServiceWrapper;
+
+        public UITemplateRemoveAdPopupPresenter(SignalBus signalBus, UITemplateIapServices uiTemplateIapServices, GameFeaturesSetting gameFeaturesSetting, UITemplateAdServiceWrapper adServiceWrapper)
+            : base(signalBus)
+        {
+            this.uiTemplateIapServices = uiTemplateIapServices;
+            this.gameFeaturesSetting   = gameFeaturesSetting;
+            this.adServiceWrapper      = adServiceWrapper;
+        }
+
+        protected override void OnViewReady()
+        {
+            base.OnViewReady();
+            this.View.btnRemoveAds.onClick.AddListener(this.OnRemoveAdsClicked);
+            this.View.btnClose.onClick.AddListener(this.CloseView);
+        }
+
+        private void OnRemoveAdsClicked()
+        {
+            this.uiTemplateIapServices.BuyProduct(
+                this.View.btnRemoveAds.gameObject,
+                this.gameFeaturesSetting.IAPConfig.removeAdsProductId,
+                (string msg) =>
+                {
+                    this.adServiceWrapper.RemoveAds();
+                    this.CloseView();
+                },
+                (string msg) => { });
+        }
+
+        public override UniTask BindData()
+        {
+            return UniTask.CompletedTask;
+        }
     }
 }

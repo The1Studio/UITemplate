8ed2dd4ddfd628d39ab53418bbd1dbbfee84a5b6
hungdeptrai1311 <vumanhhung.bc2017@gmail.com>
2024-04-12 10:03:53 +0700

    add camera utils

---
+++ Scripts/Utils/MainCameraUtils.cs
@@ -0,0 +1,34 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Utils
+{
+    using DG.Tweening;
+    using UnityEngine;
+
+    public static class MainCameraUtils
+    {
+        private static Camera mainCamera;
+
+        private static Camera GetMainCam
+        {
+            get
+            {
+                if (mainCamera == null) mainCamera = Camera.main;
+                return mainCamera;
+            }
+        }
+
+        public static void SetCameraPosition(Vector2 position) { GetMainCam.transform.position = new Vector3(position.x, position.y, -10); }
+
+        public static void SetOrthographicSize(float size) { GetMainCam.orthographicSize = size; }
+
+        public static void ZoomCamera(float targetSize, float duration, Ease easing) { GetMainCam.DOOrthoSize(targetSize, duration).SetEase(easing).SetAutoKill(true); }
+
+        public static void AdjustOrthographicHorizontalSize(float horizontalSize)
+        {
+            var unitsPerPixel    = horizontalSize / Screen.width;
+            var desireHalfHeight = 0.5f * unitsPerPixel * Screen.height;
+            SetOrthographicSize(desireHalfHeight);
+            SetCameraPosition(new Vector2(0, -1 - (float)1920 / Screen.height));
+        }
+        
+    }
+}

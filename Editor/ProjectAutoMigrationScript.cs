f3ee10d8dc6fb303eeacce8686f803f1d0ee085a
tuha <tuha263@gmail.com>
2024-05-18 13:21:09 +0700

    Update adaptive version

--- Editor/ProjectAutoMigrationScript.cs
+++ Editor/ProjectAutoMigrationScript.cs
@@ -35,6 +35,7 @@ namespace UITemplate.Editor
         
         private static readonly Dictionary<string, string> PackagesToAdd = new()
         {
+            {"com.unity.adaptiveperformance", "5.1.0"},
             {"com.unity.adaptiveperformance.samsung.android", "5.0.0"}
             // add more packages as needed
         };

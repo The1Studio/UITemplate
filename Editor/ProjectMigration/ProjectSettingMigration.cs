8ecca5ddb99c7aa1d69c71e58908df7ccb4efc30
tuha <tuha263@gmail.com>
2024-06-04 15:19:40 +0700

    Save assets

--- Editor/ProjectMigration/ProjectSettingMigration.cs
+++ Editor/ProjectMigration/ProjectSettingMigration.cs
@@ -9,16 +9,19 @@ namespace UITemplate.Editor.ProjectMigration
             if (PlayerSettings.GetApiCompatibilityLevel(BuildTargetGroup.Android) != ApiCompatibilityLevel.NET_Unity_4_8)
             {
                 PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Android, ApiCompatibilityLevel.NET_Unity_4_8);
+                AssetDatabase.SaveAssets();
             }
 
             if (PlayerSettings.GetApiCompatibilityLevel(BuildTargetGroup.iOS) != ApiCompatibilityLevel.NET_Unity_4_8)
             {
                 PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.iOS, ApiCompatibilityLevel.NET_Unity_4_8);
+                AssetDatabase.SaveAssets();
             }
             
             if (PlayerSettings.GetApiCompatibilityLevel(BuildTargetGroup.WebGL) != ApiCompatibilityLevel.NET_Unity_4_8)
             {
                 PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.WebGL, ApiCompatibilityLevel.NET_Unity_4_8);
+                AssetDatabase.SaveAssets();
             }
         }
     }

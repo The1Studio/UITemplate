0163b722debe0d792036b61185858835f63bbfed
ngnqu <ngnquocuong@gmail.com>
2024-06-28 11:05:14 +0700

    use item

--- Scripts/Blueprints/UITemplateItemBlueprint.cs
+++ Scripts/Blueprints/UITemplateItemBlueprint.cs
@@ -11,11 +11,12 @@ namespace TheOneStudio.UITemplate.UITemplate.Blueprints
     [CsvHeaderKey("Id")]
     public class UITemplateItemRecord
     {
-        public string                        Id            { get; set; }
-        public string                        Name          { get; set; }
-        public string                        Description   { get; set; }
-        public string                        ImageAddress  { get; set; }
-        public string                        Category      { get; set; }
-        public bool                          IsDefaultItem { get; set; }
+        public string Id            { get; set; }
+        public string Name          { get; set; }
+        public string Description   { get; set; }
+        public string ImageAddress  { get; set; }
+        public string Category      { get; set; }
+        public bool   IsDefaultItem { get; set; }
+        public string AnimationName { get; set; }
     }
 }

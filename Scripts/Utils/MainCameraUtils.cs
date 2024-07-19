0350ecb9e5ceab5d102b00aeb3fe9cc9a87331d0
hungdeptrai1311 <vumanhhung.bc2017@gmail.com>
2024-07-19 19:31:46 +0700

    fix all bug and polish client decor UI

--- Scripts/Utils/MainCameraUtils.cs
+++ Scripts/Utils/MainCameraUtils.cs
@@ -16,27 +16,21 @@
             }
         }
 
-        public static void SetCameraPosition(Vector2 position)
-        {
-            GetMainCam.transform.position = new(position.x, position.y, -10);
-        }
+        public static void SetCameraPosition(Vector2 position) { GetMainCam.transform.position = new Vector3(position.x, position.y, -10); }
 
-        public static void SetOrthographicSize(float size)
-        {
-            GetMainCam.orthographicSize = size;
-        }
+        public static void SetOrthographicSize(float size) { GetMainCam.orthographicSize = size; }
+        
+        public static void ZoomCamera(float targetSize, float duration, Ease easing) { GetMainCam.DOOrthoSize(targetSize, duration).SetEase(easing).SetAutoKill(true); }
 
-        public static void ZoomCamera(float targetSize, float duration, Ease easing)
-        {
-            GetMainCam.DOOrthoSize(targetSize, duration).SetEase(easing).SetAutoKill(true);
-        }
+        public static void MoveCamera(Vector2 position, float duration, Ease easing) { GetMainCam.transform.DOMove( new Vector3(position.x, position.y, -10), duration).SetEase(easing).SetAutoKill(true); }
 
         public static void AdjustOrthographicHorizontalSize(float horizontalSize)
         {
             var unitsPerPixel    = horizontalSize / Screen.width;
             var desireHalfHeight = 0.5f * unitsPerPixel * Screen.height;
             SetOrthographicSize(desireHalfHeight);
-            SetCameraPosition(new(0, -1 - (float)1920 / Screen.height));
+            SetCameraPosition(new Vector2(0, -1 - (float)1920 / Screen.height));
         }
+        
     }
 }

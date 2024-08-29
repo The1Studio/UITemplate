e4750057149e904ad4301582d0d451ccaf985649
Hải Lưu <71810160+hailuu-1103@users.noreply.github.com>
2024-08-29 09:40:50 +0700

    add finish flying animation signal (#565)
    * add finish flying animation signal
    
    * refactor code

---
+++ Scripts/Signals/OnFinishCurrencyAnimationSignal.cs
@@ -0,0 +1,16 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Signals
+{
+    public class OnFinishCurrencyAnimationSignal
+    {
+        public string Id;
+        public int    Amount;
+        public int    FinalValue;
+
+        public OnFinishCurrencyAnimationSignal(string id, int amount, int finalValue)
+        {
+            this.Id         = id;
+            this.Amount     = amount;
+            this.FinalValue = finalValue;
+        }
+    }
+}

c3d63c7810da99ae8a33b2a431fc270da4840a2d
khai1412 <84337343+khai1412@users.noreply.github.com>
2024-09-05 15:11:46 +0700

    fix banner event

---
+++ Scripts/ThirdPartyServices/AnalyticEvents/CommonEvents/BannerLoad.cs
@@ -0,0 +1,14 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.CommonEvents
+{
+    using Core.AnalyticServices.Data;
+
+    public class BannerLoad : IEvent
+    {
+        public string placement;
+
+        public BannerLoad(string placement)
+        {
+            this.placement = placement;
+        }
+    }
+}
---
+++ Scripts/ThirdPartyServices/AnalyticEvents/CommonEvents/BannerLoadFail.cs
@@ -0,0 +1,16 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.CommonEvents
+{
+    using Core.AnalyticServices.Data;
+
+    public class BannerLoadFail : IEvent
+    {
+        public string placement;
+        public string errormsg;
+
+        public BannerLoadFail(string placement, string errormsg)
+        {
+            this.placement = placement;
+            this.errormsg  = errormsg;
+        }
+    }
+}

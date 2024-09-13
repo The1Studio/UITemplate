d64c452183614ad81cc019d2793cfcea5aa77b83
hailuu <hoanghai.luu.71@gmail.com>
2024-09-13 09:21:26 +0700

    chore: refactor AOA ad revenue event signal

--- Scripts/ThirdPartyServices/AnalyticEvents/Apero/AperoAnalyticEventFactory.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/Apero/AperoAnalyticEventFactory.cs
@@ -1,6 +1,7 @@
 ﻿#if APERO
 namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Apero
 {
+    using Core.AdsServices.Signals;
     using Core.AnalyticServices;
     using Core.AnalyticServices.CommonEvents;
     using Core.AnalyticServices.Data;
@@ -14,8 +15,18 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.A
         {
             signalBus.Subscribe<OnIAPPurchaseSuccessSignal>(this.OnIAPPurchaseSuccessHandler);
             signalBus.Subscribe<AdRevenueSignal>(this.OnAdRevenueHandler);
-            signalBus.Subscribe<AdRevenueClickedSignal>(this.OnAdRevenueClickedHandler);
-            signalBus.Subscribe<AdRevenueLoadedSignal>(this.OnAdRevenueLoadedHandler);
+            
+            signalBus.Subscribe<BannerAdLoadedSignal>(signal => this.TrackAdEvent("track_ad_matched_request", signal.AdsRevenueEvent));
+            signalBus.Subscribe<BannerAdClickedSignal>(signal => this.TrackAdEvent("track_ad_click", signal.AdsRevenueEvent));
+            
+            signalBus.Subscribe<InterstitialAdLoadedSignal>(signal => this.TrackAdEvent("track_ad_matched_request", signal.AdsRevenueEvent));
+            signalBus.Subscribe<InterstitialAdClickedSignal>(signal => this.TrackAdEvent("track_ad_click", signal.AdsRevenueEvent));
+            
+            signalBus.Subscribe<RewardedAdLoadedSignal>(signal => this.TrackAdEvent("track_ad_matched_request", signal.AdsRevenueEvent));
+            signalBus.Subscribe<RewardedAdClickedSignal>(signal => this.TrackAdEvent("track_ad_click", signal.AdsRevenueEvent));
+            
+            signalBus.Subscribe<AppOpenLoadedSignal>(signal => this.TrackAdEvent("track_ad_matched_request", signal.AdsRevenueEvent));
+            signalBus.Subscribe<AppOpenFullScreenContentClosedSignal>(signal => this.TrackAdEvent("track_ad_click", signal.AdsRevenueEvent));
         }
 
         public override AnalyticsEventCustomizationConfig ByteBrewAnalyticsEventCustomizationConfig { get; set; } = new()
@@ -40,13 +51,9 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.A
 
         private void OnIAPPurchaseSuccessHandler(OnIAPPurchaseSuccessSignal signal) { this.analyticServices.Track(new Purchase(signal.Product)); }
 
-        private void OnAdRevenueHandler(AdRevenueSignal signal) { this.TrackAdEvent("track_ad_impression", signal.NetworkPlacement, signal.AdsRevenueEvent); }
+        private void OnAdRevenueHandler(AdRevenueSignal signal) { this.TrackAdEvent("track_ad_impression", signal.AdsRevenueEvent); }
 
-        private void OnAdRevenueClickedHandler(AdRevenueClickedSignal signal) { this.TrackAdEvent("track_ad_click", signal.NetworkPlacement, signal.AdsRevenueEvent); }
-
-        private void OnAdRevenueLoadedHandler(AdRevenueLoadedSignal signal) { this.TrackAdEvent("track_ad_matched_request", signal.NetworkPlacement, signal.AdsRevenueEvent); }
-
-        private void TrackAdEvent(string eventName, string placement, AdsRevenueEvent adsRevenueEvent)
+        private void TrackAdEvent(string eventName, AdsRevenueEvent adsRevenueEvent)
         {
             this.analyticServices.Track(new CustomEvent
             {
@@ -56,7 +63,7 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.A
                     { "ad_platform", adsRevenueEvent.AdsRevenueSourceId },
                     { "ad_unit_id", adsRevenueEvent.AdUnit },
                     { "ad_source", adsRevenueEvent.AdNetwork },
-                    { "ad_source_unit_id", placement },
+                    { "ad_source_unit_id", adsRevenueEvent.NetworkPlacement },
                     { "ad_format", adsRevenueEvent.AdFormat },
                     { "placement", adsRevenueEvent.Placement },
                     { "currency", adsRevenueEvent.Currency },

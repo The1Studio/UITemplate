06e9ca4a2798ccd459905fdb9a5b050380e202d0
huongprowar <huongprowar@gmail.com>
2024-09-13 16:42:17 +0700

    add inwave analytic event

---
+++ Scripts/ThirdPartyServices/AnalyticEvents/Inwave/InwaveAnalyticEventFactory.cs
@@ -0,0 +1,74 @@
+﻿#if INWAVE
+namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Inwave
+{
+    using System;
+    using System.Collections.Generic;
+    using Core.AnalyticServices;
+    using Core.AnalyticServices.CommonEvents;
+    using Core.AnalyticServices.Data;
+    using TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices.AnalyticEvents.ABI;
+    using TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.ABI;
+    using Zenject;
+
+    public class InwaveAnalyticEventFactory : BaseAnalyticEventFactory
+    {
+        public InwaveAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
+        {
+        }
+        
+        public override void ForceUpdateAllProperties() { }
+
+        public override string LevelMaxProperty             => "level_max";
+        public override string LastLevelProperty            => "last_level";
+        public override string LastAdsPlacementProperty     => "last_placement";
+        public override string TotalInterstitialAdsProperty => "total_interstitial_ads";
+        public override string TotalRewardedAdsProperty     => "total_rewarded_ads";
+
+        public override AnalyticsEventCustomizationConfig AppsFlyerAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>()
+            {
+                typeof(GameStarted),
+                typeof(AdInterClick),
+                typeof(AdInterFail),
+                typeof(AdInterDownloaded),
+                typeof(AdsRewardFail),
+                typeof(AdsRewardOffer),
+                typeof(AdsRewardedDownloaded),
+                typeof(AdsRewardClick),
+                typeof(LevelComplete),
+            },
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(BannerShown), "af_banner_shown" },
+                { nameof(GameTutorialCompletion), "af_tutorial_completion" },
+                { nameof(LevelAchieved), "af_level_achieved" },
+                { nameof(AdsIntersEligible), "af_inters_ad_eligible" },
+                { nameof(AdInterCalled), "af_inters_api_called" },
+                { nameof(AdInterShow), "af_inters_displayed" },
+                { nameof(AdsRewardEligible), "af_rewarded_ad_eligible" },
+                { nameof(AdsRewardedCalled), "af_rewarded_api_called" },
+                { nameof(AdsRewardShow), "af_rewarded_displayed" },
+                { nameof(AdsRewardComplete), "af_rewarded_ad_completed" },
+            }
+        };
+
+        public override AnalyticsEventCustomizationConfig FireBaseAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>(),
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(AdsRevenueEvent), "ad_impression_abi" },
+                { "AdsRevenueSourceId", "ad_platform" },
+                { "AdNetwork", "ad_source" },
+                { "AdUnit", "ad_unit_name" },
+                { "AdFormat", "ad_format" },
+                { "Placement", "placement" },
+                { "Currency", "currency" },
+                { "Revenue", "value" },
+                { nameof(LevelAchieved), "checkpoint" },
+            }
+        };
+    }
+}
+#endif

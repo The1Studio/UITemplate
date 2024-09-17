c239883bf778453f538b081fae00d1de2b5add57
Manh Nguyen <manhnb1911@gmail.com>
2024-09-17 15:42:29 +0700

    fix IUITemplateControllerData binding

--- Scripts/VContainer/UITemplateLocalDataVContainer.cs
+++ Scripts/VContainer/UITemplateLocalDataVContainer.cs
@@ -2,6 +2,7 @@
 #nullable enable
 namespace TheOneStudio.UITemplate
 {
+    using GameFoundation.DI;
     using GameFoundation.Scripts.Interfaces;
     using GameFoundation.Scripts.Utilities.Extension;
     using TheOneStudio.UITemplate.UITemplate.Models.Controllers;
@@ -13,7 +14,7 @@ namespace TheOneStudio.UITemplate
         public static void RegisterUITemplateLocalData(this IContainerBuilder builder)
         {
             typeof(ILocalData).GetDerivedTypes().ForEach(type => builder.Register(type, Lifetime.Singleton));
-            typeof(IUITemplateControllerData).GetDerivedTypes().ForEach(type => builder.Register(type, Lifetime.Singleton));
+            typeof(IUITemplateControllerData).GetDerivedTypes().ForEach(type => builder.Register(type, Lifetime.Singleton).AsInterfacesAndSelf());
             builder.Register<UserDataManager>(Lifetime.Singleton);
         }
     }

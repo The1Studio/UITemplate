bde2294216a029e12024baf47375e0ccd5899b32
phuongdmp7 <phuongdmp7@gmail.com>
2024-09-19 10:46:45 +0700

    add log for aoa

--- Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
@@ -311,6 +311,8 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
                 this.signalBus.Fire(new AppOpenEligibleSignal(""));
             }
 
+            var typeToAvailable = string.Join(" | ", this.aoaAdServices.Select(aoa => aoa.GetType().Name + " isReady: " + aoa.IsAOAReady()));
+            this.logService.Log($"onelog: AdServiceWrapper: ShowAOAAdsIfAvailable: useAdmob: {this.adServicesConfig.UseAoaAdmob} | {typeToAvailable}");
             foreach (var aoa in this.aoaAdServices.Where(aoaService => aoaService.IsAOAReady()))
             {
                 if ((this.adServicesConfig.UseAoaAdmob

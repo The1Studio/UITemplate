a5525dffbb481782d744f9bd0f37a62133074499
DuyMaiB <103514604+DuyMaiBui@users.noreply.github.com>
2024-09-20 12:22:35 +0700

    [WIP] BraveStar Analytic (#580)
    * update bravestar analytic factory
    
    * fix brave analytics
    
    * fix ignore event rewarded loaded

--- Scripts/ThirdPartyServices/AnalyticEvents/BraveStars/BraveStarsAnalyticEventFactory.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/BraveStars/BraveStarsAnalyticEventFactory.cs
@@ -8,6 +8,7 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.B
     using Core.AnalyticServices.Data;
     using TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices.AnalyticEvents.ABI;
     using TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.ABI;
+    using TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.CommonEvents;
     using Zenject;
 
     public class BraveStarsAnalyticEventFactory : BaseAnalyticEventFactory
@@ -15,41 +16,34 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.B
         public BraveStarsAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
         {
         }
-        
-        public override void ForceUpdateAllProperties() { }
 
-        public override string LevelMaxProperty             => "level_max";
-        public override string LastLevelProperty            => "last_level";
-        public override string LastAdsPlacementProperty     => "last_placement";
-        public override string TotalInterstitialAdsProperty => "total_interstitial_ads";
-        public override string TotalRewardedAdsProperty     => "total_rewarded_ads";
+        public override IEvent LevelWin(int level, int timeSpent, int winCount) => new CustomEvent { EventName = $"win_level_{level}" };
+
+        public override IEvent LevelStart(int level, int gold) => new CustomEvent { EventName = $"start_level_{level}" };
 
         public override AnalyticsEventCustomizationConfig AppsFlyerAnalyticsEventCustomizationConfig { get; set; } = new()
         {
             IgnoreEvents = new HashSet<Type>()
             {
                 typeof(GameStarted),
-                typeof(AdInterClick),
-                typeof(AdInterFail),
-                typeof(AdInterDownloaded),
-                typeof(AdsRewardFail),
-                typeof(AdsRewardOffer),
-                typeof(AdsRewardedDownloaded),
-                typeof(AdsRewardClick),
-                typeof(LevelComplete),
+                typeof(InterstitialAdClicked),
+                typeof(InterstitialAdDisplayedFailed),
+                typeof(RewardedSkipped),
+                typeof(RewardedAdOffer),
+                typeof(RewardedAdLoadClicked)
             },
             CustomEventKeys = new Dictionary<string, string>()
             {
                 { nameof(BannerShown), "af_banner_shown" },
                 { nameof(GameTutorialCompletion), "af_tutorial_completion" },
-                { nameof(LevelAchieved), "af_level_achieved" },
-                { nameof(AdsIntersEligible), "af_inters_ad_eligible" },
-                { nameof(AdInterCalled), "af_inters_api_called" },
-                { nameof(AdInterShow), "af_inters_displayed" },
-                { nameof(AdsRewardEligible), "af_rewarded_ad_eligible" },
-                { nameof(AdsRewardedCalled), "af_rewarded_api_called" },
-                { nameof(AdsRewardShow), "af_rewarded_displayed" },
-                { nameof(AdsRewardComplete), "af_rewarded_ad_completed" },
+                { nameof(AchievedLevel), "af_achieved_level" },
+                { nameof(InterstitialAdEligible), "af_inters_ad_eligible" },
+                { nameof(InterstitialAdDownloaded), "af_inters_api_called" },
+                { nameof(InterstitialAdDisplayed), "af_inters_displayed" },
+                { nameof(RewardedAdEligible), "af_rewarded_ad_eligible" },
+                { nameof(RewardedAdLoaded), "af_rewarded_api_called" },
+                { nameof(RewardedAdDisplayed), "af_rewarded_displayed" },
+                { nameof(RewardedAdCompleted), "af_rewarded_ad_completed" }
             }
         };
 

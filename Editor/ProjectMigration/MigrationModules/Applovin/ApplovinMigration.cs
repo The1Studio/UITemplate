c0bdaba4ad4904f246d3bd21adaef0279824eac1
HungNH <work.hung.nh@gmail.com>
2024-09-23 16:46:48 +0700

    lock version 6.5.2 applovin

--- Editor/ProjectMigration/MigrationModules/Applovin/ApplovinMigration.cs
+++ Editor/ProjectMigration/MigrationModules/Applovin/ApplovinMigration.cs
@@ -24,68 +24,6 @@ namespace UITemplate.Editor.ProjectMigration.MigrationModules
         public static async void DoMigrate()
         {
 #if APPLOVIN
-            // URL to fetch the latest version information of the AppLovin SDK
-            var url = $"https://unity.applovin.com/max/1.0/integration_manager_info?plugin_version={MaxSdk.Version}";
-
-            // Send a GET request to the URL
-            using var www       = UnityWebRequest.Get(url);
-            var       operation = www.SendWebRequest();
-
-            // Wait for the request to complete
-            await operation;
-
-            // If the request was not successful, exit the method
-            if (www.result != UnityWebRequest.Result.Success) return;
-
-            PluginData pluginData;
-            try
-            {
-                // Parse the response text into a PluginData object
-                pluginData = JsonUtility.FromJson<PluginData>(www.downloadHandler.text);
-            }
-            catch (Exception exception)
-            {
-                // If an error occurred while parsing the response text, log the error and set pluginData to null
-                Console.WriteLine(exception);
-                pluginData = null;
-            }
-
-            if (pluginData != null)
-            {
-                // Get the current version of the AppLovin SDK
-                var appLovinMax = pluginData.AppLovinMax;
-
-                // Update the current versions of the AppLovin SDK and its adapters
-                AppLovinIntegrationManager.UpdateCurrentVersions(appLovinMax, AppLovinIntegrationManager.PluginParentDirectory);
-
-                // Compare the current version of the AppLovin SDK with the latest version
-                var maxVersionComparisonResult = MaxSdkUtils.CompareVersions(MaxSdk.Version, appLovinMax.LatestVersions.Unity);
-
-                // If the current version is lesser than the latest version
-                if (maxVersionComparisonResult == MaxSdkUtils.VersionComparisonResult.Lesser)
-                {
-                    // Show a dialog with a message to upgrade AppLovin
-                    EditorUtility.DisplayDialog(
-                        "Upgrade AppLovin",
-                        $"Your current version of AppLovin is {MaxSdk.Version}. The latest version is {appLovinMax.LatestVersions.Unity}. Please upgrade AppLovin to the latest version.",
-                        "OK"
-                    );
-
-                    // Start the process of downloading the latest version of the AppLovin SDK
-                    AppLovinEditorCoroutine.StartCoroutine(AppLovinIntegrationManager.Instance.DownloadPlugin(appLovinMax, false));
-
-                    // Show the AppLovin Integration Manager window
-                    AppLovinIntegrationManagerWindow.ShowManager();
-
-                    // Show a dialog with a message to upgrade the AppLovin adapters
-                    EditorUtility.DisplayDialog(
-                        "Upgrade AppLovin Adapters",
-                        "One or more of your AppLovin adapters need to be upgraded. Please upgrade to the latest version.",
-                        "OK"
-                    );
-                }
-            }
-
             // Change the version of the iosPods in all Dependencies.xml files according to the predefined dictionary
             ChangeIosPodVersionInAllFiles();
 

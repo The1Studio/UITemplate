81543b5b5aa0759c1ea45430baf759be0b73af9f
congLy <ngcongly2002@gmail.com>
2024-09-26 14:20:33 +0700

    Add symbol Falcon to remove unused script

--- Scripts/ThirdPartyServices/AnalyticEvents/Falcon/FalconLocalData.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/Falcon/FalconLocalData.cs
@@ -3,10 +3,12 @@
     using System.Collections.Generic;
     using GameFoundation.Scripts.Interfaces;
 
+#if FALCON
     public class FalconLocalData : ILocalData
     {
         public void Init() { }
 
         public HashSet<int> PassedLevels { get; set; } = new();
     }
+#endif
 }

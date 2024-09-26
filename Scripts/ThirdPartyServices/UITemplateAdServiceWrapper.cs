797dc731a4fa879b44003ffe9d0719baf729e6d8
Hải Lưu <71810160+hailuu-1103@users.noreply.github.com>
2024-09-26 17:43:25 +0700

    Capping time counts only while Application is Focusing (#589)
    * chore: make inter ads capping time counts only while Application is focusing
    
    * feat: (inter) capping time counts only while app is focusing
    
    * chore:refactor code
    
    * chore: refactor code
    
    * chore: refactor code
    
    * chore: add comment on calculate capping time code

--- Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
@@ -342,14 +342,14 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
                 this.StartBackgroundTime = DateTime.Now;
                 return;
             }
-
+            
             var totalBackgroundSeconds = (DateTime.Now - this.StartBackgroundTime).TotalSeconds;
             if (totalBackgroundSeconds < this.adServicesConfig.MinPauseSecondToShowAoaAd)
             {
                 this.logService.Log($"AOA background time: {totalBackgroundSeconds}");
                 return;
             }
-
+            
             if (this.IsResumedFromAnotherServices)
             {
                 return;
@@ -569,8 +569,15 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
 
         public void Tick()
         {
-            this.totalNoAdsPlayingTime += Time.unscaledDeltaTime;
-
+            // Problem: Time.unscaledDeltaTime is still incrementing when the game is paused, so if we somehow pause the game, the capping time will be increased
+            // Solution: Time.unscaledDeltaTime is count from the last frame to current frame, so if the time between 2 frames is too long, we will not increase the capping time
+            // If EnableInterCappingTimeFocus is set to false, we will keep the existing capping time behavior
+            // If EnableInterCappingTimeFocus is set to true, we will not increase the capping time while application is not focused
+            if (!this.thirdPartiesConfig.AdSettings.EnableInterCappingTimeFocus || Time.unscaledDeltaTime < 1)
+            {
+                this.totalNoAdsPlayingTime += Time.unscaledDeltaTime;
+            }
+            
             if (!this.IsCheckedShowFirstOpen && this.gameSessionDataController.OpenTime >= this.adServicesConfig.AOAStartSession)
             {
                 this.CheckShowFirstOpen();

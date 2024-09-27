eda8b173988692e87fe4cea2846e2f1a7c9b35f8
Manh Nguyen <manhnb1911@gmail.com>
2024-09-27 16:10:56 +0700

    wtf bro

--- Scripts/Models/Controllers/UITemplateCommonController.cs
+++ Scripts/Models/Controllers/UITemplateCommonController.cs
@@ -1,12 +1,15 @@
 namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
 {
     using TheOneStudio.UITemplate.UITemplate.Models.LocalDatas;
+    using UnityEngine.Scripting;
 
-    public class UITemplateCommonController:IUITemplateControllerData
+    public class UITemplateCommonController : IUITemplateControllerData
     {
         private readonly UITemplateCommonData uiTemplateCommonData;
 
+        [Preserve]
         public UITemplateCommonController(UITemplateCommonData uiTemplateCommonData) { this.uiTemplateCommonData = uiTemplateCommonData; }
+
         public bool IsFirstTimeOpenGame => this.uiTemplateCommonData.IsFirstTimeOpenGame;
 
         public void ChangeGameIsAlreadyOpened() { this.uiTemplateCommonData.IsFirstTimeOpenGame = false; }
--- Scripts/Models/Controllers/UITemplateIAPOwnerPackControllerData.cs
+++ Scripts/Models/Controllers/UITemplateIAPOwnerPackControllerData.cs
@@ -1,11 +1,13 @@
 namespace TheOneStudio.UITemplate.UITemplate.Models.Controllers
 {
     using TheOneStudio.UITemplate.UITemplate.Models.LocalDatas;
+    using UnityEngine.Scripting;
 
-    public class UITemplateIAPOwnerPackControllerData:IUITemplateControllerData
+    public class UITemplateIAPOwnerPackControllerData : IUITemplateControllerData
     {
         private readonly UITemplateIAPOwnerPackData uiTemplateIAPOwnerPackData;
 
+        [Preserve]
         public UITemplateIAPOwnerPackControllerData(UITemplateIAPOwnerPackData uiTemplateIAPOwnerPackData) { this.uiTemplateIAPOwnerPackData = uiTemplateIAPOwnerPackData; }
 
         public void AddPack(string packId)

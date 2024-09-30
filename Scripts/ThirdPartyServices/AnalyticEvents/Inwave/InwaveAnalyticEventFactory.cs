fcdc5eb27fffb3c7294a0a32817895fa742ec9ea
phuongdmp7 <phuongdmp7@gmail.com>
2024-09-30 18:29:08 +0700

    add inwave analytic factory

--- Scripts/ThirdPartyServices/AnalyticEvents/Inwave/InwaveAnalyticEventFactory.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/Inwave/InwaveAnalyticEventFactory.cs
@@ -1,14 +1,52 @@
 ﻿#if INWAVE
 namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Inwave
 {
+    using System;
+    using System.Collections.Generic;
     using Core.AnalyticServices;
-    using Zenject;
+    using Core.AnalyticServices.CommonEvents;
+    using Core.AnalyticServices.Data;
+    using GameFoundation.Signals;
     using UnityEngine.Scripting;
 
     public class InwaveAnalyticEventFactory : BaseAnalyticEventFactory
     {
         [Preserve]
-        public InwaveAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices) { }
+        public InwaveAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices)
+            : base(signalBus, analyticServices)
+        {
+        }
+
+        public override void ForceUpdateAllProperties() { }
+
+        public override AnalyticsEventCustomizationConfig AppsFlyerAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>()
+            {
+                typeof(GameStarted)
+            },
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(BannerShown), "af_banner_shown" }
+            }
+        };
+
+        public override AnalyticsEventCustomizationConfig FireBaseAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>(),
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(AdsRevenueEvent), "ad_impression_new" },
+                { "AdsRevenueSourceId", "ad_platform" },
+                { "AdNetwork", "ad_source" },
+                { "AdUnit", "ad_unit_name" },
+                { "AdFormat", "ad_format" },
+                { "Placement", "placement" },
+                { "Currency", "currency" },
+                { "Revenue", "value" },
+                { "Message", "errormsg" },
+            }
+        };
     }
 }
 #endif

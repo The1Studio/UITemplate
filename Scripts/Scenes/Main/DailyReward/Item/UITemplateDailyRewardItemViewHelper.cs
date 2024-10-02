7be9bc7a27432224a4de44550ee242e1014d78a2
anhndq_home <quanganh01082002@gmail.com>
2024-10-02 22:24:05 +0700

    readd claimReward method in daily reward item

--- Scripts/Scenes/Main/DailyReward/Item/UITemplateDailyRewardItemViewHelper.cs
+++ Scripts/Scenes/Main/DailyReward/Item/UITemplateDailyRewardItemViewHelper.cs
@@ -36,8 +36,8 @@
             view.ObjLock.SetActive(!view.ObjReward.activeSelf);
         }
 
-        public virtual void DisposeItem(UITemplateDailyRewardItemPresenter presenter)
-        {
-        }
+        public virtual void DisposeItem(UITemplateDailyRewardItemPresenter presenter) { }
+        
+        public virtual void OnClaimReward(UITemplateDailyRewardItemPresenter presenter) { }
     }
 }

12a6288f461e5477ef9f4420f5b364462adf7c4e
Hoàng Anh Tú <tuha263@gmail.com>
2024-10-03 15:14:44 +0700

    Upddate google packages version

--- Editor/ProjectMigration/MigrationModules/Firebase/FirebasePackageUpdater.cs
+++ Editor/ProjectMigration/MigrationModules/Firebase/FirebasePackageUpdater.cs
@@ -19,45 +19,45 @@ namespace UITemplate.Editor.ProjectMigration.MigrationModules
         private const string BaseUrl = "https://dl.google.com/games/registry/unity/";
 
 
-        [NonSerialized]
-        private static readonly List<string> RequirePackages = new()
+        [NonSerialized] private static readonly Dictionary<string, string> RequirePackages = new()
         {
-            "com.google.firebase.analytics",
-            "com.google.firebase.app",
-            "com.google.firebase.crashlytics",
-            "com.google.firebase.messaging",
-            "com.google.firebase.remote-config"
+            { "com.google.firebase.analytics", "12.3.0" },
+            { "com.google.firebase.app", "12.3.0" },
+            { "com.google.firebase.crashlytics", "12.3.0" },
+            { "com.google.firebase.messaging", "12.3.0" },
+            { "com.google.firebase.remote-config", "12.3.0" },
+            { "com.google.play.core", "1.8.4" },
+            { "com.google.play.common", "1.9.1" },
+            { "com.google.android.appbundle", "1.9.0" },
+            {"com.google.play.review", "1.8.2"}
         };
 
         static FirebasePackagesUpdater() { UpdatePackageToVersion(); }
 
-        private static void UpdatePackageToVersion()
-        {
-            UpdateFirebasePackages(RequirePackages, "12.2.1");
-        }
+        private static void UpdatePackageToVersion() { UpdateFirebasePackages(RequirePackages); }
 
-        private static async void UpdateFirebasePackages(List<string> packages, string version)
+        private static async void UpdateFirebasePackages(Dictionary<string, string> packages)
         {
             var packagesDirectoryPath = Path.Combine(Application.dataPath, "..", PackagesDirectory);
             var existingPackages = Directory.GetFiles(packagesDirectoryPath, "*.tgz")
                 .Select(Path.GetFileNameWithoutExtension)
                 .ToList();
-            
+
             // Check if all the packages of the required version already exist
-            if (packages.All(package => existingPackages.Any(p => p.StartsWith(package) && p == package + "-" + version)))
+            if (packages.All(valuePair => existingPackages.Any(p => p.StartsWith(valuePair.Key) && p == valuePair + "-" + valuePair.Value)))
             {
                 return;
             }
-            
+
             // Show a dialog box that blocks user interaction
-            EditorUtility.DisplayDialog($"Update firebase packages to version: {version}", "Please wait while the packages are being downloaded...", "OK");
+            EditorUtility.DisplayDialog($"Update google packages to locked version", "Please wait while the packages are being downloaded...", "OK");
 
             // Disable the "OK" button
             EditorUtility.DisplayProgressBar("Download in progress", "Please wait while the packages are being downloaded...", 0);
 
             var completedDownloads = 0;
-            
-            foreach (var package in packages)
+
+            foreach (var (package, version) in packages)
             {
                 // Check if the package of the same version already exists
                 var expectedPackageName = package + "-" + version;
@@ -66,7 +66,7 @@ namespace UITemplate.Editor.ProjectMigration.MigrationModules
                     Debug.Log($"Package {package} of version {version} already exists. Skipping download.");
                     continue;
                 }
-                
+
                 // Get the old version of the package from the existing files
                 var oldPackageFile = existingPackages.FirstOrDefault(p => p.Contains(package));
                 if (oldPackageFile != null)
@@ -101,14 +101,14 @@ namespace UITemplate.Editor.ProjectMigration.MigrationModules
                     EditorUtility.DisplayProgressBar("Download in progress", $"Downloading {completedDownloads} of {packages.Count} packages...", progress);
                 }
             }
-            
+
             // Enable the "OK" button when the download is complete
             EditorUtility.ClearProgressBar();
 
-            UpdateManifestJson(packages, version);
+            UpdateManifestJson(packages);
         }
 
-        private static void UpdateManifestJson(List<string> packages, string version)
+        private static void UpdateManifestJson(Dictionary<string, string> packages)
         {
             var manifestPath = Path.Combine(Application.dataPath, "..", "Packages", "manifest.json");
             var manifestJson = File.ReadAllText(manifestPath);
@@ -121,7 +121,7 @@ namespace UITemplate.Editor.ProjectMigration.MigrationModules
                 manifest["dependencies"] = dependencies;
             }
 
-            foreach (var package in packages)
+            foreach (var (package, version) in packages)
             {
                 dependencies[package] = "file:GoogleDependency/" + package + "-" + version + ".tgz";
             }

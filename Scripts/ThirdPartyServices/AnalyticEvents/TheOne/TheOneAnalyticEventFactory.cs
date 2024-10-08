7ac254128d9b76c0cbd9cda55f75500f1e1caa11
phuongdmp7 <phuongdmp7@gmail.com>
2024-10-08 10:57:23 +0700

    update theone analytic factory

--- Scripts/ThirdPartyServices/AnalyticEvents/TheOne/TheOneAnalyticEventFactory.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/TheOne/TheOneAnalyticEventFactory.cs
@@ -1,7 +1,11 @@
 #if THEONE
 namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.TheOne
 {
+    using System;
+    using System.Collections.Generic;
     using Core.AnalyticServices;
+    using Core.AnalyticServices.CommonEvents;
+    using Core.AnalyticServices.Data;
     using GameFoundation.Signals;
     using UnityEngine.Scripting;
 
@@ -11,6 +15,34 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.T
         public TheOneAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
         {
         }
+        
+        public override AnalyticsEventCustomizationConfig AppsFlyerAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>()
+            {
+            },
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(BannerShown), "af_banner_shown" }
+            }
+        };
+
+        public override AnalyticsEventCustomizationConfig FireBaseAnalyticsEventCustomizationConfig { get; set; } = new()
+        {
+            IgnoreEvents = new HashSet<Type>(),
+            CustomEventKeys = new Dictionary<string, string>()
+            {
+                { nameof(AdsRevenueEvent), "ad_impression_abi" },
+                { "AdsRevenueSourceId", "ad_platform" },
+                { "AdNetwork", "ad_source" },
+                { "AdUnit", "ad_unit_name" },
+                { "AdFormat", "ad_format" },
+                { "Placement", "placement" },
+                { "Currency", "currency" },
+                { "Revenue", "value" },
+                { "Message", "errormsg" }
+            }
+        };
     }
 }
 #endif

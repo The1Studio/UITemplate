f93243198477975e897b9dbf56314d9892c17027
mike <duymaibui2k@gmail.com>
2024-10-10 11:32:27 +0700

    fix collap remote config

--- Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
@@ -173,7 +173,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
                 {
                     var useNewGuid = this.IsRefreshingCollapsible
                         ? this.adServicesConfig.CollapsibleBannerExpandOnRefreshEnabled
-                        : (DateTime.Now - this.LastCollapsibleBannerChangeGuid).TotalSeconds >= this.adServicesConfig.CollapsibleBannerExpandOnRefreshInterval;
+                        : (DateTime.Now - this.LastCollapsibleBannerChangeGuid).TotalSeconds >= this.adServicesConfig.InterstitialAdInterval;
                     if (useNewGuid)
                     {
                         this.LastCollapsibleBannerChangeGuid = DateTime.Now;
@@ -202,10 +202,10 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
             this.RefreshCollapsibleCts?.Cancel();
             this.RefreshCollapsibleCts?.Dispose();
             this.RefreshCollapsibleCts = null;
-            if (this.adServicesConfig.CollapsibleBannerADInterval <= 0) return;
+            if (this.adServicesConfig.CollapsibleBannerExpandOnRefreshInterval <= 0) return;
             if (!this.adServicesConfig.CollapsibleBannerAutoRefreshEnabled) return;
             UniTask.WaitForSeconds(
-                this.adServicesConfig.CollapsibleBannerADInterval,
+                this.adServicesConfig.CollapsibleBannerExpandOnRefreshInterval,
                 ignoreTimeScale: true,
                 cancellationToken: (this.RefreshCollapsibleCts = new()).Token
             ).ContinueWith(() =>

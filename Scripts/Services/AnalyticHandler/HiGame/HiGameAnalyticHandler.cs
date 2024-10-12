bf3e2b52583d5ee4d3c5b7d3bd3da34962e027e2
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2024-10-12 18:33:33 +0700

    add hi game factory (#610)
    * add hi game analytic event factory
    
    * update higame analytic
    
    * update af inter

---
+++ Scripts/Services/AnalyticHandler/HiGame/HiGameAnalyticHandler.cs
@@ -0,0 +1,69 @@
+﻿namespace TheOneStudio.UITemplate.UITemplate.Services.AnalyticHandler.HiGame
+{
+#if HIGAME
+    using Core.AnalyticServices;
+    using Core.AnalyticServices.CommonEvents;
+    using GameFoundation.Signals;
+    using TheOneStudio.UITemplate.UITemplate.Models.Controllers;
+    using TheOneStudio.UITemplate.UITemplate.Signals;
+    using TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents;
+    using UnityEngine.Scripting;
+
+    public class HiGameAnalyticHandler : UITemplateAnalyticHandler
+    {
+        #region Inject
+
+        private readonly HiGameLocalData hiGameLocalData;
+
+        [Preserve]
+        public HiGameAnalyticHandler(SignalBus signalBus,
+                                     IAnalyticServices analyticServices,
+                                     IAnalyticEventFactory analyticEventFactory,
+                                     UITemplateLevelDataController uiTemplateLevelDataController,
+                                     UITemplateInventoryDataController uITemplateInventoryDataController,
+                                     UITemplateDailyRewardController uiTemplateDailyRewardController,
+                                     UITemplateAdsController uiTemplateAdsController,
+                                     UITemplateGameSessionDataController uITemplateGameSessionDataController,
+                                     HiGameLocalData hiGameLocalData)
+            : base(signalBus, analyticServices, analyticEventFactory, uiTemplateLevelDataController, uITemplateInventoryDataController,
+                uiTemplateDailyRewardController, uITemplateGameSessionDataController)
+        {
+            this.hiGameLocalData = hiGameLocalData;
+        }
+
+        #endregion
+
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            this.signalBus.Subscribe<LevelEndedSignal>(this.OnLevelEnded);
+            this.signalBus.Subscribe<LevelSkippedSignal>(this.OnLevelSkipped);
+        }
+
+        private void OnLevelEnded(LevelEndedSignal obj)
+        {
+            if (!obj.IsWin) return;
+            var levelNotPassBefore = this.hiGameLocalData.PassedLevels.Add(obj.Level);
+            if (levelNotPassBefore)
+            {
+                this.LogCheckpoint(obj.Level);
+            }
+        }
+
+        private void OnLevelSkipped(LevelSkippedSignal obj)
+        {
+            if (this.hiGameLocalData.PassedLevels.Add(obj.Level)) this.LogCheckpoint(obj.Level);
+        }
+
+        private void LogCheckpoint(int level)
+        {
+            if (level > 20) return;
+            this.analyticServices.Track(new CustomEvent()
+            {
+                EventName = $"checkpoint_{level}"
+            });
+        }
+    }
+#endif
+}

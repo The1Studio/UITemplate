2c21b1bb96ad01b1e0ba254a56fa2bfa320e784b
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2024-10-12 18:33:33 +0700

    add hi game factory (#610)
    * add hi game analytic event factory
    
    * update higame analytic
    
    * update af inter

--- Scripts/ThirdPartyServices/AnalyticEvents/CommonEvents/Level.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/CommonEvents/Level.cs
@@ -65,4 +65,22 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.C
             this.timeSpent = timeSpent;
         }
     }
+    
+    public class LevelEnd : IEvent
+    {
+        public int    level;
+        public string status;
+        public int    worldId;
+        public double timePlay;
+        public long   timerStamp;
+
+        public LevelEnd(int level, string status, int worldId, double timePlay, long timeStamp)
+        {
+            this.level      = level;
+            this.status     = status;
+            this.worldId    = worldId;
+            this.timePlay   = timePlay;
+            this.timerStamp = timeStamp;
+        }
+    }
 }

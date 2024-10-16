590e89ce4d1e029a1bb60e90c6012b1adf4e041e
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2024-10-16 14:07:07 +0700

    update daily reward container (#622)

--- Scripts/VContainer/UITemplateDailyRewardVContainer.cs
+++ Scripts/VContainer/UITemplateDailyRewardVContainer.cs
@@ -2,7 +2,6 @@
 #nullable enable
 namespace TheOneStudio.UITemplate
 {
-    using System.Linq;
     using GameFoundation.Scripts.Utilities.Extension;
     using TheOneStudio.UITemplate.UITemplate.Scenes.Main.DailyReward;
     using TheOneStudio.UITemplate.UITemplate.Scenes.Main.DailyReward.Item;
@@ -13,13 +12,9 @@ namespace TheOneStudio.UITemplate
     {
         public static void RegisterDailyReward(this IContainerBuilder builder)
         {
-            builder.Register<UITemplateDailyRewardItemViewHelper>(Lifetime.Singleton);
-            builder.Register<UITemplateDailyRewardPackViewHelper>(Lifetime.Singleton);
-            var concreteDailyRewardAnimationHelperType = typeof(DailyRewardAnimationHelper)
-                .GetDerivedTypes()
-                .OrderBy(type => type == typeof(DailyRewardAnimationHelper))
-                .First();
-            builder.Register(concreteDailyRewardAnimationHelperType, Lifetime.Singleton).As<DailyRewardAnimationHelper>();
+            builder.RegisterFromDerivedType<UITemplateDailyRewardItemViewHelper>();
+            builder.RegisterFromDerivedType<UITemplateDailyRewardPackViewHelper>();
+            builder.RegisterFromDerivedType<DailyRewardAnimationHelper>();
         }
     }
 }

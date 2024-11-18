69daa608cec7321f72cba959c51c9acff0588d9c
huongprowar <huongprowar@gmail.com>
2024-11-18 17:05:50 +0700

    add theone event

--- Scripts/ThirdPartyServices/AnalyticEvents/TheOne/TheOneAnalyticEventFactory.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/TheOne/TheOneAnalyticEventFactory.cs
@@ -2,19 +2,16 @@
 namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.TheOne
 {
     using System;
-    using System.Collections.Generic;
     using Core.AnalyticServices;
     using Core.AnalyticServices.CommonEvents;
     using Core.AnalyticServices.Data;
     using GameFoundation.Signals;
+    using TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.CommonEvents;
     using UnityEngine.Scripting;
 
     public class TheOneAnalyticEventFactory : BaseAnalyticEventFactory
     {
-        [Preserve]
-        public TheOneAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
-        {
-        }
+        [Preserve] public TheOneAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices) { }
 
         public override AnalyticsEventCustomizationConfig AppsFlyerAnalyticsEventCustomizationConfig { get; set; } = new()
         {
@@ -43,6 +40,19 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.T
                 { "Message", "errormsg" },
             },
         };
+
+        public override IEvent LevelLose(int level, int timeSpent, int loseCount)
+        {
+            this.analyticServices.Track(new LevelEnd(level, "win", 0, timeSpent, DateTimeOffset.UtcNow.ToUnixTimeSeconds()));
+            return base.LevelLose(level, timeSpent, loseCount);
+        }
+
+
+        public override IEvent LevelWin(int level, int timeSpent, int winCount)
+        {
+            this.analyticServices.Track(new LevelEnd(level, "lose", 0, timeSpent, DateTimeOffset.UtcNow.ToUnixTimeSeconds()));
+            return base.LevelWin(level, timeSpent, winCount);
+        }
     }
 }
 #endif

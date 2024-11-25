7c1f24f7c65e5cb77d26b566d864e23fa86c7671
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2024-11-25 18:14:07 +0700

    upgrade google mobile ads (#650)

--- Editor/ProjectMigration/MigrationModules/PackageMigration.cs
+++ Editor/ProjectMigration/MigrationModules/PackageMigration.cs
@@ -86,7 +86,7 @@ namespace TheOne.Tool.Migration.ProjectMigration.MigrationModules
         [NonSerialized]
         private static readonly Dictionary<string, string> PackagesVersionToUse = new()
         {
-            { "com.google.ads.mobile", "9.3.0" },
+            { "com.google.ads.mobile", "9.4.0" },
             { "com.unity.purchasing", "4.12.2" },
             { "com.cysharp.unitask", "2.5.10" },
             { "jp.hadashikick.vcontainer", "1.16.5" },

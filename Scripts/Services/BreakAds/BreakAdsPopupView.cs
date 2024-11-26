41cd1e117b426c07869957b2c159261a98680b77
phuongdmp7 <83566201+phuongdmp7@users.noreply.github.com>
2024-11-26 09:13:56 +0700

    add reward break ads (#651)
    * add reward break ads
    
    * wip
    
    * update break ads

--- Scripts/Services/BreakAds/BreakAdsPopupView.cs
+++ Scripts/Services/BreakAds/BreakAdsPopupView.cs
@@ -5,22 +5,45 @@
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.View;
     using GameFoundation.Scripts.Utilities.LogService;
     using GameFoundation.Signals;
+    using ServiceImplementation.Configs;
+    using Sirenix.OdinInspector;
+    using TheOneStudio.UITemplate.UITemplate.Models.Controllers;
+    using TMPro;
+    using UnityEngine;
     using UnityEngine.Scripting;
 
     public class BreakAdsPopupView : BaseView
     {
+        [BoxGroup("Reward Currency")] public RectTransform currencyTransform;
+        [BoxGroup("Reward Currency")] public GameObject    goRewardCurrency;
+        [BoxGroup("Reward Currency")] public TMP_Text      txtRewardCurrencyValue;
+        [BoxGroup("Reward Currency")] public string        currencyValuePattern = "+{0}";
     }
 
     [PopupInfo(nameof(BreakAdsPopupView), isOverlay: true)]
     public class BreakAdsPopupPresenter : BasePopupPresenter<BreakAdsPopupView>
     {
-        private readonly BreakAdsViewHelper breakAdsViewHelper;
+        #region Inject
+
+        private readonly BreakAdsViewHelper                breakAdsViewHelper;
+        private readonly ThirdPartiesConfig                thirdPartiesConfig;
+        private readonly UITemplateInventoryDataController inventoryDataController;
 
         [Preserve]
-        public BreakAdsPopupPresenter(SignalBus signalBus, ILogService logger, BreakAdsViewHelper breakAdsViewHelper) : base(signalBus, logger)
+        public BreakAdsPopupPresenter(
+            SignalBus                         signalBus,
+            ILogService                       logger,
+            BreakAdsViewHelper                breakAdsViewHelper,
+            ThirdPartiesConfig                thirdPartiesConfig,
+            UITemplateInventoryDataController inventoryDataController
+        ) : base(signalBus, logger)
         {
-            this.breakAdsViewHelper = breakAdsViewHelper;
+            this.breakAdsViewHelper      = breakAdsViewHelper;
+            this.thirdPartiesConfig      = thirdPartiesConfig;
+            this.inventoryDataController = inventoryDataController;
         }
+
+        #endregion
 
         protected override void OnViewReady()
         {
@@ -29,7 +52,26 @@
 
         public override UniTask BindData()
         {
+            this.SetupUI();
             return this.breakAdsViewHelper.BindData();
+        }
+
+        public override async UniTask CloseViewAsync()
+        {
+            await this.RewardAfterWatchedAds();
+            await base.CloseViewAsync();
+        }
+
+        protected virtual void SetupUI()
+        {
+            this.View.goRewardCurrency.gameObject.SetActive(this.thirdPartiesConfig.AdSettings.IsBreakAdsRewardCurrency);
+            this.View.txtRewardCurrencyValue.text = string.Format(this.View.currencyValuePattern, this.thirdPartiesConfig.AdSettings.BreakAdsRewardCurrencyAmount);
+        }
+
+        protected virtual async UniTask RewardAfterWatchedAds()
+        {
+            if (!this.thirdPartiesConfig.AdSettings.IsBreakAdsRewardCurrency) return;
+            await this.inventoryDataController.AddCurrency(this.thirdPartiesConfig.AdSettings.BreakAdsRewardCurrencyAmount, this.thirdPartiesConfig.AdSettings.BreakAdsRewardCurrency, this.View.currencyTransform);
         }
 
         public override void Dispose()

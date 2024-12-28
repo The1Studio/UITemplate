7e4f5e4601f1ef76c9f40b48cb33285ed7243e4a
doquanghuy2778 <dohuy0079@gmail.com>
2024-12-28 18:17:57 +0700

    fix aoa count

--- Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
@@ -597,14 +597,14 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
         public void Tick()
         {
             Debug.LogError($"ShowAOA {this.IsAOAEnable}");
+            if(!this.IsAOAEnable) return;
             // Problem: Time.unscaledDeltaTime is still incrementing when the game is paused, so if we somehow pause the game, the capping time will be increased
             // Solution: Time.unscaledDeltaTime is count from the last frame to current frame, so if the time between 2 frames is too long, we will not increase the capping time
             // If EnableInterCappingTimeFocus is set to false, we will keep the existing capping time behavior
             // If EnableInterCappingTimeFocus is set to true, we will not increase the capping time while application is not focused
-            if (!this.IsAOAEnable && (!this.thirdPartiesConfig.AdSettings.EnableInterCappingTimeFocus || Time.unscaledDeltaTime < 1))
+            if (!this.thirdPartiesConfig.AdSettings.EnableInterCappingTimeFocus || Time.unscaledDeltaTime < 1)
             {
                 this.totalNoAdsPlayingTime += Time.unscaledDeltaTime;
-                Debug.LogError($"time interval: {this.totalNoAdsPlayingTime}");
             }
         }
 

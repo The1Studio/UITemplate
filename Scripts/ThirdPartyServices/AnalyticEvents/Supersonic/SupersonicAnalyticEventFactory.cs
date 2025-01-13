804d926cfd4ffd7cca68fc4f3002df71d196c818
CuongND <duycuong2k3@gmail.com>
2025-01-13 16:50:05 +0700

    Implement supersonic analytic event

--- Scripts/ThirdPartyServices/AnalyticEvents/Supersonic/SupersonicAnalyticEventFactory.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/Supersonic/SupersonicAnalyticEventFactory.cs
@@ -6,10 +6,12 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.S
     using Core.AnalyticServices;
     using Core.AnalyticServices.CommonEvents;
     using Core.AnalyticServices.Data;
+    using GameFoundation.DI;
     using GameFoundation.Signals;
+    using SupersonicWisdomSDK;
     using UnityEngine.Scripting;
 
-    public class SupersonicAnalyticEventFactory : BaseAnalyticEventFactory
+    public class SupersonicAnalyticEventFactory : BaseAnalyticEventFactory, IInitializable
     {
         [Preserve]
         public SupersonicAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices)
@@ -45,6 +47,32 @@ namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.S
                 { "Message", "errormsg" }
             }
         };
+
+        public void Initialize()
+        {
+            SupersonicWisdom.Api.Initialize();
+            SupersonicWisdom.Api.AddOnReadyListener(this.OnSupersonicWisdomReady);
+        }
+
+        public void OnSupersonicWisdomReady() { }
+
+        public override IEvent LevelStart(int level, int gold)
+        {
+            SupersonicWisdom.Api.NotifyLevelStarted(ESwLevelType.Regular, level, null);
+            return base.LevelStart(level, gold);
+        }
+
+        public override IEvent LevelLose(int level, int timeSpent, int loseCount)
+        {
+            SupersonicWisdom.Api.NotifyLevelFailed(ESwLevelType.Regular, level, null);
+            return base.LevelLose(level, timeSpent, loseCount);
+        }
+
+        public override IEvent LevelWin(int level, int timeSpent, int winCount)
+        {
+            SupersonicWisdom.Api.NotifyLevelCompleted(ESwLevelType.Regular, level, null);
+            return base.LevelWin(level, timeSpent, winCount);
+        }
     }
 }
 #endif

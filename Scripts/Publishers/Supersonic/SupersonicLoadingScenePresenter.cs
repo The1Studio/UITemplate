0105b8db77c14a391b01af2dc27cc3f8dfbbfde6
phuongdmp7 <phuongdmp7@gmail.com>
2025-01-13 15:11:26 +0700

    update SupersonicLoadingScenePresenter.cs

--- Scripts/Publishers/Supersonic/SupersonicLoadingScenePresenter.cs
+++ Scripts/Publishers/Supersonic/SupersonicLoadingScenePresenter.cs
@@ -7,25 +7,29 @@
     using GameFoundation.Scripts.Utilities.LogService;
     using GameFoundation.Scripts.Utilities.ObjectPool;
     using GameFoundation.Signals;
+    using ServiceImplementation.AdsServices.ConsentInformation;
     using TheOneStudio.UITemplate.UITemplate.Scenes.Loading;
     using TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices;
     using TheOneStudio.UITemplate.UITemplate.UserData;
-#if SUPERSONIC_WISDOM
+    #if SUPERSONIC_WISDOM
     using SupersonicWisdomSDK;
-#endif
+    #endif
 
     public class SupersonicLoadingScenePresenter : UITemplateLoadingScreenPresenter
     {
-        protected SupersonicLoadingScenePresenter(SignalBus signalBus,
-                                                ILogService logger,
-                                                UITemplateAdServiceWrapper adService,
-                                                BlueprintReaderManager blueprintManager,
-                                                UserDataManager userDataManager,
-                                                IGameAssets gameAssets,
-                                                ObjectPoolManager objectPoolManager,
-                                                UITemplateAdServiceWrapper uiTemplateAdServiceWrapper,
-                                                IAnalyticServices analyticServices)
-            : base(signalBus, logger, adService, blueprintManager, userDataManager, gameAssets, objectPoolManager, uiTemplateAdServiceWrapper, analyticServices)
+        protected SupersonicLoadingScenePresenter(
+            SignalBus                  signalBus,
+            ILogService                logger,
+            UITemplateAdServiceWrapper adService,
+            BlueprintReaderManager     blueprintManager,
+            UserDataManager            userDataManager,
+            IGameAssets                gameAssets,
+            ObjectPoolManager          objectPoolManager,
+            UITemplateAdServiceWrapper uiTemplateAdServiceWrapper,
+            IAnalyticServices          analyticServices,
+            IConsentInformation        consentInformation
+        )
+            : base(signalBus, logger, adService, blueprintManager, userDataManager, gameAssets, objectPoolManager, uiTemplateAdServiceWrapper, analyticServices, consentInformation)
         {
         }
 
@@ -34,15 +38,15 @@
             base.OnViewReady();
             this.InitializeSuperSonicWisdom();
         }
-        
+
         private void InitializeSuperSonicWisdom()
         {
-#if SUPERSONIC_WISDOM
+            #if SUPERSONIC_WISDOM
             var tcs = new UniTaskCompletionSource();
             SupersonicWisdom.Api.AddOnReadyListener(() => tcs.TrySetResult());
             SupersonicWisdom.Api.Initialize();
             this.TrackProgress(tcs.Task);
-#endif
+            #endif
         }
     }
 }

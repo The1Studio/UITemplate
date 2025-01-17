c85c9f89a0bce2c419091035c846f65351d4fb3b
TungLV-GOG1071 <goldendarkness1071@gmail.com>
2025-01-17 15:31:05 +0700

    Add duration for tooltips of ftue

--- Scripts/FTUE/Signal/FTUEShowTooltipSignal.cs
+++ Scripts/FTUE/Signal/FTUEShowTooltipSignal.cs
@@ -6,11 +6,13 @@
     {
         public string     FTUEId          { get; set; }
         public GameObject HighlightObject { get; set; }
+        public float      Duration        { get; set; }
 
-        public FTUEShowTooltipSignal(string ftueId, GameObject highlightObject)
+        public FTUEShowTooltipSignal(string ftueId, GameObject highlightObject, float duration)
         {
             this.FTUEId          = ftueId;
             this.HighlightObject = highlightObject;
+            this.Duration        = duration;
         }
     }
 }

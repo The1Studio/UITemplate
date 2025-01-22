92708bc3fdd4fc9be8ef1bc344b7db9267c13895
phuongdmp <phuongdmp7@gmail.com>
2025-01-22 14:14:05 +0700

    fix lock input

--- Scripts/Services/LockInputService.cs
+++ Scripts/Services/LockInputService.cs
@@ -27,10 +27,12 @@
             this.signalBus.Subscribe<InterstitialAdCalledSignal>(() => this.SetLockInput(false));
             this.signalBus.Subscribe<InterstitialAdDisplayedFailedSignal>(() => this.SetLockInput(true));
             this.signalBus.Subscribe<InterstitialAdClosedSignal>(() => this.SetLockInput(true));
+            this.signalBus.Subscribe<InterstitialAdLoadFailedSignal>(() => this.SetLockInput(true));
 
             this.signalBus.Subscribe<RewardedAdCalledSignal>(() => this.SetLockInput(false));
             this.signalBus.Subscribe<RewardedAdShowFailedSignal>(() => this.SetLockInput(true));
             this.signalBus.Subscribe<RewardedAdClosedSignal>(() => this.SetLockInput(true));
+            this.signalBus.Subscribe<RewardedAdLoadFailedSignal>(() => this.SetLockInput(true));
         }
 
         private void SetLockInput(bool value)

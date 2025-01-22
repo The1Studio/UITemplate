42998c83e57fb74596b818b1037fab3971417a77
phuongdmp <phuongdmp7@gmail.com>
2025-01-22 15:28:18 +0700

    up0date

--- Editor/ProjectMigration/MigrationModules/PackageMigration.cs
+++ Editor/ProjectMigration/MigrationModules/PackageMigration.cs
@@ -82,8 +82,8 @@ namespace TheOne.Tool.Migration.ProjectMigration.MigrationModules
             { "com.google.ads.mobile", "9.5.0" },
             { "com.unity.purchasing", "4.12.2" },
             { "com.cysharp.unitask", "2.5.10" },
-            { "jp.hadashikick.vcontainer", "1.16.7" },
-            { "com.coffee.ui-effect", "5.0.1" }
+            { "jp.hadashikick.vcontainer", "1.16.8" },
+            { "com.coffee.ui-effect", "5.2.0" },
         };
 
         [NonSerialized]

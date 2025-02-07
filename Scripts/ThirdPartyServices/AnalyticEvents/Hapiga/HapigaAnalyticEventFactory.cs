dd2b751ce51bbbbf7011e166ba3773eeeb2343bb
Trong Le <110284635+TVanLe@users.noreply.github.com>
2025-02-07 15:05:13 +0700

    custom event factory hapiga

---
+++ Scripts/ThirdPartyServices/AnalyticEvents/Hapiga/HapigaAnalyticEventFactory.cs
@@ -0,0 +1,38 @@
+﻿#if HAPIGA
+namespace TheOneStudio.UITemplate.UITemplate.ThirdPartyServices.AnalyticEvents.Hapiga
+{
+    using Core.AnalyticServices;
+    using Core.AnalyticServices.CommonEvents;
+    using Core.AnalyticServices.Data;
+    using GameFoundation.Signals;
+    using UnityEngine.Scripting;
+
+    public class HapigaAnalyticEventFactory : BaseAnalyticEventFactory
+    {
+        [Preserve]
+        public HapigaAnalyticEventFactory(SignalBus signalBus, IAnalyticServices analyticServices) : base(signalBus, analyticServices)
+        {
+        }
+
+        public override IEvent LevelStart(int level, int gold)
+        {
+            if (level < 100)
+                this.analyticServices.Track(new CustomEvent()
+                {
+                    EventName = $"start_level_{level:D3}",
+                });
+            return base.LevelStart(level, gold);
+        }
+
+        public override IEvent LevelWin(int level, int timeSpent, int winCount)
+        {
+            if (level < 100)
+                this.analyticServices.Track(new CustomEvent()
+                {
+                    EventName = $"win_level_{level:D3}",
+                });
+            return base.LevelWin(level, timeSpent, winCount);
+        }
+    }
+}
+#endif

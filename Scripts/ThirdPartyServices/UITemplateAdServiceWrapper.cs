4d69d1f4b233fff3dbc806b31e7331f8d16e813b
phuongdmp <phuongdmp7@gmail.com>
2025-02-07 18:38:44 +0700

    phuongdmp/native/apply-inter-capping-time-check-to-all-project

--- Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
@@ -391,7 +391,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
             }
 
             if (this.IsResumedFromAnotherServices) return;
-            
+
             this.IsResumedFromAnotherServices = true;
             this.ShowAOAAdsIfAvailable(false).Forget();
         }
@@ -720,11 +720,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
 
         public void Tick()
         {
-            // Problem: Time.unscaledDeltaTime is still incrementing when the game is paused, so if we somehow pause the game, the capping time will be increased
-            // Solution: Time.unscaledDeltaTime is count from the last frame to current frame, so if the time between 2 frames is too long, we will not increase the capping time
-            // If EnableInterCappingTimeFocus is set to false, we will keep the existing capping time behavior
-            // If EnableInterCappingTimeFocus is set to true, we will not increase the capping time while application is not focused
-            if (!this.thirdPartiesConfig.AdSettings.EnableInterCappingTimeFocus || Time.unscaledDeltaTime < 1) this.totalNoAdsPlayingTime += Time.unscaledDeltaTime;
+            if (Time.unscaledDeltaTime < 1) this.totalNoAdsPlayingTime += Time.unscaledDeltaTime;
 
             if (!this.IsCheckedShowFirstOpen && this.gameSessionDataController.OpenTime >= this.adServicesConfig.AOAStartSession) this.CheckShowFirstOpen();
         }

476533c8b2a15ebb758a56b4c55924462161ac92
HungVM <vumanhhung.bc2017@gmail.com>
2025-02-20 15:43:41 +0700

    change: register analytic handler

--- Scripts/VContainer/UITemplateAdsVContainer.cs
+++ Scripts/VContainer/UITemplateAdsVContainer.cs
@@ -3,6 +3,7 @@
 namespace TheOneStudio.UITemplate
 {
     using GameFoundation.DI;
+    using GameFoundation.Scripts.Utilities.Extension;
     using TheOneStudio.UITemplate.UITemplate.Scripts.Services;
     using TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices;
     using TheOneStudio.UITemplate.UITemplate.Services;
@@ -14,27 +15,17 @@ namespace TheOneStudio.UITemplate
     {
         public static void RegisterUITemplateAdsService(this IContainerBuilder builder)
         {
-#if BRAVESTARS
-            builder.Register<BraveStarsAnalyticHandler>(Lifetime.Singleton).As<UITemplateAnalyticHandler>().AsInterfacesAndSelf();
-#elif APERO
-            builder.Register<AperoAnalyticHandler>(Lifetime.Singleton).As<UITemplateAnalyticHandler>().AsInterfacesAndSelf();
-#elif HIGAME
-            builder.Register<HiGameAnalyticHandler>(Lifetime.Singleton).As<UITemplateAnalyticHandler>().AsInterfacesAndSelf();
-#elif ATHENA
-            builder.Register<AthenaAnalyticHandler>(Lifetime.Singleton).As<UITemplateAnalyticHandler>().AsInterfacesAndSelf();
-#else
-            builder.Register<UITemplateAnalyticHandler>(Lifetime.Singleton).AsInterfacesAndSelf();
-#endif
+            builder.RegisterFromDerivedType<UITemplateAnalyticHandler>().AsImplementedInterfaces();
 
-#if CREATIVE
+            #if CREATIVE
             builder.Register<UITemplateAdServiceWrapper, UITemplateAdServiceWrapperCreative>(Lifetime.Singleton);
-#else
+            #else
             builder.Register<UITemplateAdServiceWrapper>(Lifetime.Singleton).AsInterfacesAndSelf();
-#endif
+            #endif
 
-#if CREATIVE
+            #if CREATIVE
             builder.Register<CreativeService>(Lifetime.Singleton).AsInterfacesAndSelf();
-#endif
+            #endif
         }
     }
 }

c0cf44410a2f31db1654de1335f03153f6c86b12
buichivu <117926157+buichivu@users.noreply.github.com>
2025-03-18 18:35:52 +0700

    Fix collapsible banner no show when time collap_interval
    * Check Out UItemplate Fix collap banner
    
    * Build test

--- Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
@@ -128,6 +128,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
             this.signalBus.Subscribe<BannerAdPresentedSignal>(this.OnBannerAdPresented);
             this.signalBus.Subscribe<ApplicationPauseSignal>(this.OnApplicationPauseHandler);
             this.signalBus.Subscribe<CollapsibleBannerAdLoadFailedSignal>(this.OnCollapsibleBannerLoadFailed);
+            this.signalBus.Subscribe<CollapsibleBannerAdDismissedSignal>(this.OnCollapsibleBannerDismissed);
 
             //AOA
             this.StartLoadingAOATime = DateTime.Now;
@@ -204,7 +205,10 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
                 this.signalBus.Fire(new UITemplateOnUpdateBannerStateSignal(true));
             }
         }
-
+        private void OnCollapsibleBannerDismissed(CollapsibleBannerAdDismissedSignal signal)
+        {
+            this.ScheduleRefreshCollapsible();
+        }
         private void ScheduleRefreshCollapsible()
         {
             this.RefreshCollapsibleCts?.Cancel();
@@ -219,6 +223,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
             ).ContinueWith(() =>
             {
                 this.IsRefreshingCollapsible = true;
+                this.HideBannerAd();
                 this.ShowBannerAd();
             }).Forget();
         }

b7b2f9428b000935e1cdd957b61628832116b4bc
HungVM <vumanhhung.bc2017@gmail.com>
2025-03-19 17:40:35 +0700

    change: no internet default config

--- Scripts/Configs/GameEvents/NoInternetConfig.cs
+++ Scripts/Configs/GameEvents/NoInternetConfig.cs
@@ -11,7 +11,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Configs.GameEvents
 
         [ShowIf(nameof(isCustomScreenTrigger))] public List<string> screenTriggerIds;
 
-        public int   SessionToShow        = 2;
+        public int   SessionToShow        = 1;
         public int   ContinuesFailToShow  = 3;
         public float DelayToCheck         = 30f;
         public float CheckInterval        = 1f;

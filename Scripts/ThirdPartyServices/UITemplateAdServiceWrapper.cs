f42658961fb73ebc5cb17df90e8b572c6a13273b
Dung <dungnguyen260103@gmail.com>
2025-03-20 14:47:49 +0700

    can show banner ads

--- Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
+++ Scripts/ThirdPartyServices/UITemplateAdServiceWrapper.cs
@@ -165,6 +165,7 @@ namespace TheOneStudio.UITemplate.UITemplate.Scripts.ThirdPartyServices
         #region banner
 
         public BannerLoadStrategy BannerLoadStrategy => this.thirdPartiesConfig.AdSettings.BannerLoadStrategy;
+        public bool IsBannerAdReady() => this.bannerAdService.IsBannerAdReady();
 
         public virtual async void ShowBannerAd(int width = 320, int height = 50, bool forceShowMediation = false)
         {

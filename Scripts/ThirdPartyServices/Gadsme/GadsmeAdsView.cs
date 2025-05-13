a5a87f9bf6ee84d0dcd89b89965e483f7289d7db
HungNH <127379134+hungnh307@users.noreply.github.com>
2025-05-13 10:40:30 +0700

    Hungnh/native game/gadsme new (#764)
    * update
    
    * gadsme test
    
    * fix click gadsme
    
    * remove prefabs gadsme, fix gadsme rmcf
    
    * update rmcf
    
    * update gadsme ads
    
    * fix gadsme
    
    * fix gadsme remove ads
    
    * format code
    
    * fix admod native ads
    
    * format adservice wraper
    
    * format code and revert file meta
    
    ---------
    
    Co-authored-by: phuongdmp <phuongdmp7@gmail.com>
    Co-authored-by: Trong Le <110284635+tvanle@users.noreply.github.com>

---
+++ Scripts/ThirdPartyServices/Gadsme/GadsmeAdsView.cs
@@ -0,0 +1,54 @@
+﻿namespace Core.AdsServices.Native
+{
+    using Gadsme;
+    using GameFoundation.DI;
+    using GameFoundation.Signals;
+    using ServiceImplementation.Configs.Ads;
+    using TheOneStudio.UITemplate.UITemplate.Signals;
+    using UnityEngine;
+
+    public class GadsmeAdsView : MonoBehaviour
+    {
+        [SerializeField] protected GameObject nonAdsHolder;
+        [SerializeField] protected GameObject adsHolder;
+
+        public  bool              isCanClick;
+        public  int               adsChannelNumber = -1; // -1 means no channel
+        public  GadsmePlacement   currentAdsPlacement;
+        public  AdServicesConfig  adServicesConfig;
+        private INativeAdsService NativeAdsService { get; set; }
+        private SignalBus         SignalBus        { get; set; }
+        public void OnEnable()
+        {
+            var container = this.GetCurrentContainer();
+            this.NativeAdsService = container.Resolve<INativeAdsService>();
+            this.adServicesConfig = container.Resolve<AdServicesConfig>();
+            this.SignalBus        = container.Resolve<SignalBus>();
+
+            this.SignalBus.Subscribe<OnRemoveAdsSucceedSignal>(this.OnRemoveAds );
+            if (this.currentAdsPlacement == null)
+            {
+                this.currentAdsPlacement = this.adsHolder.GetComponentInChildren<GadsmePlacement>();
+            }
+            this.currentAdsPlacement.clickInteraction = this.isCanClick;
+            this.currentAdsPlacement.adChannelNumber  = this.adsChannelNumber;
+            this.ShowAds(this.adServicesConfig.EnableGadsme);
+        }
+        public void OnRemoveAds ()
+        {
+            Debug.Log("onelog : remove gadsme ads ");
+            this.ShowAds(false);
+        }
+        public void ShowAds(bool isShow)
+        {
+            var enable = isShow && !this.NativeAdsService.IsRemoveAds();
+            this.nonAdsHolder.SetActive(!enable);
+            this.adsHolder.SetActive(enable);
+        }
+        private void OnDisable()
+        {
+            this.ShowAds(false);
+            this.SignalBus.Unsubscribe<OnRemoveAdsSucceedSignal>(this.OnRemoveAds );
+        }
+    }
+}

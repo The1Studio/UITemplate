9a5bf6283a22bdab4235bb9b96005d59b8235e82
tuha <tuha263@gmail.com>
2025-05-26 15:03:59 +0700

    Session watcher
    Signed-off-by: tuha <tuha263@gmail.com>

---
+++ Scripts/Services/GameAnalyzer/SessionWatcher.cs
@@ -0,0 +1,83 @@
+﻿namespace Core.AnalyticServices.Data
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using Core.AnalyticServices.CommonEvents;
+    using GameFoundation.Scripts.UIModule.ScreenFlow.Managers;
+    using TheOneStudio.UITemplate.UITemplate.Models.Controllers;
+    using UnityEngine;
+
+    internal sealed class SessionWatcher : MonoBehaviour
+    {
+        private IAnalyticServices                   analyticServices;
+        private IScreenManager                      screenManager;
+        private UITemplateGameSessionDataController gameSessionDataController;
+
+        private string SessionId { get; set; }
+
+
+        private const float HeartbeatInterval = 1f;
+
+        public void Construct(IAnalyticServices analyticServices, IScreenManager screenManager, UITemplateGameSessionDataController gameSessionDataController)
+        {
+            this.analyticServices          = analyticServices;
+            this.screenManager             = screenManager;
+            this.gameSessionDataController = gameSessionDataController;
+            this.Init();
+        }
+
+        private void Init() { this.SessionId = Guid.NewGuid().ToString(); }
+
+        private void Start()
+        {
+            this.StartCoroutine(this.Heartbeat());
+        }
+
+        private IEnumerator Heartbeat()
+        {
+            while (true)
+            {
+                yield return new WaitForSecondsRealtime(HeartbeatInterval);
+                this.analyticServices.Track(new CustomEvent()
+                {
+                    EventName = "ut_heartbeat",
+                    EventProperties = new Dictionary<string, object>
+                    {
+                        { "session_id", this.SessionId },
+                        { "game_session_id", this.gameSessionDataController.OpenTime },
+                        { "placement", this.screenManager.CurrentActiveScreen?.Value.ScreenId ?? "unknown" }
+                    },
+                });
+            }
+        }
+
+        private void OnApplicationQuit()
+        {
+            this.analyticServices.Track(new CustomEvent()
+            {
+                EventName = "ut_application_quit",
+                EventProperties = new Dictionary<string, object>
+                {
+                    { "session_id", this.SessionId },
+                    { "game_session_id", this.gameSessionDataController.OpenTime },
+                    { "placement", this.screenManager.CurrentActiveScreen?.Value.ScreenId ?? "unknown" }
+                },
+            });
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            this.analyticServices.Track(new CustomEvent()
+            {
+                EventName = hasFocus ? "ut_focus_in" : "ut_focus_out",
+                EventProperties = new Dictionary<string, object>()
+                {
+                    { "session_id", this.SessionId },
+                    { "game_session_id", this.gameSessionDataController.OpenTime },
+                    { "placement", this.screenManager.CurrentActiveScreen?.Value.ScreenId ?? "unknown" }
+                }
+            });
+        }
+    }
+}

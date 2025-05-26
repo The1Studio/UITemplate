b03205627a8a8dbbc781cd8f46b84371174a30b1
tuha <tuha263@gmail.com>
2025-05-26 18:42:37 +0700

    Add auto-resolve for SessionWatcher in VContainer
    Enhanced the `RegisterSessionWatcher` method to include auto-resolution of the `SessionWatcher` component. This ensures seamless dependency injection setup and reduces potential manual resolution errors.
    
    Signed-off-by: tuha <tuha263@gmail.com>

--- Scripts/Services/GameAnalyzer/SessionWatcherVContainer.cs
+++ Scripts/Services/GameAnalyzer/SessionWatcherVContainer.cs
@@ -1,11 +1,16 @@
 ﻿#if GDK_VCONTAINER
 namespace Core.AnalyticServices.Data
 {
+    using GameFoundation.DI;
     using VContainer;
 
     public static class SessionWatcherVContainer
     {
-        public static void RegisterSessionWatcher(this IContainerBuilder builder) { builder.Register<SessionWatcher>(Lifetime.Singleton); }
+        public static void RegisterSessionWatcher(this IContainerBuilder builder)
+        {
+            builder.Register<SessionWatcher>(Lifetime.Singleton); 
+            builder.AutoResolve<SessionWatcher>();
+        }
     }
 }
 #endif

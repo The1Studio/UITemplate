6eb8b2093bfa7126c7ec06dcb92f119ecd84883d
CuongND <duycuong2k3@gmail.com>
2025-06-20 17:22:42 +0700

    Fix: Add [Preserve] attribute to event classes for serialization

--- Scripts/ThirdPartyServices/AnalyticEvents/Sonat/TutorialBegin.cs
+++ Scripts/ThirdPartyServices/AnalyticEvents/Sonat/TutorialBegin.cs
@@ -3,13 +3,12 @@
     using System.Collections.Generic;
     using Core.AnalyticServices.Data;
     using UnityEngine.Scripting;
-
+    [Preserve]
     internal sealed class TutorialBegin : IEvent
     {
         public string                     Id         { get; }
         public Dictionary<string, object> Properties { get; }
 
-        [Preserve]
         public TutorialBegin(string id, Dictionary<string, object> properties)
         {
             this.Id         = id;

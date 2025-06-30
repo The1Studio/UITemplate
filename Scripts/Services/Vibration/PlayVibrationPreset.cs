60df7e416413298023db0ad7e5f64df53f6f95b5
Nguyễn Mạnh <75465287+frostbun@users.noreply.github.com>
2025-06-30 12:30:47 +0700

    add PlayVibration Mono (#794)

---
+++ Scripts/Services/Vibration/PlayVibrationPreset.cs
@@ -0,0 +1,25 @@
+﻿#nullable enable
+namespace UITemplate.Scripts.Services.Vibration
+{
+    using GameFoundation.DI;
+    using TheOneStudio.UITemplate.UITemplate.Interfaces;
+    using TheOneStudio.UITemplate.UITemplate.Services.Vibration;
+    using UnityEngine;
+
+    internal sealed class PlayVibrationPreset : MonoBehaviour
+    {
+        [SerializeField] private VibrationPresetType vibrationPresetType;
+
+        private IVibrationService vibrationService = null!;
+
+        private void Awake()
+        {
+            this.vibrationService = this.GetCurrentContainer().Resolve<IVibrationService>();
+        }
+
+        private void OnEnable()
+        {
+            this.vibrationService.PlayPresetType(this.vibrationPresetType);
+        }
+    }
+}

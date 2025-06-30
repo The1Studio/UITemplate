326c43e2c489ebac893fa4c56be62ec9407f71b8
phuongdmp <phuongdmp7@gmail.com>
2025-06-30 17:48:06 +0700

    refactor break ads

--- Scripts/Services/BreakAds/BreakAdsViewHelper.cs
+++ Scripts/Services/BreakAds/BreakAdsViewHelper.cs
@@ -6,26 +6,37 @@
     using GameFoundation.Scripts.UIModule.ScreenFlow.BaseScreen.Presenter;
     using GameFoundation.Signals;
     using ServiceImplementation.Configs;
+    using TheOneStudio.UITemplate.UITemplate.Models.Controllers;
     using UnityEngine.Scripting;
 
     // Rebind this class to your own view
     public class BreakAdsViewHelper
     {
+        public bool IsViewReady => this.BreakAdsPopupPresenter.ScreenStatus == ScreenStatus.Opened;
+
         protected BreakAdsPopupView       View;
         protected BreakAdsPopupPresenter  BreakAdsPopupPresenter;
         protected CancellationTokenSource Cts;
 
-        public bool IsViewReady => BreakAdsPopupPresenter.ScreenStatus == ScreenStatus.Opened;
+        #region Inject
 
-        protected readonly SignalBus          SignalBus;
-        private readonly   ThirdPartiesConfig thirdPartiesConfig;
+        protected readonly SignalBus                         signalBus;
+        protected readonly ThirdPartiesConfig                thirdPartiesConfig;
+        protected readonly UITemplateInventoryDataController inventoryDataController;
 
         [Preserve]
-        public BreakAdsViewHelper(SignalBus signalBus, ThirdPartiesConfig thirdPartiesConfig)
+        public BreakAdsViewHelper(
+            SignalBus                         signalBus,
+            ThirdPartiesConfig                thirdPartiesConfig,
+            UITemplateInventoryDataController inventoryDataController
+        )
         {
-            this.SignalBus          = signalBus;
-            this.thirdPartiesConfig = thirdPartiesConfig;
+            this.signalBus               = signalBus;
+            this.thirdPartiesConfig      = thirdPartiesConfig;
+            this.inventoryDataController = inventoryDataController;
         }
+
+        #endregion
 
         public virtual void OnViewReady(BreakAdsPopupView view, BreakAdsPopupPresenter breakAdsPopupPresenter)
         {
@@ -35,9 +46,9 @@
 
         public virtual UniTask BindData()
         {
-            this.SignalBus.Subscribe<InterstitialAdClosedSignal>(this.BreakAdsPopupPresenter.CloseView);
-            this.SignalBus.Subscribe<InterstitialAdDisplayedFailedSignal>(this.BreakAdsPopupPresenter.CloseView);
-            this.SignalBus.Subscribe<InterstitialAdDisplayedSignal>(this.BreakAdsPopupPresenter.CloseView);
+            this.signalBus.Subscribe<InterstitialAdClosedSignal>(this.BreakAdsPopupPresenter.CloseView);
+            this.signalBus.Subscribe<InterstitialAdDisplayedFailedSignal>(this.BreakAdsPopupPresenter.CloseView);
+            this.signalBus.Subscribe<InterstitialAdDisplayedSignal>(this.BreakAdsPopupPresenter.CloseView);
             this.AutomaticCloseView();
 
             return UniTask.CompletedTask;
@@ -59,10 +70,23 @@
 
         public virtual void Dispose()
         {
-            this.SignalBus.Unsubscribe<InterstitialAdClosedSignal>(this.BreakAdsPopupPresenter.CloseView);
-            this.SignalBus.Unsubscribe<InterstitialAdDisplayedFailedSignal>(this.BreakAdsPopupPresenter.CloseView);
-            this.SignalBus.Unsubscribe<InterstitialAdDisplayedSignal>(this.BreakAdsPopupPresenter.CloseView);
-            this.Cts?.Cancel();
+            if (this.Cts == null) return;
+            
+            this.RewardAfterWatchedAds();
+
+            this.signalBus.Unsubscribe<InterstitialAdClosedSignal>(this.BreakAdsPopupPresenter.CloseView);
+            this.signalBus.Unsubscribe<InterstitialAdDisplayedFailedSignal>(this.BreakAdsPopupPresenter.CloseView);
+            this.signalBus.Unsubscribe<InterstitialAdDisplayedSignal>(this.BreakAdsPopupPresenter.CloseView);
+
+            this.Cts.Cancel();
+            this.Cts.Dispose();
+            this.Cts = null;
+        }
+
+        protected virtual void RewardAfterWatchedAds()
+        {
+            if (!this.thirdPartiesConfig.AdSettings.IsBreakAdsRewardCurrency) return;
+            this.inventoryDataController.AddCurrency(this.thirdPartiesConfig.AdSettings.BreakAdsRewardCurrencyAmount, this.thirdPartiesConfig.AdSettings.BreakAdsRewardCurrency, "break_ads", this.View.currencyTransform);
         }
     }
 }

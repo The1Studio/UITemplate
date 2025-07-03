d827de3b1606aaefe7841b048891c027329c79d0
phuongdmp <phuongdmp7@gmail.com>
2025-07-03 16:24:00 +0700

    update PerformanceUtil

--- Scripts/Utils/PerformanceUtil.cs
+++ Scripts/Utils/PerformanceUtil.cs
@@ -1,5 +1,6 @@
 ﻿namespace TheOneStudio.UITemplate.UITemplate.Utils
 {
+    using System.Linq;
     using SystemInfo = UnityEngine.Device.SystemInfo;
 
     public enum QualityLevel
@@ -13,25 +14,60 @@
     {
         public static QualityLevel CheckDevicePerformance()
         {
-            #if UNITY_IOS
-            if (IsiOSLowEnd())
-                return Quality.Low;
-            else if (IsiOSMediumEnd())
-            {
-                return Quality.Medium;
-            }
-            return Quality.High;
+            #if UNITY_ANDROID
+            return CheckDevicePerformanceAndroid();
+            #else
+            return CheckDevicePerformanceIOS();
             #endif
+        }
 
-            var ramMb    = SystemInfo.systemMemorySize;
-            var cpuCores = SystemInfo.processorCount;
+        #region Android
 
-            if (ramMb >= 8192 || cpuCores >= 8) return QualityLevel.High;
-            if (ramMb >= 4096 || cpuCores >= 6) return QualityLevel.Medium;
+        private static QualityLevel CheckDevicePerformanceAndroid()
+        {
+            var ramMb     = SystemInfo.systemMemorySize;
+            var cpuCores  = SystemInfo.processorCount;
+            var gpuModel  = SystemInfo.graphicsDeviceName.ToLower();
+            var gpuMemory = SystemInfo.graphicsMemorySize; // in MB
+
+            var isWeakGpu = WeakAndroidGpus.Any(weak => gpuModel.Contains(weak));
+
+            if (ramMb >= 8192 && cpuCores >= 8 && gpuMemory >= 2000 && !isWeakGpu) return QualityLevel.High;
+            if (ramMb >= 4096 && cpuCores >= 4 && gpuMemory >= 1000 && !isWeakGpu) return QualityLevel.Medium;
+
             return QualityLevel.Low;
         }
 
-        public static bool IsiOSLowEnd()
+        private static readonly string[] WeakAndroidGpus = new string[]
+        {
+            // Adreno
+            "adreno 3", "adreno 4", "adreno 505", "adreno 506", "adreno 508", "adreno 509", "adreno 510", "adreno 512", "adreno 516", "adreno 610", "adreno 612", "adreno 615", "adreno 618", "adreno 620",
+            // Mali
+            "mali-400", "mali-450", "mali-t720", "mali-t760", "mali-t820", "mali-t830", "mali-t860", "mali-t880", "mali-g31", "mali-g51", "mali-g52", "mali-g57 mc1", "mali-g57 mc2",
+            // PowerVR
+            "powervr ge8100", "powervr ge8300", "powervr sgx544", "powervr sgx543", "powervr gx6250", "powervr gx6450", "powervr ge8320", "powervr ge8322",
+            // Tegra
+            "tegra 3", "tegra 4", "tegra k1", "tegra x1",
+            // Vivante, ARM khác
+            "vivante", "mali-470",
+        };
+
+        #endregion
+
+        #region iOS
+
+        private static QualityLevel CheckDevicePerformanceIOS()
+        {
+            if (IsiOSLowEnd())
+                return QualityLevel.Low;
+            else if (IsiOSMediumEnd())
+            {
+                return QualityLevel.Medium;
+            }
+            return QualityLevel.High;
+        }
+
+        private static bool IsiOSLowEnd()
         {
             var model = SystemInfo.deviceModel.ToLower();
 
@@ -58,7 +94,7 @@
             return false;
         }
 
-        public static bool IsiOSMediumEnd()
+        private static bool IsiOSMediumEnd()
         {
             var model = SystemInfo.deviceModel.ToLower();
 
@@ -74,5 +110,7 @@
 
             return false;
         }
+
+        #endregion
     }
 }
